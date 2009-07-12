676a0f2e1c97de0573328f89a5d4b89004074bed
Dev <Dev@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2009-07-12 00:23:15 +0000

    Merged UDataStreamReader and UDataStreamWriter units into single UDataStreamIO unit.

--- trunk/Src/UDataStreamIO.pas
+++ trunk/Src/UDataStreamIO.pas
@@ -1,14 +1,19 @@
 {
- * UDataStreamWriter.pas
+ * UDataStreamIO.pas
  *
- * Implements a stream wrapper class that can write numeric and string data to a
- * stream. Integers of various sizes are encoded as ASCII hex digits.
+ * Stream wrapper that can read numeric and string data from a stream. Assumes
+ * integers of various sizes are encoded as ASCII hex digits in stream.
  *
- * Requires the DelphiDabbler Stream Extension Classes v2.0.1 or later.
+ * Originally named UDataStreamReader.pas. Renamed as UDataStreamIO.pas at v2.0.
  *
- * v1.0 of 11 Sep 2008  - Original version.
- * v1.1 of 22 May 2009  - Added new TDataStreamWriter.WriteSizedLongString
+ * v0.1 of 30 Apr 2006  - Original version named UDataStreamReader.
+ * v1.0 of 24 May 2006  - Improved and corrected comments.
+ *                      - Relocated and rationalised $WARN directives.
+ * v1.1 of 22 May 2009  - Added new TDataStreamWriter.ReadSizedLongString
  *                        method.
+ * v2.0 of 12 Jul 2009  - Renamed unit as UDataStreamIO.
+ *                      - Added TDataStreamWriter formerly in UDataStreamWriter.
+ *                      - Made private section of TDataStreamReader strict.
  *
  *
  * ***** BEGIN LICENSE BLOCK *****
@@ -23,20 +28,22 @@
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  * the specific language governing rights and limitations under the License.
  *
- * The Original Code is UDataStreamWriter.pas
+ * The Original Code is UDataStreamIO.pas
  *
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2008-2009 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2006-2009 Peter
  * Johnson. All Rights Reserved.
  *
  * ***** END LICENSE BLOCK *****
 }
 
 
-unit UDataStreamWriter;
+unit UDataStreamIO;
 
+{$WARN UNSAFE_CAST OFF}
+{$WARN UNSAFE_CODE OFF}
 
 interface
 
@@ -47,6 +54,48 @@ uses
 
 
 type
+
+  {
+  TDataStreamReader:
+    Stream wrapper that can read numeric and string data from a stream. Assumes
+    integers of various sizes are encoded as ASCII hex digits in stream.
+  }
+  TDataStreamReader = class(TPJStreamWrapper)
+  strict private
+    function ReadHexDigits(const Count: Integer): LongInt;
+      {Reads hex digits from stream.
+        @param Count [in] Number of hex digits to read.
+        @return Value read from stream.
+      }
+  public
+    function ReadSmallInt: SmallInt;
+      {Reads small integer from stream, encoded as 4 hex digits.
+        @return Value read from stream.
+      }
+    function ReadLongInt: LongInt;
+      {Reads long integer from stream, encoded as 8 char hex digits.
+        @return Value read from stream.
+      }
+    function ReadInt64: Int64;
+      {Reads an Int64 value from stream, encoded as 16 char hex digit.
+        @return Value read from stream.
+      }
+    function ReadString(const Length: Integer): string;
+      {Reads a string of specified size from stream.
+        @param Length [in] Length of string to be read.
+        @return String read from stream.
+      }
+    function ReadSizedString: string;
+      {Reads string from stream that is preceded by a small int length
+      descriptor.
+        @return String read from stream.
+      }
+    function ReadSizedLongString: string;
+      {Reads string from stream that is preceded by a long int length
+      descriptor.
+        @return String read from stream.
+      }
+  end;
 
   {
   TDataStreamWriter:
@@ -96,6 +145,75 @@ uses
   // Delphi
   SysUtils;
 
+
+{ TDataStreamReader }
+
+function TDataStreamReader.ReadHexDigits(
+  const Count: Integer): LongInt;
+  {Reads hex digits from stream.
+    @param Count [in] Number of hex digits to read.
+    @return Value read from stream.
+  }
+begin
+  Result := StrToInt('$' + ReadString(Count));
+end;
+
+function TDataStreamReader.ReadInt64: Int64;
+  {Reads an Int64 value from stream, encoded as 16 char hex digit.
+    @return Value read from stream.
+  }
+begin
+  Int64Rec(Result).Hi := ReadLongInt;
+  Int64Rec(Result).Lo := ReadLongInt;
+end;
+
+function TDataStreamReader.ReadLongInt: LongInt;
+  {Reads long integer from stream, encoded as 8 char hex digits.
+    @return Value read from stream.
+  }
+begin
+  Result := ReadHexDigits(8);
+end;
+
+function TDataStreamReader.ReadSizedLongString: string;
+  {Reads string from stream that is preceded by a long int length descriptor.
+    @return String read from stream.
+  }
+var
+  Length: LongInt; // length of string
+begin
+  Length := ReadLongInt;
+  Result := ReadString(Length);
+end;
+
+function TDataStreamReader.ReadSizedString: string;
+  {Reads string from stream that is preceded by a small int length descriptor.
+    @return String read from stream.
+  }
+var
+  Length: SmallInt; // length of string
+begin
+  Length := ReadSmallInt;
+  Result := ReadString(Length);
+end;
+
+function TDataStreamReader.ReadSmallInt: SmallInt;
+  {Reads small integer from stream, encoded as 4 hex digits.
+    @return Value read from stream.
+  }
+begin
+  Result := ReadHexDigits(4);
+end;
+
+function TDataStreamReader.ReadString(const Length: Integer): string;
+  {Reads a string of specified size from stream.
+    @param Length [in] length of string to be read.
+    @return String read from stream.
+  }
+begin
+  SetLength(Result, Length);
+  BaseStream.ReadBuffer(Result[1], Length);
+end;
 
 { TDataStreamWriter }
 
