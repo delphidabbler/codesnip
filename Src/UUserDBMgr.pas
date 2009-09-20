ba2783efb6862aeb244a7eb75ebf39f60a4008c0
Peter Johnson <delphidabbler@users.noreply.github.com>
2009-09-20 16:18:42 +0000

    * Renamed TUserDBMgr's Add, Edit and Delete methods to AddSnippet, EditSnippet and DeleteSnippet respectively to avoid confusion with similar methods that act on categories. * Updated FmMain to use the renamed methods.

--- Src/UUserDBMgr.pas
+++ Src/UUserDBMgr.pas
@@ -74,38 +74,33 @@ type
         @return Required category list. Caller must free.
       }
   public
+    class procedure AddSnippet;
+      {Adds a new user defined snippet to database.
+      }
+    class procedure EditSnippet(const SnippetName: string);
+      {Gets user to edit a named snippet using edit snippet dialog box.
+        @param SnippetName [in] Name of snippet to be edited. Must be user
+          defined.
+      }
+    class procedure DeleteSnippet(const ViewItem: TViewItem);
+      {Deletes a snippet from the database if possible.
+        @param ViewItem [in] View item containing snippet to be deleted. Must be
+          a user defined snippet.
+      }
     class function CanEdit(const ViewItem: TViewItem): Boolean;
       {Checks if a view item can be edited.
         @param ViewItem [in] View item to check.
-        @return True if view item can be edited, i.e. if ViewItem is a routine
+        @return True if view item can be edited, i.e. if ViewItem is a snippet
           and is user defined.
       }
-    class function CanSave: Boolean;
-      {Checks if user database can be saved.
-        @return True if database can be saved, False if not.
+    class procedure AddCategory;
+      {Adds a new category to user database.
       }
-    class procedure Edit(const RoutineName: string);
-      {Gets user to edit a named snippet using edit snippet dialog box.
-        @param RoutineName [in] Name of routine to be edited. Must be user
-          defined.
+    class procedure DeleteACategory;
+      {Selects and deletes a category from database if possible.
       }
-    class procedure Add;
-      {Adds a new user defined snippet to database.
-      }
-    class procedure Save;
-      {Saves user database.
-      }
-    class procedure Delete(const ViewItem: TViewItem);
-      {Deletes a snippet from the database if possible.
-        @param ViewItem [in] Snippet to be deleted. Must be a user defined
-          routine.
-      }
-    class procedure BackupDatabase;
-      {Creates a backup of user database in a file specified by user.
-      }
-    class function RestoreDatabase: Boolean;
-      {Restores user database from a previously created backup file.
-        @return True if user OKs, False if not.
+    class procedure RenameACategory;
+      {Selects and renames a category in database if possible.
       }
     class function CanRenameACategory: Boolean;
       {Checks if it is possoble to rename any categories.
@@ -116,14 +111,19 @@ type
         @return True if there are some user-defined, deletable, categories,
           False if not.
       }
-    class procedure AddCategory;
-      {Adds a new category to user database.
+    class procedure Save;
+      {Saves user database.
       }
-    class procedure DeleteACategory;
-      {Selects and deletes a category from database if possible.
+    class function CanSave: Boolean;
+      {Checks if user database can be saved.
+        @return True if database can be saved, False if not.
       }
-    class procedure RenameACategory;
-      {Selects and renames a category in database if possible.
+    class procedure BackupDatabase;
+      {Creates a backup of user database in a file specified by user.
+      }
+    class function RestoreDatabase: Boolean;
+      {Restores user database from a previously created backup file.
+        @return True if user OKs, False if not.
       }
   end;
 
@@ -142,20 +142,20 @@ uses
 
 { TUserDBMgr }
 
-class procedure TUserDBMgr.Add;
-  {Adds a new user defined snippet to database.
-  }
-begin
-  // Display Add Snippet dialog box which performs update of database.
-  TUserDBEditDlg.AddNewRoutine(nil);
-end;
-
 class procedure TUserDBMgr.AddCategory;
   {Adds a new category to user database.
   }
 begin
   // all work takes place in dialog box
   TAddCategoryDlg.Execute(nil);
+end;
+
+class procedure TUserDBMgr.AddSnippet;
+  {Adds a new user defined snippet to database.
+  }
+begin
+  // Display Add Snippet dialog box which performs update of database.
+  TUserDBEditDlg.AddNewRoutine(nil);
 end;
 
 class procedure TUserDBMgr.BackupDatabase;
@@ -212,11 +212,11 @@ end;
 class function TUserDBMgr.CanEdit(const ViewItem: TViewItem): Boolean;
   {Checks if a view item can be edited.
     @param ViewItem [in] View item to check.
-    @return True if view item can be edited, i.e. if ViewItem is a routine and
+    @return True if view item can be edited, i.e. if ViewItem is a snippet and
       is user defined.
   }
 begin
-  Assert(Assigned(ViewItem), ClassName + '.CanEdit: VeiwItem is nil');
+  Assert(Assigned(ViewItem), ClassName + '.CanEdit: ViewItem is nil');
   Result := Assigned(ViewItem) and
     (ViewItem.Kind = vkRoutine) and
     ViewItem.Routine.UserDefined;
@@ -325,72 +325,6 @@ begin
       Result.Add(Cat);
 end;
 
-class procedure TUserDBMgr.Delete(const ViewItem: TViewItem);
-  {Deletes a snippet from the database if possible.
-    @param ViewItem [in] Snippet to be deleted. Must be a user defined routine.
-  }
-
-  // ---------------------------------------------------------------------------
-  function RoutineNames(const IDList: ISnippetIDList): IStringList;
-    {Builds a list of routine names from a routine ID list.
-      @param IDList [in] List of routine IDs.
-      @return List of routine names.
-    }
-  var
-    ID: TSnippetID; // loops through all IDs in list
-  begin
-    Result := TIStringList.Create;
-    for ID in IDList do
-      Result.Add(ID.Name);
-  end;
-  // ---------------------------------------------------------------------------
-
-var
-  Dependents: ISnippetIDList; // list of dependent routine IDs
-  Referrers: ISnippetIDList;  // list referring routine IDs
-  ConfirmMsg: string;         // message displayed to confirm deletion
-resourcestring
-  // Prompts & error messages
-  sConfirmDelete = 'Please confirm you wish to delete %s';
-  sConfirmDeleteEx = 'Please confirm you wish to delete %0:s.' + EOL2
-    + 'Deleting this snippet will also remove it from the cross reference list '
-    + 'of the following snippets:' + EOL + '    %1:s';
-  sHasDependents = 'Sorry, this snippet can''t be deleted. It is required by '
-    + 'the following snippets:' + EOL + '    %s';
-begin
-  Assert(ViewItem.Kind = vkRoutine,
-    ClassName + '.Delete: Current view kind is not vkRoutine');
-  Assert(ViewItem.Routine.UserDefined,
-    ClassName + '.Delete: Routine must be user defined');
-  // Check if routine has dependents: don't allow deletion if so
-  Dependents := (Snippets as ISnippetsEdit).GetDependents(ViewItem.Routine);
-  if Dependents.Count > 0 then
-  begin
-    TMessageBox.Error(
-      nil,
-      Format(
-        sHasDependents,
-        [RoutineNames(Dependents).GetText(',' + EOL + '    ', False)]
-      )
-    );
-    Exit;
-  end;
-  // Get permission to delete. If routine has dependents list them in prompt
-  Referrers := (Snippets as ISnippetsEdit).GetReferrers(ViewItem.Routine);
-  if Referrers.Count = 0 then
-    ConfirmMsg := Format(sConfirmDelete, [ViewItem.Routine.Name])
-  else
-    ConfirmMsg := Format(
-      sConfirmDeleteEx,
-      [
-        ViewItem.Routine.Name,
-        RoutineNames(Referrers).GetText(',' + EOL + '    ', False)
-      ]
-    );
-  if TMessageBox.Confirm(nil, ConfirmMsg) then
-    (Snippets as ISnippetsEdit).DeleteRoutine(ViewItem.Routine);
-end;
-
 class procedure TUserDBMgr.DeleteACategory;
   {Selects and deletes a category from database if possible.
   }
@@ -406,17 +340,84 @@ begin
   end;
 end;
 
-class procedure TUserDBMgr.Edit(const RoutineName: string);
+class procedure TUserDBMgr.DeleteSnippet(const ViewItem: TViewItem);
+  {Deletes a snippet from the database if possible.
+    @param ViewItem [in] View item containing snippet to be deleted. Must be a
+      user defined snippet.
+  }
+
+  // ---------------------------------------------------------------------------
+  function SnippetNames(const IDList: ISnippetIDList): IStringList;
+    {Builds a list of snippet names from a snippet ID list.
+      @param IDList [in] List of snippet IDs.
+      @return List of snippet names.
+    }
+  var
+    ID: TSnippetID; // loops through all IDs in list
+  begin
+    Result := TIStringList.Create;
+    for ID in IDList do
+      Result.Add(ID.Name);
+  end;
+  // ---------------------------------------------------------------------------
+
+var
+  Dependents: ISnippetIDList; // list of dependent snippet IDs
+  Referrers: ISnippetIDList;  // list referring snippet IDs
+  ConfirmMsg: string;         // message displayed to confirm deletion
+resourcestring
+  // Prompts & error messages
+  sConfirmDelete = 'Please confirm you wish to delete %s';
+  sConfirmDeleteEx = 'Please confirm you wish to delete %0:s.' + EOL2
+    + 'Deleting this snippet will also remove it from the cross reference list '
+    + 'of the following snippets:' + EOL + '    %1:s';
+  sHasDependents = 'Sorry, this snippet can''t be deleted. It is required by '
+    + 'the following snippets:' + EOL + '    %s';
+begin
+  Assert(ViewItem.Kind = vkRoutine,
+    ClassName + '.Delete: Current view kind is not vkRoutine');
+  Assert(ViewItem.Routine.UserDefined,
+    ClassName + '.Delete: Snippet must be user defined');
+  // Check if snippet has dependents: don't allow deletion if so
+  Dependents := (Snippets as ISnippetsEdit).GetDependents(ViewItem.Routine);
+  if Dependents.Count > 0 then
+  begin
+    TMessageBox.Error(
+      nil,
+      Format(
+        sHasDependents,
+        [SnippetNames(Dependents).GetText(',' + EOL + '    ', False)]
+      )
+    );
+    Exit;
+  end;
+  // Get permission to delete. If snippet has dependents list them in prompt
+  Referrers := (Snippets as ISnippetsEdit).GetReferrers(ViewItem.Routine);
+  if Referrers.Count = 0 then
+    ConfirmMsg := Format(sConfirmDelete, [ViewItem.Routine.Name])
+  else
+    ConfirmMsg := Format(
+      sConfirmDeleteEx,
+      [
+        ViewItem.Routine.Name,
+        SnippetNames(Referrers).GetText(',' + EOL + '    ', False)
+      ]
+    );
+  if TMessageBox.Confirm(nil, ConfirmMsg) then
+    (Snippets as ISnippetsEdit).DeleteRoutine(ViewItem.Routine);
+end;
+
+class procedure TUserDBMgr.EditSnippet(const SnippetName: string);
   {Gets user to edit a named snippet using edit snippet dialog box.
-    @param RoutineName [in] Name of routine to be edited. Must be user defined.
+    @param SnippetName [in] Name of snippet to be edited. Must be user defined.
   }
 var
-  Routine: TRoutine;    // reference to routine to be edited
+  Snippet: TRoutine;    // reference to snippet to be edited
 begin
-  Routine := Snippets.Routines.Find(RoutineName, True);
-  if not Assigned(Routine) then
-    raise EBug.Create(ClassName + '.Edit: Routine not in user database');
-  TUserDBEditDlg.EditRoutine(nil, Routine);
+  Snippet := Snippets.Routines.Find(SnippetName, True);
+  if not Assigned(Snippet) then
+    raise EBug.Create(ClassName + '.EditSnippet: Snippet not in user database');
+  TUserDBEditDlg.EditRoutine(nil, Snippet);
 end;
 
 class procedure TUserDBMgr.RenameACategory;
