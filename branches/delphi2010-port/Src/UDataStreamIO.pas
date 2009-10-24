1bcf9e267cf3acc925d1cfd4511a5583f3aacc3d
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2009-10-24 10:02:53 +0000

    Modified UDataStreamIO.pas for Unicode compatibility. * Changed string type read/written to TDataStreamString which is defined as AnsiString on Ansi Delphis and Latin1String on Unicode: i.e. single byte strings on all compilers. * Provided overloaded write string methods that take Unicode strings and convert to Latin1. * Provided methods to convert strings to byte arrays for writing.

--- branches/delphi2010-port/Src/UDataStreamIO.pas
+++ branches/delphi2010-port/Src/UDataStreamIO.pas
@@ -41,13 +41,28 @@ unit UDataStreamIO;
 
 interface
 
+{ TODO -cDocs : Update documentation for new and changed methods. }
+{ TODO -cRefactor : Rationalise code: use WriteBytes for all stream output
+  regardless of Unicode or not. }
+{ TODO -cRefactor : Refactor out the TBytes overrides of the WriteXXXString
+  methods. Not used publicly. }
 
 uses
+  // Delphi
+  SysUtils,
   // DelphiDabbler library
-  PJStreamWrapper;
+  PJStreamWrapper,
+  // Project
+  UUnicodeHelper;
 
 
 type
+
+  {
+  TDataStreamString:
+    Type of string read and written by TDataStreamReader and TDataStreamWriter.
+  }
+  TDataStreamString = Latin1String;
 
   {
   TDataStreamReader:
@@ -74,17 +89,17 @@ type
       {Reads an Int64 value from stream, encoded as 16 char hex digit.
         @return Value read from stream.
       }
-    function ReadString(const Length: Integer): string;
+    function ReadString(const Length: Integer): TDataStreamString;
       {Reads a string of specified size from stream.
         @param Length [in] Length of string to be read.
         @return String read from stream.
       }
-    function ReadSizedString: string;
+    function ReadSizedString: TDataStreamString;
       {Reads string from stream that is preceded by a small int length
       descriptor.
         @return String read from stream.
       }
-    function ReadSizedLongString: string;
+    function ReadSizedLongString: TDataStreamString;
       {Reads string from stream that is preceded by a long int length
       descriptor.
         @return String read from stream.
@@ -98,8 +113,15 @@ type
   }
   TDataStreamWriter = class(TPJStreamWrapper)
   strict private
+    {$IFDEF UNICODE}
+    procedure WriteBytes(const Bytes: TBytes);
+      {Writes an array of bytes to a stream. Bytes must represent valid Latin 1
+      characters and must not be 0.
+        @param Bytes [in] Bytes to be written.
+      }
+    {$ENDIF}
     procedure WriteHex(const Value: LongInt; const Count: Integer);
-      {Writes hex digits to stream.
+      {Writes a hex representation of a number to stream.
         @param Value [in] Value to be written.
         @param Count [in] Number of hex digits required.
       }
@@ -109,47 +131,93 @@ type
         @param Value [in] Value to be written.
       }
     procedure WriteLongInt(const Value: LongInt);
-      {Write a 32 bit integer to the stream as hex digits.
+      {Writes a 32 bit integer to the stream as hex digits.
         @param Value [in] Value to be written.
       }
-    procedure WriteString(const Str: string; const Length: Integer); overload;
+
+    procedure WriteString(const Str: TDataStreamString;
+      const Length: Integer); overload;
       {Writes a fixed number of characters from a string to the stream.
         @param Str [in] String to be written.
         @param Length [in] Number of characters from string to write.
       }
-    procedure WriteString(const Str: string); overload;
+    procedure WriteString(const Str: TDataStreamString); overload;
       {Writes a string to stream.
         @param Str [in] String to be written.
       }
-    procedure WriteSizedString(const Str: string);
+    {$IFDEF UNICODE}
+    procedure WriteString(const Str: string; const Length: Integer); overload;
+    procedure WriteString(const Str: string); overload;
+    {$ENDIF}
+
+    procedure WriteSizedString(const Str: TDataStreamString); overload;
       {Writes a string to stream preceded by a 16 bit length as hex digits.
         @param Str [in] String to be written.
       }
-    procedure WriteSizedLongString(const Str: string);
+    {$IFDEF UNICODE}
+    procedure WriteSizedString(const Bytes: TBytes); overload;
+      {Writes an array of bytes to stream as a sized string preceeded by a 16
+      bit length as hex digits. Bytes must represent valid Latin 1 characters
+      and must not be zero.
+        @param Bytes [in] Bytes to be written.
+      }
+    procedure WriteSizedString(const Str: string); overload;
+    {$ENDIF}
+
+    procedure WriteSizedLongString(const Str: TDataStreamString); overload;
       {Writes a string to stream preceded by a 32 bit length as hex digits.
         @param Str [in] String to be written.
       }
+    {$IFDEF UNICODE}
+    procedure WriteSizedLongString(const Str: string); overload;
+    procedure WriteSizedLongString(const Bytes: TBytes); overload;
+      {Writes an array of bytes to stream as a sized string preceeded by a 32
+      bit length as hex digits. Bytes must represent valid Latin 1 characters
+      and must not be zero.
+        @param Bytes [in] Bytes to be written.
+      }
+    {$ENDIF}
   end;
 
 
 implementation
 
+{
+  About data streams
 
-uses
-  // Delphi
-  SysUtils;
+  Data streams are always composed of text strings. Numbers are encoded as
+  strings of hex digits.
+  Text contained in the streams should be encoded in the Latin-1 code page.
+  Characters occupy 1 byte.
+
+  There are several "data types":
+  + SmallInt          = 16 bit integer encoded as 4 hex digits
+  + LongInt           = 32 bit integer encoded as 8 hex digits
+  + Int64             = 64 bit integer encoded as 16 hex digits
+  + SizedString       = variable length string of 1 byte chars preceeded by
+                        a SmallInt that gives the number of characters
+  + SizedLongString   = variable length string of 1 byte chars preceeded by
+                        a LongInt that gives the number of characters
+  + Character         = a single 1 byte character
+
+  There is a pseudo-data type - the fixed size string that is composed of a
+  known number of Characters.
+
+  There is no meta-data in the stream to describe the data type of an element of
+  the file. The reader / writer must have knowledge of the file format being
+  read / written.
+}
 
 
 { TDataStreamReader }
 
-function TDataStreamReader.ReadHexDigits(
-  const Count: Integer): LongInt;
+function TDataStreamReader.ReadHexDigits(const Count: Integer): LongInt;
   {Reads hex digits from stream.
     @param Count [in] Number of hex digits to read.
     @return Value read from stream.
   }
 begin
-  Result := StrToInt('$' + ReadString(Count));
+  Result := StrToInt('$' + string(ReadString(Count)));
 end;
 
 function TDataStreamReader.ReadInt64: Int64;
@@ -169,7 +237,7 @@ begin
   Result := ReadHexDigits(8);
 end;
 
-function TDataStreamReader.ReadSizedLongString: string;
+function TDataStreamReader.ReadSizedLongString: TDataStreamString;
   {Reads string from stream that is preceded by a long int length descriptor.
     @return String read from stream.
   }
@@ -180,7 +248,7 @@ begin
   Result := ReadString(Length);
 end;
 
-function TDataStreamReader.ReadSizedString: string;
+function TDataStreamReader.ReadSizedString: TDataStreamString;
   {Reads string from stream that is preceded by a small int length descriptor.
     @return String read from stream.
   }
@@ -199,7 +267,7 @@ begin
   Result := ReadHexDigits(4);
 end;
 
-function TDataStreamReader.ReadString(const Length: Integer): string;
+function TDataStreamReader.ReadString(const Length: Integer): TDataStreamString;
   {Reads a string of specified size from stream.
     @param Length [in] length of string to be read.
     @return String read from stream.
@@ -211,9 +279,20 @@ end;
 
 { TDataStreamWriter }
 
+{$IFDEF UNICODE}
+procedure TDataStreamWriter.WriteBytes(const Bytes: TBytes);
+  {Writes an array of bytes to a stream. Bytes must represent valid Latin 1
+  characters and must not be 0.
+    @param Bytes [in] Bytes to be written.
+  }
+begin
+  BaseStream.WriteBuffer(Pointer(Bytes)^, Length(Bytes));
+end;
+{$ENDIF}
+
 procedure TDataStreamWriter.WriteHex(const Value: LongInt;
   const Count: Integer);
-  {Writes hex digits to stream.
+  {Writes a hex representation of a number to stream.
     @param Value [in] Value to be written.
     @param Count [in] Number of hex digits required.
   }
@@ -222,14 +301,14 @@ begin
 end;
 
 procedure TDataStreamWriter.WriteLongInt(const Value: Integer);
-  {Write a 32 bit integer to the stream as hex digits.
+  {Writes a 32 bit integer to the stream as hex digits.
     @param Value [in] Value to be written.
   }
 begin
   WriteHex(Value, 8);
 end;
 
-procedure TDataStreamWriter.WriteSizedLongString(const Str: string);
+procedure TDataStreamWriter.WriteSizedLongString(const Str: TDataStreamString);
   {Writes a string to stream preceded by a 32 bit length as hex digits.
     @param Str [in] String to be written.
   }
@@ -238,7 +317,25 @@ begin
   WriteString(Str, Length(Str));
 end;
 
-procedure TDataStreamWriter.WriteSizedString(const Str: string);
+{$IFDEF UNICODE}
+procedure TDataStreamWriter.WriteSizedLongString(const Bytes: TBytes);
+  {Writes an array of bytes to stream as a sized string preceeded by a 32 bit
+  length as hex digits. Bytes must represent valid Latin 1 characters and must
+  not be zero.
+    @param Bytes [in] Bytes to be written.
+  }
+begin
+  WriteLongInt(Length(Bytes));
+  WriteBytes(Bytes);
+end;
+
+procedure TDataStreamWriter.WriteSizedLongString(const Str: string);
+begin
+  WriteSizedLongString(Latin1BytesOf(Str));
+end;
+{$ENDIF}
+
+procedure TDataStreamWriter.WriteSizedString(const Str: TDataStreamString);
   {Writes a string to stream preceded by a 16 bit length as hex digits.
     @param Str [in] String to be written.
   }
@@ -246,6 +343,24 @@ begin
   WriteSmallInt(Length(Str));
   WriteString(Str, Length(Str));
 end;
+
+{$IFDEF UNICODE}
+procedure TDataStreamWriter.WriteSizedString(const Bytes: TBytes);
+  {Writes an array of bytes to stream as a sized string preceeded by a 16 bit
+  length as hex digits. Bytes must represent valid Latin 1 characters and must
+  not be zero.
+    @param Bytes [in] Bytes to be written.
+  }
+begin
+  WriteSmallInt(Length(Bytes));
+  WriteBytes(Bytes);
+end;
+
+procedure TDataStreamWriter.WriteSizedString(const Str: string);
+begin
+  WriteSizedString(Latin1BytesOf(Str));
+end;
+{$ENDIF}
 
 procedure TDataStreamWriter.WriteSmallInt(const Value: SmallInt);
   {Writes a 16 bit integer to the stream as hex digits.
@@ -255,7 +370,7 @@ begin
   WriteHex(Value, 4);
 end;
 
-procedure TDataStreamWriter.WriteString(const Str: string);
+procedure TDataStreamWriter.WriteString(const Str: TDataStreamString);
   {Writes a string to stream.
     @param Str [in] String to be written.
   }
@@ -263,15 +378,33 @@ begin
   WriteString(Str, Length(Str));
 end;
 
-procedure TDataStreamWriter.WriteString(const Str: string;
+procedure TDataStreamWriter.WriteString(const Str: TDataStreamString;
   const Length: Integer);
   {Writes a fixed number of characters from a string to the stream.
     @param Str [in] String to be written.
     @param Length [in] Number of characters from string to write.
   }
 begin
-  BaseStream.WriteBuffer(Str[1], Length);
+  Assert(SizeOf(AnsiChar) = 1,
+    ClassName + '.WriteString: SizeOf(AnsiChar) <> 1');
+  BaseStream.WriteBuffer(Pointer(Str)^, Length);
 end;
+
+{$IFDEF UNICODE}
+procedure TDataStreamWriter.WriteString(const Str: string);
+begin
+  WriteString(Str, Length(Str));
+end;
+
+procedure TDataStreamWriter.WriteString(const Str: string;
+  const Length: Integer);
+var
+  Bytes: TBytes;
+begin
+  Bytes := Latin1BytesOf(Str);
+  BaseStream.WriteBuffer(Pointer(Bytes)^, Length);
+end;
+{$ENDIF}
 
 end.
 
