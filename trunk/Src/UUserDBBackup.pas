bf6a3bf76b954dc61608828f27308202feaea214
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-02-08 20:59:50 +0000

    Updated copyright date where required on some files modified in 2010.

--- trunk/Src/UUserDBBackup.pas
+++ trunk/Src/UUserDBBackup.pas
@@ -23,7 +23,7 @@
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2008-2009 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2008-2010 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributor(s)
