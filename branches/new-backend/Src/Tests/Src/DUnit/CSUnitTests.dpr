ef9aa49930cd0ce5e7930c9cbe1cd0fa507f15dc
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-03-19 14:53:22 +0000

    * Added build instructions to header comments. * Added placeholders for Rev and Date svn:keyword placeholders.

--- branches/new-backend/Src/Tests/Src/DUnit/CSUnitTests.dpr
+++ branches/new-backend/Src/Tests/Src/DUnit/CSUnitTests.dpr
@@ -1,17 +1,39 @@
-{ TODO : re-comment this file }
-{ TODO : tweak project file like md5 test program }
+{
+ * Delphi DUnit Test Project for CodeSnip
+ * --------------------------------------
+ *
+ * This project provides a DUnit test framework for some of the source code from
+ * the CodeSnip project. It can be compiled as either a GUI or Console
+ * application.
+ *
+ * Console Application
+ * ~~~~~~~~~~~~~~~~~~~
+ *
+ * The CONSOLE_TESTRUNNER symbol must be defined to create a console
+ * application. From the IDE define the symbol in the conditional defines entry
+ * in project options. To compile from the command line pass the -D switch to
+ * the compiler:
+ *
+ *   DCC32 -DCONSOLE_TESTRUNNER -B CSUnitTests
+ *
+ * GUI Application
+ * ~~~~~~~~~~~~~~~
+ *
+ * The CONSOLE_TESTRUNNER symbol must not be defined to compile a GUI
+ * application. From the IDE make sure the symbol is removed from the
+ * conditional defines entry in project options if necessary. To compile from
+ * the command line do this:
+ *
+ *   DCC32 -B CSUnitTests
+ *
+ * -----------------------------------------------------------------------------
+ * $Rev$
+ * $Date$
+ * -----------------------------------------------------------------------------
+}
+
 
 program CSUnitTests;
-{
-
-Delphi DUnit Test Project
--------------------------
-This project contains the DUnit test framework and the GUI/Console test runners.
-Add "CONSOLE_TESTRUNNER" to the conditional defines entry in the project options
-to use the console test runner.  Otherwise the GUI test runner will be used by
-default.
-
-}
 
 {$IFDEF CONSOLE_TESTRUNNER}
 {$APPTYPE CONSOLE}
@@ -24,7 +46,6 @@ uses
   TextTestRunner,
   UBaseObjects in '..\..\..\UBaseObjects.pas',
   TestUBaseObjects in 'TestUBaseObjects.pas';
-
 {$R *.RES}
 
 begin
@@ -32,7 +53,7 @@ begin
   if IsConsole then
     with TextTestRunner.RunRegisteredTests do
       Free
-  else
-    GUITestRunner.RunRegisteredTests;
+    else
+      GUITestRunner.RunRegisteredTests;
 end.
 
