4cb8d1e4af81d726b1cd90f7ce3e5091a687fdc5
Peter Johnson <delphidabbler@users.noreply.github.com>
2010-06-15 18:12:33 +0000

    * Removed now unused TIntegerList class. * Replaced calls to FreeAndNil with direct calls to objects' Free methods.

--- Src/ULists.pas
+++ Src/ULists.pas
@@ -57,7 +57,7 @@ type
       fIndex: Integer;    // Index of current object in enumeration
   public
     constructor Create(const List: TObjectList);
-      {Class constructor. Initialises enumeration.
+      {Constructor. Initialises enumeration.
         @param List [in] Object to be enurmerated.
       }
     function GetCurrent: TObject;
@@ -91,111 +91,6 @@ type
       {Gets an enumerator for the list.
         @return Required enumerator.
       }
-  end;
-
-  {
-  TIntegerList:
-    Maintains a list of integers and associated objects.
-  }
-  TIntegerList = class(TObject)
-  strict private
-    type
-      {
-      TListItem:
-        Wrapper object for items stored in list.
-      }
-      TListItem = class(TObject)
-      strict private
-        var
-          fValue: Integer;  // Value of Value property
-          fObj: TObject;    // Value of Obj property
-      public
-        constructor Create(const Value: Integer; const Obj: TObject = nil);
-          {Class constructor. Sets up object represent an integer and any
-          associated object.
-          }
-        property Value: Integer read fValue;
-          {Integer value of list item}
-        property Obj: TObject read fObj;
-          {Object associated with Value, or nil if no such object}
-      end;
-    type
-      {
-      TEnumerator:
-        List enumerator.
-      }
-      TEnumerator = class(TObject)
-      strict private
-        var
-          fIndex: Integer;      // Index of current item in enumeration
-          fList: TIntegerList;  // List being eumerated
-      public
-        function GetCurrent: Integer;
-          {Gets current integer in enumeration.
-            @return Current integer.
-          }
-        function MoveNext: Boolean;
-          {Moves to next item in enumeration.
-            @return True if there is a next item, False if enumeration
-              completed.
-          }
-        property Current: Integer read GetCurrent;
-          {Current item in enumeration}
-        constructor Create(const List: TIntegerList);
-          {Class constructor. Sets up enumerator for a list.
-            @param List [in] List to be enumerated.
-          }
-      end;
-    var
-      fItems: TObjectListEx;  // Stores list items
-    function GetItem(Idx: Integer): Integer;
-      {Read accessor for Items[] property.
-        @param Idx [in] Index of required item.
-        @return Requested integer.
-      }
-    function GetCount: Integer;
-      {Read accessor for Count property.
-        @return Number of items in list.
-      }
-    function GetObject(Idx: Integer): TObject;
-      {Read accessor for Objects[] property.
-        @param Idx [in] Index of required object.
-        @return Requested object reference.
-      }
-  public
-    constructor Create;
-      {Class constructor. Sets up object.
-      }
-    destructor Destroy; override;
-      {Class destructor Tears down object.
-      }
-    function Add(const Value: Integer; const Obj: TObject = nil): Integer;
-      {Adds an integer with optional associated object to the list.
-        @param Value [in] Integer to be added.
-        @param Obj [in] Object associated with integer or nil.
-        @return Index of new list item.
-      }
-    function IndexOf(const Value: Integer): Integer;
-      {Finds index of an integer in list.
-        @param Value [in] Value to be found.
-        @return Index of Value in list, or -1 if not found.
-      }
-    function FindObject(const Value: Integer): TObject;
-      {Finds object associated with a integer in list.
-        @param Value [in] Integer value assoicated with object.
-        @return Required object reference or nil if not found or no object
-          associated with Value.
-      }
-    function GetEnumerator: TEnumerator;
-      {Gets an enumerator for the list.
-        @return Enumerator instance.
-      }
-    property Items[Idx: Integer]: Integer read GetItem; default;
-      {Accesses all integers in list by index}
-    property Objects[Idx: Integer]: TObject read GetObject;
-      {Accesses all associated objects in list by index}
-    property Count: Integer read GetCount;
-      {Number of items in list}
   end;
 
   {
@@ -246,15 +141,14 @@ type
   public
     constructor Create(const OwnsObjects: Boolean;
       const CompareFn: TObjectCompare);
-      {Class constructor. Sets up object.
+      {Constructor. Sets up object.
         @param OwnsObjects [in] Flag indicating if list owns the objects added
           to it. When True objects are freed when list is freed. When False
           objects are not freed.
         @param CompareFn [in] Method used to compare objects in list.
       }
     destructor Destroy; override;
-      {Class detructor. Tears down object. Frees object in list if list owns
-      objects.
+      {Destructor. Tears down object. Frees object in list if list owns objects.
       }
     function Add(const Obj: TObject): Integer;
       {Adds an object to the list, maintaining list's sort order.
@@ -320,7 +214,7 @@ type
   public
     constructor Create(const KeyCompare: TObjectCompare;
       const OwnsKeys: Boolean = False; const OwnsValues: Boolean = False);
-      {Class constructor. Sets up object.
+      {Constructor. Sets up object.
         @param KeyCompare [in] Method used to compare key objects in list.
         @param OwnsKeys [in] Whether dictionary owns key objects and frees them
           when deleted. If True no object may be used more than once, either as
@@ -330,8 +224,7 @@ type
           as a key or a value, otherwise an access violation may occur.
       }
     destructor Destroy; override;
-      {Class destructor. Tears down object and frees keys and / or values if
-      required.
+      {Destructor. Tears down object and frees keys and / or values if required.
       }
     function Add(const Key, Value: TObject): Integer;
       {Adds a key/value pair to the dictionary, maintaining key sort order.
@@ -365,8 +258,6 @@ implementation
 
 
 uses
-  // Delphi
-  SysUtils,
   // Project
   UExceptions;
 
@@ -374,7 +265,7 @@ uses
 { TObjectListEnum }
 
 constructor TObjectListEnum.Create(const List: TObjectList);
-  {Class constructor. Initialises enumeration.
+  {Constructor. Initialises enumeration.
     @param List [in] Object to be enurmerated.
   }
 begin
@@ -428,146 +319,6 @@ begin
   Result := Count = 0;
 end;
 
-{ TIntegerList }
-
-function TIntegerList.Add(const Value: Integer; const Obj: TObject): Integer;
-  {Adds an integer with optional associated object to the list.
-    @param Value [in] Integer to be added.
-    @param Obj [in] Object associated with integer or nil.
-    @return Index of new list item.
-  }
-begin
-  Result := fItems.Add(TListItem.Create(Value, Obj));
-end;
-
-constructor TIntegerList.Create;
-  {Class constructor. Sets up object.
-  }
-begin
-  inherited Create;
-  fItems := TObjectListEx.Create(True);
-end;
-
-destructor TIntegerList.Destroy;
-  {Class destructor Tears down object.
-  }
-begin
-  FreeAndNil(fItems); // frees owned TListItem objects
-  inherited;
-end;
-
-function TIntegerList.FindObject(const Value: Integer): TObject;
-  {Finds object associated with a integer in list.
-    @param Value [in] Integer value assoicated with object.
-    @return Required object reference or nil if not found or no object
-      associated with Value.
-  }
-var
-  Idx: Integer; // loops thru list items
-begin
-  Idx := IndexOf(Value);
-  if Idx >= 0 then
-    Result := Objects[Idx]
-  else
-    Result := nil;
-end;
-
-function TIntegerList.GetCount: Integer;
-  {Read accessor for Count property.
-    @return Number of items in list.
-  }
-begin
-  Result := fItems.Count;
-end;
-
-function TIntegerList.GetEnumerator: TEnumerator;
-  {Gets an enumerator for the list.
-    @return Enumerator instance.
-  }
-begin
-  Result := TEnumerator.Create(Self);
-end;
-
-function TIntegerList.GetItem(Idx: Integer): Integer;
-  {Read accessor for Items[] property.
-    @param Idx [in] Index of required item.
-    @return Requested integer.
-  }
-begin
-  Result := (fItems[Idx] as TListItem).Value;
-end;
-
-function TIntegerList.GetObject(Idx: Integer): TObject;
-  {Read accessor for Objects[] property.
-    @param Idx [in] Index of required object.
-    @return Requested object reference.
-  }
-begin
-  Result := (fItems[Idx] as TListItem).Obj;
-end;
-
-function TIntegerList.IndexOf(const Value: Integer): Integer;
-  {Finds index of an integer in list.
-    @param Value [in] Value to be found.
-    @return Index of Value in list, or -1 if not found.
-  }
-var
-  Idx: Integer; // loops all items in list
-begin
-  Result := -1;
-  for Idx := 0 to Pred(Count) do
-  begin
-    if Items[Idx] = Value then
-    begin
-      Result := Idx;
-      Break;
-    end;
-  end;
-end;
-
-{ TIntegerList.TListItem }
-
-constructor TIntegerList.TListItem.Create(const Value: Integer;
-  const Obj: TObject);
-  {Class constructor. Sets up object represent an integer and any associated
-  object.
-  }
-begin
-  inherited Create;
-  fValue := Value;
-  fObj := Obj;
-end;
-
-{ TIntegerList.TEnumerator }
-
-constructor TIntegerList.TEnumerator.Create(const List: TIntegerList);
-  {Class constructor. Sets up enumerator for a list.
-    @param List [in] List to be enumerated.
-  }
-begin
-  inherited Create;
-  fList := List;
-  fIndex := -1;
-end;
-
-function TIntegerList.TEnumerator.GetCurrent: Integer;
-  {Gets current integer in enumeration.
-    @return Current integer.
-  }
-begin
-  Result := fList[fIndex];
-end;
-
-function TIntegerList.TEnumerator.MoveNext: Boolean;
-  {Moves to next item in enumeration.
-    @return True if there is a next item, False if enumeration completed.
-  }
-begin
-  Result := fIndex < Pred(fList.Count);
-  if Result then
-    Inc(fIndex);
-end;
-
 { TSortedObjectList }
 
 function TSortedObjectList.Add(const Obj: TObject): Integer;
@@ -614,7 +365,7 @@ end;
 
 constructor TSortedObjectList.Create(const OwnsObjects: Boolean;
   const CompareFn: TObjectCompare);
-  {Class constructor. Sets up object.
+  {Constructor. Sets up object.
     @param OwnsObjects [in] Flag indicating if list owns the objects added to
       it. When True objects are freed when list is freed. When False objects are
       not freed.
@@ -628,11 +379,10 @@ begin
 end;
 
 destructor TSortedObjectList.Destroy;
-  {Class detructor. Tears down object. Frees object in list if list owns
-  objects.
+  {Destructor. Tears down object. Frees object in list if list owns objects.
   }
 begin
-  FreeAndNil(fItems); // frees contained objects if specified
+  fItems.Free;  // frees contained objects if specified
   inherited;
 end;
 
@@ -753,7 +503,7 @@ end;
 
 constructor TSortedObjectDictionary.Create(const KeyCompare: TObjectCompare;
   const OwnsKeys, OwnsValues: Boolean);
-  {Class constructor. Sets up object.
+  {Constructor. Sets up object.
     @param KeyCompare [in] Method used to compare key objects in list.
     @param OwnsKeys [in] Whether dictionary owns key objects and frees them
       when deleted. If True no object may be used more than once, either as
@@ -769,12 +519,11 @@ begin
 end;
 
 destructor TSortedObjectDictionary.Destroy;
-  {Class destructor. Tears down object and frees keys and / or values if
-  required.
+  {Destructor. Tears down object and frees keys and / or values if required.
   }
 begin
-  FreeAndNil(fValues);
-  FreeAndNil(fKeys);
+  fValues.Free;
+  fKeys.Free;
   inherited;
 end;
 
