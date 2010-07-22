8131be1969d6fbe9201e25fc560a8d26fd966b9a
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-07-22 00:54:12 +0000

    CodeSnip version 3.6.3

---
+++ tags/version-3.6.3/Src/NsWebServices.UMailListSubs.pas
@@ -0,0 +1,150 @@
+{
+ * NsWebServices.UMailListSubs.pas
+ *
+ * Implements class that interacts with the mailing list subscription web
+ * service to subscribe a user to the CodeSnip mailing list.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is NsWebServices.UMailListSubs.pas, formerly
+ * UMailListSubscriber.pas.
+ *
+ * The Initial Developer of the Original Code is Peter Johnson
+ * (http://www.delphidabbler.com/).
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2006-2010 Peter
+ * Johnson. All Rights Reserved.
+ *
+ * Contributor(s)
+ *   NONE
+ *
+ * ***** END LICENSE BLOCK *****
+}
+
+
+unit NsWebServices.UMailListSubs;
+
+
+interface
+
+
+uses
+  // Project
+  NsWebServices.UDDabStandard, NsWebServices.UExceptions;
+
+
+type
+
+  {
+  TMailListSubscriber:
+    Class that interacts with mailing list subscription web service to subscribe
+    user to CodeSnip mailing list.
+  }
+  TMailListSubscriber = class(TDDabStdWebService)
+  public
+    constructor Create;
+      {Class constructor. Initialises service.
+      }
+    function Subscribe(const Email, Name: string): string;
+      {Sends subscription request to web service and gathers response.
+        @param Email [in] Email address to be subscribed.
+        @param Name [in] Name of email account holder. May be ''.
+        @return Message returned from web service in response to request.
+        @except Raises EWebServiceFailure if response from web service has
+          invalid format.
+        @except Raises EWebServiceError on receipt of valid error response.
+        @except Raises EMailListSubscriber if response not in expected format.
+      }
+  end;
+
+  {
+  EMailListSubscriber:
+    Class of exception raised when errors detected in TMailListSubscriber.
+  }
+  EMailListSubscriber = class(EWebService);
+
+
+implementation
+
+
+uses
+  // Delphi
+  SysUtils, Classes,
+  // Project
+  UURIParams, UWebInfo;
+
+
+const
+  // Web service info
+  cScriptName = 'maillist-subs.php';        // script name
+  cUserAgent = 'DelphiDabbler-MLSubs-v1';   // user agent string
+
+
+resourcestring
+  // Error message
+  sBadRespone = 'Invalid response from mail list web service "subscribe" '
+    + 'command.';
+
+
+{ TMailListSubscriber }
+
+constructor TMailListSubscriber.Create;
+  {Class constructor. Initialises service.
+  }
+begin
+  inherited Create(TWebServiceInfo.Create(cScriptName, cUserAgent));
+end;
+
+function TMailListSubscriber.Subscribe(const Email, Name: string): string;
+  {Sends subscription request to web service and gathers response.
+    @param Email [in] Email address to be subscribed.
+    @param Name [in] Name of email account holder. May be ''.
+    @return Message returned from web service in response to request.
+    @except Raises EWebServiceFailure if response from web service has invalid
+      format.
+    @except Raises EWebServiceError on receipt of valid error response.
+    @except Raises EMailListSubscriber if response not in expected format.
+  }
+var
+  Response: TStringList;  // valid response from web service
+  Query: TURIParams;      // parameters for query string
+begin
+  Assert(Email <> '', ClassName + '.Subscribe: Email required');
+  // Send subscribe command to web service and gather response
+  Query := nil;
+  Response := TStringList.Create;
+  try
+    Query := TURIParams.Create;
+    Query.Add('listid', 'codesnip');
+    Query.Add('name', Name);
+    Query.Add('email', Email);
+    PostCommand('subscribe', Query, Response);
+    // Response must be at least two lines: 1st line is success status code. 2nd
+    // and subsequent lines are response message
+    if Response.Count < 2 then
+      raise EMailListSubscriber.Create(sBadRespone);
+    // Discard success status code: we don't use this
+    Response.Delete(0);
+    // Return message that follows success status code
+    Result := Trim(Response.Text);
+  finally
+    Query.Free;
+    Response.Free;
+  end;
+end;
+
+end.
+
---
+++ tags/version-3.6.3/Src/UNews.pas
@@ -0,0 +1,235 @@
+{
+ * UNews.pas
+ *
+ * Maintains a list of news items for display by program and reads list from
+ * formatted data stream.
+ *
+ * $Rev$
+ * $Date$
+ *
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * Version: MPL 1.1
+ *
+ * The contents of this file are subject to the Mozilla Public License Version
+ * 1.1 (the "License"); you may not use this file except in compliance with the
+ * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is UNews.pas
+ *
+ * The Initial Developer of the Original Code is Peter Johnson
+ * (http://www.delphidabbler.com/).
+ *
+ * Portions created by the Initial Developer are Copyright (C) 2006-2010 Peter
+ * Johnson. All Rights Reserved.
+ *
+ * Contributor(s)
+ *   NONE
+ *
+ * ***** END LICENSE BLOCK *****
+}
+
+
+unit UNews;
+
+
+interface
+
+
+uses
+  // Delphi
+  Classes, Generics.Collections,
+  // Project
+  UDataStreamIO;
+
+
+type
+
+  {
+  TNewsItem:
+    Encapsulates a news item. Provides properties to describe it and a means
+    of creating object from a formatted data stream.
+  }
+  TNewsItem = class(TObject)
+  strict private
+    var
+      fId: SmallInt;    // Value of Id property
+      fDate: TDateTime; // Value of Date property
+      fHTML: string;    // Value of HTML property
+  public
+    constructor Create(const Reader: TDataStreamReader);
+      {Constructor. Creates news item object from a data stream.
+        @param Reader [in] Object used to read/parse data stream.
+      }
+    property Id: SmallInt read fId;
+      {Unique identifier of news item}
+    property Date: TDateTime read fDate;
+      {Date news item published}
+    property HTML: string read fHTML;
+      {News item content as HTML}
+  end;
+
+  {
+  TNews:
+    Stores news items for display in application. Parses news items from a
+    data stream supplied to constructor.
+  }
+  TNews = class(TObject)
+  strict private
+    type
+      // Implements list of news items
+      TNewsList = TObjectList<TNewsItem>;
+    var
+      fNewsItems: TNewsList;  // Stores list of news items
+    procedure Parse(const NewsData: TStream);
+      {Creates list of news items from a data stream.
+        @param NewsData [in] Data stream containing news items.
+      }
+    function GetCount: Integer;
+      {Read accessor for Count property.
+        @return Number of news items in list.
+      }
+    function GetItem(Idx: Integer): TNewsItem;
+      {Read accessor for Items[] property.
+        @param Idx [in] Index of required item.
+        @return Requested news item.
+      }
+  public
+    constructor Create(const NewsData: TStream);
+      {Constructor. Creates list of news items described by a data stream.
+        @param NewsData [in] Stream containing news items.
+      }
+    destructor Destroy; override;
+      {Destructor. Tears down object.
+      }
+    function GetEnumerator: TEnumerator<TNewsItem>;
+      {Creates an enumerator for this object.
+        @return Reference to new enumerator. Caller is repsonsible for freeing
+          this object.
+      }
+    property Count: Integer read GetCount;
+      {Number of news items in list}
+    property Items[Idx: Integer]: TNewsItem read GetItem; default;
+      {Array of news items}
+  end;
+
+
+implementation
+
+
+uses
+  // Project
+  UUtils;
+
+
+{
+  News data stream format
+  -----------------------
+
+  Stream comprises text characters. Numbers are encoded in hex format.
+
+  File format is:
+
+    Item-count: SmallInt    - number of news items in stream
+
+  followed by Item-count news item records:
+
+    Id: SmallInt            - unique item identifier
+    Date: SizedString       - publication date as MySQL date (YYYY-MM-DD)
+    Content: SizedString    - news item content as HTML
+
+  Data types are:
+
+    SmallInt      - 16 bit integer encoded as 4 hex digits
+    SizedString   - SmallInt specifying string length followed by specified
+                    number of characters
+}
+
+
+{ TNews }
+
+constructor TNews.Create(const NewsData: TStream);
+  {Constructor. Creates list of news items described by a data stream.
+    @param NewsData [in] Stream containing news items.
+  }
+begin
+  inherited Create;
+  fNewsItems := TNewsList.Create(True);
+  Parse(NewsData);
+end;
+
+destructor TNews.Destroy;
+  {Destructor. Tears down object.
+  }
+begin
+  fNewsItems.Free;  // frees news items in list
+  inherited;
+end;
+
+function TNews.GetCount: Integer;
+  {Read accessor for Count property.
+    @return Number of news items in list.
+  }
+begin
+  Result := fNewsItems.Count;
+end;
+
+function TNews.GetEnumerator: TEnumerator<TNewsItem>;
+  {Creates an enumerator for this object.
+    @return Reference to new enumerator. Caller is repsonsible for freeing this
+      object.
+  }
+begin
+  Result := fNewsItems.GetEnumerator;
+end;
+
+function TNews.GetItem(Idx: Integer): TNewsItem;
+  {Read accessor for Items[] property.
+    @param Idx [in] Index of required item.
+    @return Requested news item.
+  }
+begin
+  Result := fNewsItems[Idx];
+end;
+
+procedure TNews.Parse(const NewsData: TStream);
+  {Creates list of news items from a data stream.
+    @param NewsData [in] Data stream containing news items.
+  }
+var
+  Reader: TDataStreamReader;  // object used to interpret data stream
+  NumItems: SmallInt;         // number of news items to create
+  Idx: Integer;               // loops thru news items
+begin
+  Reader := TDataStreamReader.Create(NewsData);
+  try
+    // Get number of items in data stream
+    NumItems := Reader.ReadSmallInt;
+    // Create required number of news items from stream
+    for Idx := 1 to NumItems do
+      fNewsItems.Add(TNewsItem.Create(Reader))
+  finally
+    Reader.Free;
+  end;
+end;
+
+{ TNewsItem }
+
+constructor TNewsItem.Create(const Reader: TDataStreamReader);
+  {Constructor. Creates news item object from a data stream.
+    @param Reader [in] Object used to read/parse data stream.
+  }
+begin
+  inherited Create;
+  // Get property values from stream. Order is important
+  fId := Reader.ReadSmallInt;
+  fDate := MySQLDateToDateTime(string(Reader.ReadSizedString));
+  fHTML := string(Reader.ReadSizedString);
+end;
+
+end.
+
