42318dc443537e176ef613bd120116d140d402cd
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-09-15 23:35:10 +0000

    * Fixed memory leak in IStringList instances. * Replaced a numeric literal with constant.

--- trunk/Src/UCodeImportExport.pas
+++ trunk/Src/UCodeImportExport.pas
@@ -149,7 +149,7 @@ type
         @return List containing names of all snippets names.
       }
     procedure WriteReferenceList(const ParentNode: IXMLNode;
-      const ListNodeName: string; const PasNames: IStringList);
+      const ListNodeName: string; PasNames: IStringList);
       {Writes a XML node that contains a list of pascal names.
         @param ParentNode [in] Node under which this list node is to be created.
         @param ListNodeName [in] Tag name of list node.
@@ -313,7 +313,7 @@ procedure TCodeExporter.Execute;
     end;
     // Minimum export file version number is at least 3, and 4 if extra property
     // contains data that requires later version of REML
-    if MinREMLVer < 3 then
+    if MinREMLVer < cMinOutputVersion then
       Result := cMinOutputVersion
     else
       Result := cVersion4;
@@ -419,7 +419,7 @@ begin
 end;
 
 procedure TCodeExporter.WriteReferenceList(const ParentNode: IXMLNode;
-  const ListNodeName: string; const PasNames: IStringList);
+  const ListNodeName: string; PasNames: IStringList);
   {Writes a XML node that contains a list of pascal names.
     @param ParentNode [in] Node under which this list node is to be created.
     @param ListNodeName [in] Tag name of list node.
@@ -522,7 +522,7 @@ procedure TCodeImporter.Execute;
   }
 
   // ---------------------------------------------------------------------------
-  procedure GetUnits(const RoutineNode: IXMLNode; const Units: IStringList);
+  procedure GetUnits(const RoutineNode: IXMLNode; Units: IStringList);
     {Gets a list of units required by a snippet.
       @param RoutineNode [in] Node of snippet for which units are required.
       @param Units [in] Receives list of unit names. Cleared if there are no
