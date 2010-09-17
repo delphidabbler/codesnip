ec401260f995d511fe1d0abd94c3dd9b618d3315
Peter Johnson <delphidabbler@users.noreply.github.com>
2010-09-17 19:27:53 +0000

    Added new Web.UXMLRequestor.pas unit for downloading and parsing XML documents.

--- Src/CodeSnip.dpr
+++ Src/CodeSnip.dpr
@@ -361,7 +361,8 @@ uses
   Web.UExceptions in 'Web.UExceptions.pas',
   Web.UHTTPEx in 'Web.UHTTPEx.pas',
   Web.URegistrar in 'Web.URegistrar.pas',
-  Web.UStdWebService in 'Web.UStdWebService.pas';
+  Web.UStdWebService in 'Web.UStdWebService.pas',
+  Web.UXMLRequestor in 'Web.UXMLRequestor.pas';
 
 // Include compiler defines: checks if compiler supported
 {$Include CompilerDefines.inc}
