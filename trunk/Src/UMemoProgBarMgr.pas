838060384b95c9e12974d1814f5e2d53138551ac
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-09-25 01:37:33 +0000

    Corrected copyright date range to be in line with date of last revision.

--- trunk/Src/UMemoProgBarMgr.pas
+++ trunk/Src/UMemoProgBarMgr.pas
@@ -24,7 +24,7 @@
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2006-2009 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2006-2010 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributor(s)
