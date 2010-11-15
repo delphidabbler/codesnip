71802fbc8bd524faddc83ab285ed15d0f2e99134
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2010-11-15 17:42:36 +0000

    Renamed UWBNulDropTarget unit and its TWBNulDropTarget class to UNulDropTarget and TNulDropTarget respectively. Updated using units accordingly.

--- trunk/Src/UNulDropTarget.pas
+++ trunk/Src/UNulDropTarget.pas
@@ -1,8 +1,8 @@
 {
- * UWBNulDropTarget.pas
+ * UNulDropTarget.pas
  *
- * Contains class that implements IDropTarget interface that is designed to be
- * associated with a web browser control to inhibit drag and drop operations.
+ * Contains class that implements the IDropTarget interface that is designed to
+ * inhibit all drag and drop operations.
  *
  * $Rev$
  * $Date$
@@ -19,12 +19,12 @@
  * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
  * the specific language governing rights and limitations under the License.
  *
- * The Original Code is UWBNulDropTarget.pas
+ * The Original Code is UNulDropTarget.pas, formerly UWBNulDropTarget.pas
  *
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2006-2009 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2006-2010 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributor(s)
@@ -34,7 +34,7 @@
 }
 
 
-unit UWBNulDropTarget;
+unit UNulDropTarget;
 
 
 interface
@@ -48,11 +48,11 @@ uses
 type
 
   {
-  TWBDropTarget:
-    Implementation of IDropTarget designed to be associated with a web browser
-    control to inhibit drag and drop operations.
+  TNulDropTarget:
+    Implementation of IDropTarget designed to inhibit all drag and drop
+    operations.
   }
-  TWBNulDropTarget = class(TInterfacedObject,
+  TNulDropTarget = class(TInterfacedObject,
     IDropTarget
   )
   protected
@@ -98,9 +98,9 @@ type
 implementation
 
 
-{ TWBNulDropTarget }
+{ TNulDropTarget }
 
-function TWBNulDropTarget.DragEnter(const dataObj: IDataObject;
+function TNulDropTarget.DragEnter(const dataObj: IDataObject;
   grfKeyState: Integer; pt: TPoint; var dwEffect: Integer): HResult;
   {Determines whether a drop can be accepted and its effect if it is accepted.
   We indicate drops not accepted and do nothing else.
@@ -116,7 +116,7 @@ begin
   Result := S_OK;
 end;
 
-function TWBNulDropTarget.DragLeave: HResult;
+function TNulDropTarget.DragLeave: HResult;
   {Removes target feedback and releases the data object. We do nothing.
     @return S_OK if method completes successfully or error values otherwise.
   }
@@ -124,7 +124,7 @@ begin
   Result := S_OK;
 end;
 
-function TWBNulDropTarget.DragOver(grfKeyState: Integer; pt: TPoint;
+function TNulDropTarget.DragOver(grfKeyState: Integer; pt: TPoint;
   var dwEffect: Integer): HResult;
   {Provides target feedback to the user as mouse moves over drop window. We
   indicate that drop is not accepted.
@@ -139,7 +139,7 @@ begin
   Result := S_OK;
 end;
 
-function TWBNulDropTarget.Drop(const dataObj: IDataObject; grfKeyState: Integer;
+function TNulDropTarget.Drop(const dataObj: IDataObject; grfKeyState: Integer;
   pt: TPoint; var dwEffect: Integer): HResult;
   {Called when user drops data object on drop window. Incorporates source data
   into target window, removes target feedback and releases data object. In this
