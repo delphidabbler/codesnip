9a5870992b775efcf616f25142de66d03745b4fa
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-05-18 12:38:05 +0000

    XMLDoc commented unit.

--- branches/v4-dev/Src/UCodeImportMgr.pas
+++ branches/v4-dev/Src/UCodeImportMgr.pas
@@ -48,56 +48,120 @@ uses
 
 
 type
-
+  ///  <summary>
+  ///  Stores information about if and how a snippet is to be imported.
+  ///  </summary>
   TImportInfo = record
   strict private
+    // Property values
     fOrigName: string;
     fImportAsName: string;
     fSkip: Boolean;
   public
+    ///  <summary>Initialises properties to given values.</summary>
     constructor Create(const AOrigName, AImportAsName: string;
       const ASkip: Boolean = False);
+    ///  <summary>Name of snippet per import file.</summary>
     property OrigName: string read fOrigName;
+    ///  <summary>Name of snippet to be used when updating database.</summary>
+    ///  <remarks>Can be changed by user.</remarks>
     property ImportAsName: string read fImportAsName write fImportAsName;
+    ///  <summary>Flag indicating if snippet is to be skipped (ignored) when
+    ///  updating database.</summary>
     property Skip: Boolean read fSkip write fSkip;
   end;
 
+type
+  ///  <summary>
+  ///  List of TImportInfo records. Designed to store descriptions of how to
+  ///  import each snippet in an import file.
+  ///  </summary>
   TImportInfoList = class(TList<TImportInfo>)
   public
+    ///  <summary>Constructs list with appropriate comparer.</summary>
     constructor Create;
+    ///  <summary>Finds a record based on its OrigName field value.</summary>
+    ///  <param name="Name">string [in] Name to be found.</param>
+    ///  <param name="ImportInfo">TImportInfo [out] Found record. Undefined if
+    ///  Name not found.</param>
+    ///  <returns>Boolean: True if Name found, False if not.</returns>
     function FindByName(const Name: string; out ImportInfo: TImportInfo):
       Boolean;
+    ///  <summary>Returns index of record in list whose OrigName field matches
+    ///  given name or -1 if name not found.</summary>
     function IndexOfName(const Name: string): Integer;
   end;
 
-  {
-  TCodeImportMgr:
-    Sealed class that manages import of a codesnip export file into the
-    user-defined database. Designed for ease of interaction with a suitable UI.
-  }
+type
+  ///  <summary>
+  ///  Manages import of a codesnip export file into the user-defined database.
+  ///  </summary>
+  ///  <remarks>
+  ///  Designed for ease of interaction with a suitable UI.
+  ///  </remarks>
   TCodeImportMgr = class sealed(TObject)
   strict private
     var
-      fSnippetInfoList: TSnippetInfoList; // todo: change to proper list
+      ///  <summary>List of snippet information read from import file.</summary>
+      // TODO: Change from array to list object
+      fSnippetInfoList: TSnippetInfoList;
+      ///  <summary>Value of ImportInfo property.</summary>
       fImportInfoList: TImportInfoList;
+      ///  <summary>Value of UserInfo property.</summary>
       fUserInfo: TUserInfo;
+    ///  <summary>Initialises data before importing.</summary>
+    // TODO: Replace single call with content of method and remove method
     procedure Initialise;
+    ///  <summary>Initialises import information list with details of snippets
+    ///  read from import file.</summary>
+    // TODO: Rename this method
     procedure UpdateImportInfo;
+    ///  <summary>Returns list of names that can't be used to rename an imported
+    ///  snippet.</summary>
+    ///  <param name="ExcludedName">string [in] Name of snippet to be excluded
+    ///  from import list.</param>
+    ///  <returns>IStringList: List of disallowed snippet names.</returns>
+    ///  <remarks>List is made up of all names of snippets in user database plus
+    ///  names of all imported snippets except for ExcludedName. ExcludedName
+    ///  should be the name of a snippet being renamed.</remarks>
     function DisallowedNames(const ExcludedName: string): IStringList;
+    ///  <summary>Returns a name for snippet SnippetName that does not already
+    ///  exist in user database or imported snippet list.</summary>
+    ///  <remarks>
+    ///  <para>If SnippetName is not in user database then it is returned
+    ///  unchanged.</para>
+    ///  <para>If SnippetName is in user database then numbers are appended
+    ///  sequentially until a unique name is found.</para>
+    ///  </remarks>
     function GetUniqueSnippetName(const SnippetName: string): string;
   public
+    ///  <summary>Constructor. Sets up object.</summary>
     constructor Create;
+    ///  <summary>Destructor. Tears down object.</summary>
     destructor Destroy; override;
+    ///  <summary>Imports snippet info from file without updating database.
+    ///  </summary>
+    ///  <remarks>ImportInfo list property is initialised ready for
+    ///  customisation.</remarks>
     procedure Import(const FileName: string);
+    ///  <summary>Updates database based on imported snippets and customisation
+    ///  described by ImportInfo property.</summary>
+    ///  <remarks>Any existing snippets with same name as imported snippets are
+    ///  overwritten.</remarks>
     procedure UpdateDatabase;
+    ///  <summary>Information about user who created the import file.</summary>
+    ///  <remarks>May be null if no user info included in import file.</remarks>
     property UserInfo: TUserInfo read fUserInfo;
+    ///  <summary>List of information describing if and how to import snippets
+    ///  in import file. Permits customisation of import.</summary>
     property ImportInfo: TImportInfoList read fImportInfoList;
   end;
 
-  {
-  ECodeImportMgr:
-    Class of exception raised when import manager encounteres an expected error.
-  }
+type
+  ///  <summary>
+  ///  Class of exception raised when import manager encounters an anticipated
+  ///  error.
+  ///  </summary>
   ECodeImportMgr = class(ECodeSnip);
 
 
@@ -130,8 +194,8 @@ end;
 function TCodeImportMgr.DisallowedNames(const ExcludedName: string):
   IStringList;
 var
-  Snippet: TSnippet;
-  SnippetInfo: TSnippetInfo;
+  Snippet: TSnippet;          // each snippet in user database
+  SnippetInfo: TSnippetInfo;  // info about each imported snippet
 begin
   Result := TIStringList.Create;
   Result.CaseSensitive := False;
@@ -146,8 +210,8 @@ end;
 function TCodeImportMgr.GetUniqueSnippetName(
   const SnippetName: string): string;
 var
-  UsedNames: IStringList;
-  Postfix: Cardinal;
+  UsedNames: IStringList; // list of snippet names in use
+  Postfix: Cardinal;      // number to be appended to name to make unique
 begin
   UsedNames := DisallowedNames(SnippetName);
   if not UsedNames.Contains(SnippetName) then
@@ -161,7 +225,7 @@ end;
 
 procedure TCodeImportMgr.Import(const FileName: string);
 var
-  Data: TBytes;
+  Data: TBytes; // content of import file as bytes
 begin
   Initialise;
   fImportInfoList.Clear;
@@ -185,11 +249,9 @@ end;
 procedure TCodeImportMgr.UpdateDatabase;
 
   // ---------------------------------------------------------------------------
+  // Adjusts a snippet's dependency list so that main database is searched for a
+  // required snippet if it is not in the user database.
   procedure AdjustDependsList(const Depends: ISnippetIDList);
-    {Adjusts a snippet's dependency list so that main database is searched for
-    a required snippet if it is not in the user database.
-      @param Depends [in] Dependency list to be adjusted.
-    }
   var
     Idx: Integer;           // loops through dependencies
     SnippetID: TSnippetID;  // each snippet ID in dependency list
@@ -206,11 +268,9 @@ procedure TCodeImportMgr.UpdateDatabase;
     end;
   end;
 
+  ///  Builds an active text representation of the contributing user's name and
+  ///  email address.
   function UserInfoActiveText: IActiveText;
-    {Builds an active text representation of the contributing user's name and
-    email address.
-      @return Required active text representation.
-    }
   resourcestring
     // user information text template
     sContributorText = 'Contributed by: %0:s <%1:s>';
@@ -231,12 +291,12 @@ procedure TCodeImportMgr.UpdateDatabase;
   // ---------------------------------------------------------------------------
 
 var
-//  Idx: Integer;           // loops through all snippets
-  Editor: IDatabaseEdit;  // object used to update user database
-  Snippet: TSnippet;      // reference to existing snippets
-  SnippetInfo: TSnippetInfo;
-  ImportInfo: TImportInfo;
+  Editor: IDatabaseEdit;      // object used to update user database
+  Snippet: TSnippet;          // reference any existing snippet to overwrite
+  SnippetInfo: TSnippetInfo;  // info about each snippet from import file
+  ImportInfo: TImportInfo;    // info about how / whether to import a snippet
 resourcestring
+  // Error message
   sBadNameError = 'Can''t find snippet "%s" in import data';
 begin
   Editor := Database as IDatabaseEdit;
@@ -263,7 +323,7 @@ end;
 
 procedure TCodeImportMgr.UpdateImportInfo;
 var
-  SnippetInfo: TSnippetInfo;
+  SnippetInfo: TSnippetInfo;  // info about each snippet in import file
 begin
   fImportInfoList.Clear;
   for SnippetInfo in fSnippetInfoList do
@@ -292,6 +352,7 @@ constructor TImportInfoList.Create;
 begin
   { TODO: create a CompareNames class static function on TSnippetID to simply
           compare names with having to create a TSnippetID record }
+  { TODO: can also create a comparer class for TSnippetID and snippet names }
   inherited Create(
     TDelegatedComparer<TImportInfo>.Create(
       function(const Left, Right: TImportInfo): Integer
@@ -307,7 +368,7 @@ end;
 function TImportInfoList.FindByName(const Name: string;
   out ImportInfo: TImportInfo): Boolean;
 var
-  Idx: Integer;
+  Idx: Integer;   // index of named snippet in list
 begin
   Idx := IndexOf(TImportInfo.Create(Name, ''));
   if Idx = -1 then
