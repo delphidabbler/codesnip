9011aba7b40bcbde209de42c86ce204927740e42
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-05-20 19:25:28 +0000

    Removed unused unit reference.

--- branches/v4-dev/Src/FrPrintingPrefs.pas
+++ branches/v4-dev/Src/FrPrintingPrefs.pas
@@ -24,7 +24,7 @@
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2007-2010 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2007-2011 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributor(s)
@@ -113,7 +113,7 @@ implementation
 
 uses
   // Delphi
-  SysUtils, StrUtils, Windows, Graphics, Math, ComCtrls,
+  SysUtils, Windows, Graphics, Math, ComCtrls,
   // Project
   FmPreferencesDlg, Hiliter.UAttrs, Hiliter.UHiliters, IntfCommon, UConsts,
   UEncodings, UKeysHelper, UPrintInfo, URTFBuilder, URTFUtils, UUtils;
