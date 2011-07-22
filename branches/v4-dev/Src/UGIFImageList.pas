07ffb0cecc0795312daa625d8486e4b672aea749
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-07-22 19:58:43 +0000

    Updated copyright date to include 2011.

--- branches/v4-dev/Src/UGIFImageList.pas
+++ branches/v4-dev/Src/UGIFImageList.pas
@@ -27,7 +27,7 @@
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2008-2010 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2008-2011 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributor(s)
