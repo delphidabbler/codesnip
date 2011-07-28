7cdddf620486b9a70c130907b7ac022e033617cf
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-07-28 20:55:18 +0000

    Various refactorings: * Extract method. * Extract field. * Moved event types into THilitePasParser. * Removed some Boolean flags used to indicate state and replaced by state set. Tidied code following above changes.

--- branches/v4-dev/Src/Hiliter.UPasParser.pas
+++ branches/v4-dev/Src/Hiliter.UPasParser.pas
@@ -41,36 +41,18 @@ interface
 
 
 uses
-  // Delphi
-  Classes,
   // Project
-  Hiliter.UGlobals;
+  Hiliter.UGlobals, Hiliter.UPasLexer;
 
 
 type
 
-  THilitePasParser = class;
+  TParserState = (
+    psBetweenLines,
+    psASM
+  );
 
-  {
-  TParseElementEvent:
-    Type of event raised when a code element has been parsed that needs to be
-    highlighted in a particular style. Should be handled by classes that render
-    the highlighted text.
-      @param Parser [in] Parser object that triggered event.
-      @param Element [in] Identifies highlight style the text should be rendered
-        in.
-      @param ElemText [in] Text to be rendered.
-  }
-  TParseElementEvent = procedure(Parser: THilitePasParser;
-    Element: THiliteElement; const ElemText: string) of object;
-
-  {
-  TParseLineEvent:
-    Type of event raised at start and end of lines of highlighted code when
-    parsing a Pascal file for highlighting.
-      @param Parser [in] Parser object that triggered event.
-  }
-  TParseLineEvent = procedure(Parser: THilitePasParser) of object;
+  TParserStates = set of TParserState;
 
   {
   THilitePasParser:
@@ -78,14 +60,43 @@ type
     Notifies owning objects of each element and other key parsing events.
   }
   THilitePasParser = class(TObject)
+  public
+    type
+      {
+      TParseElementEvent:
+        Type of event raised when a code element has been parsed that needs to
+        be highlighted in a particular style. Should be handled by classes that
+        render the highlighted text.
+          @param Parser [in] Parser object that triggered event.
+          @param Element [in] Identifies highlight style the text should be
+            rendered in.
+          @param ElemText [in] Text to be rendered.
+      }
+      TParseElementEvent = procedure(Parser: THilitePasParser;
+        Element: THiliteElement; const ElemText: string) of object;
+      {
+      TParseLineEvent:
+        Type of event raised at start and end of lines of highlighted code when
+        parsing a Pascal file for highlighting.
+          @param Parser [in] Parser object that triggered event.
+      }
+      TParseLineEvent = procedure(Parser: THilitePasParser) of object;
   strict private
+    fLexer: THilitePasLexer;   // object that tokenises Pascal source
     fOnElement: TParseElementEvent;
       {OnElement event handler}
     fOnLineBegin: TParseLineEvent;
       {OnLineBegin event handler}
     fOnLineEnd: TParseLineEvent;
       {OnLineEnd event handler}
-  protected
+    fState: TParserStates;
+    function ParseASMElement: THiliteElement;
+    function ParsePascalElement: THiliteElement;
+    function IsTokenStr(const TokenStr: string): Boolean; inline;
+    procedure EnsureLineBegun;
+    procedure EmitEndOfLine;
+    procedure EmitElement(const Elem: THiliteElement);
+  strict protected
     procedure DoElement(Elem: THiliteElement; const ElemText: string); virtual;
       {Triggers OnElement event.
         @param Elem [in] Element that has been parsed.
@@ -98,6 +109,7 @@ type
       {Triggers OnLineEnd event.
       }
   public
+    destructor Destroy; override;
     procedure Parse(const Source: string);
       {Parses Pascal source code, triggering events as each line and token is
       parsed.
@@ -122,13 +134,17 @@ implementation
 
 
 uses
-  // Delphi
-  Windows {for inlining},
   // Project
-  Hiliter.UPasLexer, UStrUtils;
+  UStrUtils;
 
 
 { THilitePasParser }
+
+destructor THilitePasParser.Destroy;
+begin
+  fLexer.Free;
+  inherited;
+end;
 
 procedure THilitePasParser.DoElement(Elem: THiliteElement;
   const ElemText: string);
@@ -157,104 +173,127 @@ begin
     fOnLineEnd(Self);
 end;
 
+procedure THilitePasParser.EmitElement(const Elem: THiliteElement);
+begin
+  EnsureLineBegun;
+  DoElement(Elem, fLexer.TokenStr);
+end;
+
+procedure THilitePasParser.EmitEndOfLine;
+begin
+  // if we never started this line do it or we may skip blank line
+  EnsureLineBegun;
+  DoLineEnd;
+  Include(fState, psBetweenLines);
+end;
+
+procedure THilitePasParser.EnsureLineBegun;
+begin
+  if psBetweenLines in fState then
+  begin
+    DoLineBegin;
+    Exclude(fState, psBetweenLines);
+  end;
+end;
+
+function THilitePasParser.IsTokenStr(const TokenStr: string): Boolean;
+begin
+  Result := StrSameText(fLexer.TokenStr, TokenStr);
+end;
+
 procedure THilitePasParser.Parse(const Source: string);
   {Parses Pascal source code, triggering events as each line and token is
   parsed.
     @param Source [in] String containing Pascal source.
   }
 var
-  Lexer: THilitePasLexer;   // object that tokenises Pascal source
   Elem: THiliteElement;     // identifies a parsed highlight element
-  InASM: Boolean;           // flag true when parsing assembler code
-  BetweenLines: Boolean;    // flag true after EOL before new line starts
 begin
-  // Create lexical analyser that tokenises source code
-  Lexer := THilitePasLexer.Create(Source);
-  try
-    // Intialise state: not in assembler and line not started
-    InASM := False;
-    BetweenLines := True;
-    // Read all tokens from source using analyser
-    while Lexer.NextToken <> tkEOF do
+  // Create new lexical analyser to tokenise source code
+  fLexer.Free;
+  fLexer := THilitePasLexer.Create(Source);
+  fState := [psBetweenLines];
+  while fLexer.NextToken <> tkEOF do
+  begin
+    // We treat end of line separately from other tokens
+    if fLexer.Token <> tkEOL then
     begin
-      // We treat end of line separately from other tokens
-      if Lexer.Token <> tkEOL then
-      begin
-        if InASM then
-        begin
-          // We are inside assembler code
-          // assume assembler style then handle special cases
-          Elem := heAssembler;
-          case Lexer.Token of
-            // we end assembler when an "end" keyword is encountered
-            tkKeyword:
-            begin
-              if StrSameText(Lexer.TokenStr, 'end') then
-              begin
-                InASM := False;
-                Elem := heReserved;
-              end;
-            end;
-            // handle tokens that are not formatted as assembler
-            // Note: we differ from Delphi editor here in that we don't format
-            // strings specially in assembler - they take on attributes of
-            // assembler code
-            tkWhiteSpace:           Elem := heWhiteSpace;
-            tkComment:              Elem := heComment;
-            tkCompilerDir:          Elem := hePreProcessor;
-            tkError:                Elem := heError;
-          end;
-        end
-        else
-        begin
-          // Normal case: in standard Pascal code
-          case Lexer.Token of
-            // Keywords are always reserved. We need to check for "asm" keyword
-            // and switch to assembler mode when found. "asm" keyword is
-            // rendered as reserved word.
-            tkKeyword:
-            begin
-              Elem := heReserved;
-              if StrSameText(Lexer.TokenStr, 'asm') then
-                InASM := True;
-            end;
-            // Map other tokens onto highlight elements
-            tkDirective:            Elem := heReserved;
-            tkComment:              Elem := heComment;
-            tkCompilerDir:          Elem := hePreProcessor;
-            tkIdentifier:           Elem := heIdentifier;
-            tkString,
-            tkChar:                 Elem := heString;
-            tkNumber:               Elem := heNumber;
-            tkFloat:                Elem := heFloat;
-            tkHex:                  Elem := heHex;
-            tkSymbol:               Elem := heSymbol;
-            tkWhitespace:           Elem := heWhitespace;
-            else                    Elem := heError;
-          end;
-        end;
-        // Emit the element
-        if BetweenLines then
-        begin
-          // we only start a new line when there's something to write on it
-          DoLineBegin;
-          BetweenLines := False;
-        end;
-        DoElement(Elem, Lexer.TokenStr);
-      end
+      if psASM in fState then
+        // We are inside assembler code
+        Elem := ParseASMElement
       else
+        // Normal case: in standard Pascal code
+        Elem := ParsePascalElement;
+      EmitElement(Elem);
+    end
+    else
+      EmitEndOfLine;
+  end;
+end;
+
+function THilitePasParser.ParseASMElement: THiliteElement;
+begin
+  // Assume assembler style then handle special cases
+  // Note: we differ from Delphi editor here in that we don't format strings
+  // specially in assembler - they take on attributes of assembler code
+  Result := heAssembler;
+  case fLexer.Token of
+    tkKeyword:
+    begin
+      if IsTokenStr('end') then
       begin
-        // End of line
-        if BetweenLines then
-          // we've never started this line: do it or we may skip blank line
-          DoLineBegin;
-        DoLineEnd;
-        // we are now between lines
-        BetweenLines := True;
+        // "end" keyword ends assembler code block
+        Exclude(fState, psASM);
+        Result := heReserved;
       end;
     end;
-  finally
-    Lexer.Free;
+    // handle tokens that are not formatted as assembler
+    tkWhiteSpace:
+      Result := heWhiteSpace;
+    tkComment:
+      Result := heComment;
+    tkCompilerDir:
+      Result := hePreProcessor;
+    tkError:
+      Result := heError;
+  end;
+end;
+
+function THilitePasParser.ParsePascalElement: THiliteElement;
+begin
+  case fLexer.Token of
+    tkKeyword:
+    begin
+      // Keywords are always reserved. We need to check for "asm" keyword
+      // and switch to assembler mode when found. "asm" keyword is
+      // rendered as reserved word.
+      Result := heReserved;
+      if IsTokenStr('asm') then
+        Include(fState, psASM);
+    end;
+    tkDirective:
+      Result := heReserved;
+    tkComment:
+      Result := heComment;
+    tkCompilerDir:
+      Result := hePreProcessor;
+    tkIdentifier:
+      Result := heIdentifier;
+    tkString,
+    tkChar:
+      Result := heString;
+    tkNumber:
+      Result := heNumber;
+    tkFloat:
+      Result := heFloat;
+    tkHex:
+      Result := heHex;
+    tkSymbol:
+      Result := heSymbol;
+    tkWhitespace:
+      Result := heWhitespace;
+    else
+      Result := heError;
   end;
 end;
 
