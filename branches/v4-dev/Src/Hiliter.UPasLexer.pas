9d114e3b7c9181ff888debcb8d97bccc259d53c6
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-09-19 08:36:55 +0000

    Merged trunk changes for release 3.9.1 (i.e. 1558-1562) into the v4-dev branch.
    Resolved conflicts as follows:
    
    * Src/VCodeSnip.vi: kept version in v4-dev unchanged.
    
    Some files are physically unchanged and have changes only to their 
    svn:mergeinfo property.

--- branches/v4-dev/Src/Hiliter.UPasLexer.pas
+++ branches/v4-dev/Src/Hiliter.UPasLexer.pas
@@ -64,7 +64,7 @@ type
     tkWhitespace,     // white space (spaces, tabs etc, excluding CR and LF)
     tkEOL,            // end of line (usually CRLF but CR and LF on own valid)
     tkEOF,            // end of file
-    tkError           // error condition: shouldn't occur
+    tkError           // error condition: shouldn't occur in valid Pascal code
   );
 
 type
@@ -687,8 +687,13 @@ begin
       fReader.NextChar;
     end;
     // Next comes whole number: get it
-    ParseWholeNumber;
-    Result := tkFloat;
+    if TCharacter.IsDigit(fReader.Ch) then
+    begin
+      ParseWholeNumber;
+      Result := tkFloat
+    end
+    else
+      Result := tkError;
   end;
 end;
 
