56cd242262014e0a85e8936378f4939321f417af
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-10-24 21:46:47 +0000

    Modified Delete Category dialog box to refuse to permit a category to be chosen for deletion if it contains snippets.

--- branches/v4-dev/Src/FmDeleteCategoryDlg.pas
+++ branches/v4-dev/Src/FmDeleteCategoryDlg.pas
@@ -54,6 +54,7 @@ type
   }
   TDeleteCategoryDlg = class(TCategoryEditDlg, INoPublicConstruct)
     frmCategories: TCategoryListFrame;
+    lblErrorMsg: TLabel;
     procedure btnOKClick(Sender: TObject);
   strict private
     fCategories: TCategoryList; // List of categories that can be deleted
@@ -62,6 +63,8 @@ type
       according to changes.
         @param Sender [in] Not used.
       }
+    procedure UpdateErrorLabelState;
+    function CanDelete(const Cat: TCategory): Boolean;
     procedure DeleteCategory(const Cat: TCategory);
       {Deletes category and all its snippets from database.
         @param Cat [in] Category to be deleted.
@@ -95,7 +98,7 @@ uses
   // Delphi
   SysUtils,
   // Project
-  DB.UMain, UConsts, UMessageBox;
+  DB.UMain, UColours, UConsts, UCtrlArranger, UFontHelper, UMessageBox;
 
 {$R *.dfm}
 
@@ -107,6 +110,8 @@ procedure TDeleteCategoryDlg.ArrangeForm;
   }
 begin
   frmCategories.ArrangeFrame;
+  TCtrlArranger.SetLabelHeight(lblErrorMsg);
+  lblErrorMsg.Top := TCtrlArranger.BottomOf(frmCategories, 8);
   inherited;
 end;
 
@@ -117,28 +122,24 @@ procedure TDeleteCategoryDlg.btnOKClick(Sender: TObject);
   }
 var
   Cat: TCategory; // selected category
-resourcestring
-  // Confirmation prompt
-  sConfirmDelete = 'The "%0:2s" category contains %1:d snippet(s).'
-    + EOL2
-    + 'If you delete the category all the snippet(s) will also be deleted.'
-    + EOL2 + EOL
-    + 'Are you sure you want to delete "%0:2s"?';
 begin
   inherited;
   Cat := frmCategories.SelectedCategory;
   Assert(Assigned(Cat), ClassName + '.btnOKClick: No category selected');
-  if (Cat.Snippets.Count = 0)
-    or TMessageBox.Confirm(
-      Self, Format(sConfirmDelete, [Cat.Description, Cat.Snippets.Count])
-    ) then
-    DeleteCategory(Cat)
-  else
-    ModalResult := mrNone;
+  Assert(CanDelete(Cat), ClassName + '.btnOKClick: Category can''t be deleted');
+  DeleteCategory(Cat);
+end;
+
+function TDeleteCategoryDlg.CanDelete(const Cat: TCategory): Boolean;
+begin
+  { TODO: Make a method of TCategory that checks this, and add assertion to
+          check this and remove snippet deletion code from delete category
+          method of TDatabase. }
+  Result := Cat.Snippets.Count = 0;
 end;
 
 procedure TDeleteCategoryDlg.ConfigForm;
-  {Configures form. Populates controls and supplies event handler to frame.
+  {Configures form. Sets up controls and supplies event handler to frame.
   }
 resourcestring
   // Prompt text for frame
@@ -148,6 +149,9 @@ begin
   frmCategories.OnChange := SelectionChangeHandler;
   frmCategories.Prompt := sPrompt;
   frmCategories.SetCategories(fCategories);
+  lblErrorMsg.Font.Color := clWarningText;
+  TFontHelper.SetDefaultBaseFont(lblErrorMsg.Font, False);
+  lblErrorMsg.Visible := False;
 end;
 
 procedure TDeleteCategoryDlg.DeleteCategory(const Cat: TCategory);
@@ -182,6 +186,15 @@ procedure TDeleteCategoryDlg.SelectionChangeHandler(Sender: TObject);
   }
 begin
   UpdateOKBtn;
+  UpdateErrorLabelState;
+end;
+
+procedure TDeleteCategoryDlg.UpdateErrorLabelState;
+begin
+  if not frmCategories.IsValidEntry then
+    lblErrorMsg.Visible := False
+  else
+    lblErrorMsg.Visible := not CanDelete(frmCategories.SelectedCategory);
 end;
 
 procedure TDeleteCategoryDlg.UpdateOKBtn;
@@ -189,7 +202,8 @@ procedure TDeleteCategoryDlg.UpdateOKBtn;
   dialog box.
   }
 begin
-  btnOK.Enabled := frmCategories.IsValidEntry;
+  btnOK.Enabled := frmCategories.IsValidEntry
+    and CanDelete(frmCategories.SelectedCategory);
 end;
 
 end.
