579dfd4d71087d11eb12b3b7f76418f4189365d5
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-10-25 00:13:19 +0000

    Changed to use new TCategory.CanDelete method to check if category can be deleted instead of own home-grown test.

--- branches/v4-dev/Src/FmDeleteCategoryDlg.pas
+++ branches/v4-dev/Src/FmDeleteCategoryDlg.pas
@@ -64,7 +64,9 @@ type
         @param Sender [in] Not used.
       }
     procedure UpdateErrorLabelState;
-    function CanDelete(const Cat: TCategory): Boolean;
+      {Shows or hides error state label depending of whether selected category
+      can be deleted.
+      }
     procedure DeleteCategory(const Cat: TCategory);
       {Deletes category and all its snippets from database.
         @param Cat [in] Category to be deleted.
@@ -124,16 +126,8 @@ begin
   inherited;
   Cat := frmCategories.SelectedCategory;
   Assert(Assigned(Cat), ClassName + '.btnOKClick: No category selected');
-  Assert(CanDelete(Cat), ClassName + '.btnOKClick: Category can''t be deleted');
+  Assert(Cat.CanDelete, ClassName + '.btnOKClick: Category can''t be deleted');
   DeleteCategory(Cat);
-end;
-
-function TDeleteCategoryDlg.CanDelete(const Cat: TCategory): Boolean;
-begin
-  { TODO: Make a method of TCategory that checks this, and add assertion to
-          check this and remove snippet deletion code from delete category
-          method of TDatabase. }
-  Result := Cat.Snippets.Count = 0;
 end;
 
 procedure TDeleteCategoryDlg.ConfigForm;
@@ -188,11 +182,14 @@ begin
 end;
 
 procedure TDeleteCategoryDlg.UpdateErrorLabelState;
+  {Shows or hides error state label depending of whether selected category can
+  be deleted.
+  }
 begin
   if not frmCategories.IsValidEntry then
     lblErrorMsg.Visible := False
   else
-    lblErrorMsg.Visible := not CanDelete(frmCategories.SelectedCategory);
+    lblErrorMsg.Visible := not frmCategories.SelectedCategory.CanDelete;
 end;
 
 procedure TDeleteCategoryDlg.UpdateOKBtn;
@@ -201,7 +198,7 @@ procedure TDeleteCategoryDlg.UpdateOKBtn;
   }
 begin
   btnOK.Enabled := frmCategories.IsValidEntry
-    and CanDelete(frmCategories.SelectedCategory);
+    and frmCategories.SelectedCategory.CanDelete;
 end;
 
 end.
