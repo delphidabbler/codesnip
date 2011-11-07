5436aafe0362ab11d116867ca5fc809c7a5d263a
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2011-11-07 01:30:22 +0000

    XMLDoc commented unit.

--- branches/v4-dev/Src/UCategoryAction.pas
+++ branches/v4-dev/Src/UCategoryAction.pas
@@ -49,28 +49,32 @@ uses
 
 
 type
-
-  {
-  TCategoryAction:
-    Custom action used to request display of a category. Stores ID of required
-    category in properties.
-  }
+  ///  <summary>
+  ///  Custom action used to request display of a category.
+  ///  </summary>
   TCategoryAction = class(TBasicAction, ISetNotifier)
   strict private
-    fCatID: string;       // Value of CatID property
-    fNotifier: INotifier; // Value of Notifier property.
+    var
+      ///  <summary>Value of CatID property.</summary>
+      fCatID: string;
+      ///  <summary>Reference to Notifier object.</summary>
+      fNotifier: INotifier;
   public
+    ///  <summary>Performs the action by displaying a category in the main
+    ///  display.</summary>
+    ///  <remarks>
+    ///  <para>Notifier object is used to cause the category to be displayed.
+    ///  </para>
+    ///  <para>Any OnExecute event handler is ignored.</para>
+    ///  </remarks>
+    ///  <returns>Boolean. False to indicate OnExecute event handler not called.
+    ///  </returns>
     function Execute: Boolean; override;
-      {Executes action by displaying a category in the main display via the
-      Notifier object. Any OnExcute event handler is ignored.
-        @return False to indicate OnExecute event handler not called.
-      }
+    ///  <summary>Stores reference to given notifier object.</summary>
+    ///  <remarks>Implements ISetNotifier.SetNotifier</remarks>
     procedure SetNotifier(const Notifier: INotifier);
-      {Stores a reference to the notifier object.
-        @param Notifier [in] Required notifier object.
-      }
+    ///  <summary>ID of category to be displayed.</summary>
     property CatID: string read fCatID write fCatID;
-      {ID of category to be displayed}
   end;
 
 
@@ -85,10 +89,6 @@ uses
 { TCategoryAction }
 
 function TCategoryAction.Execute: Boolean;
-  {Executes action by displaying a category in the main display via the Notifier
-  object. Any OnExcute event handler is ignored.
-    @return False to indicate OnExecute event handler not called.
-  }
 var
   Cat: TCategory;   // category to be displayed
 begin
@@ -103,9 +103,6 @@ begin
 end;
 
 procedure TCategoryAction.SetNotifier(const Notifier: INotifier);
-  {Stores a reference to the notifier object.
-    @param Notifier [in] Required notifier object.
-  }
 begin
   fNotifier := Notifier;
 end;
