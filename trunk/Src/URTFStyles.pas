c9375a775ebdf0f32d19c85bcb90e87aa20d8dac
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2012-06-20 17:02:43 +0000

    Added new TRTFStyle record along with supporting types. New CreateNull and IsNull methods of TRTFFont. New CreateNull method and equality and inequality operator overloads in TRTFParaSpacing.

--- trunk/Src/URTFStyles.pas
+++ trunk/Src/URTFStyles.pas
@@ -55,6 +55,10 @@ type
     constructor Create(const Name: string;
       const Generic: TRTFGenericFont = rgfDontCare;
       const Charset: TFontCharset = 0);
+    ///  <summary>Creates and returns a new null RTF font.</summary>
+    class function CreateNull: TRTFFont; static;
+    ///  <summary>Checks if the font is null.</summary>
+    function IsNull: Boolean;
     ///  <summary>Compares this font to another.</summary>
     ///  <param name="RTFFont">TRTFont [in] Other font to be compared.</param>
     ///  <returns>Integer. -ve if this font is less than RTFFont, 0 if same and
@@ -76,6 +80,45 @@ type
       Before: Double;
       After: Double;
     constructor Create(const ABefore, AAfter: Double);
+    class function CreateNull: TRTFParaSpacing; static;
+    class operator Equal(const Left, Right: TRTFParaSpacing): Boolean;
+    class operator NotEqual(const Left, Right: TRTFParaSpacing): Boolean;
+  end;
+
+type
+  TRTFStyleCap = (
+    scParaSpacing,
+    scFont,
+    scFontSize,
+    scFontStyles,
+    scColour
+  );
+
+type
+  TRTFStyleCaps = set of TRTFStyleCap;
+
+type
+  TRTFStyle = record
+  public
+    var
+      ParaSpacing: TRTFParaSpacing;
+      Font: TRTFFont;
+      FontSize: Double;
+      FontStyles: TFontStyles;
+      Colour: TColor;
+      Capabilities: TRTFStyleCaps;
+    constructor Create(const ACapabilities: TRTFStyleCaps;
+      const AParaSpacing: TRTFParaSpacing; const AFont: TRTFFont;
+      const AFontSize: Double; const AFontStyles: TFontStyles;
+      const AColour: TColor); overload;
+    constructor Create(const ACapabilities: TRTFStyleCaps;
+      const AFont: TRTFFont; const AFontSize:
+      Double; const AFontStyles: TFontStyles; const AColour: TColor); overload;
+    constructor Create(const AParaSpacing: TRTFParaSpacing); overload;
+    class function CreateNull: TRTFStyle; static;
+    function IsNull: Boolean;
+    class operator Equal(const Left, Right: TRTFStyle): Boolean;
+    class operator NotEqual(const Left, Right: TRTFStyle): Boolean;
   end;
 
 
@@ -83,6 +126,8 @@ implementation
 
 
 uses
+  // Delphi
+  Math,
   // Project
   UStrUtils;
 
@@ -102,12 +147,102 @@ begin
   fCharset := Charset;
 end;
 
+class function TRTFFont.CreateNull: TRTFFont;
+begin
+  Result := TRTFFont.Create('');
+end;
+
+function TRTFFont.IsNull: Boolean;
+begin
+  Result := fName = '';
+end;
+
 { TRTFParaSpacing }
 
 constructor TRTFParaSpacing.Create(const ABefore, AAfter: Double);
 begin
   Before := ABefore;
   After := AAfter;
+end;
+
+class function TRTFParaSpacing.CreateNull: TRTFParaSpacing;
+begin
+  Result := TRTFParaSpacing.Create(0.0, 0.0);
+end;
+
+class operator TRTFParaSpacing.Equal(const Left,
+  Right: TRTFParaSpacing): Boolean;
+begin
+  Result := (Left.Before = Right.Before) and (Left.After = Right.After);
+end;
+
+class operator TRTFParaSpacing.NotEqual(const Left,
+  Right: TRTFParaSpacing): Boolean;
+begin
+  Result := (Left.Before <> Right.Before) or (Left.After <> Right.After);
+end;
+
+{ TRTFStyle }
+
+constructor TRTFStyle.Create(const ACapabilities: TRTFStyleCaps;
+  const AParaSpacing: TRTFParaSpacing; const AFont: TRTFFont;
+  const AFontSize: Double; const AFontStyles: TFontStyles;
+  const AColour: TColor);
+begin
+  Capabilities := ACapabilities;
+  ParaSpacing := AParaSpacing;
+  Font := AFont;
+  FontSize := AFontSize;
+  FontStyles := AFontStyles;
+  Colour := AColour;
+end;
+
+constructor TRTFStyle.Create(const ACapabilities: TRTFStyleCaps;
+  const AFont: TRTFFont; const AFontSize: Double;
+  const AFontStyles: TFontStyles; const AColour: TColor);
+begin
+  Create(
+    ACapabilities - [scParaSpacing],
+    TRTFParaSpacing.CreateNull,
+    AFont,
+    AFontSize,
+    AFontStyles,
+    AColour
+  );
+end;
+
+constructor TRTFStyle.Create(const AParaSpacing: TRTFParaSpacing);
+begin
+  Create([scParaSpacing], AParaSpacing, TRTFFont.CreateNull, 0.0, [], clNone);
+end;
+
+class function TRTFStyle.CreateNull: TRTFStyle;
+begin
+  Result := TRTFStyle.Create(
+    [], TRTFParaSpacing.CreateNull, TRTFFont.CreateNull, 0.0, [], clNone
+  );
+end;
+
+class operator TRTFStyle.Equal(const Left, Right: TRTFStyle): Boolean;
+begin
+  if (Left.Capabilities = []) and (Right.Capabilities = []) then
+    Exit(True);
+  Result := (Left.Capabilities = Right.Capabilities)
+    and (Left.ParaSpacing = Right.ParaSpacing)
+    and StrSameText(Left.Font.Name, Right.Font.Name)
+    and SameValue(Left.FontSize, Right.FontSize)
+    and (Left.FontStyles = Right.FontStyles)
+    and (Left.Colour = Right.Colour);
+end;
+
+function TRTFStyle.IsNull: Boolean;
+begin
+  Result := Capabilities = [];
+end;
+
+class operator TRTFStyle.NotEqual(const Left, Right: TRTFStyle): Boolean;
+begin
+  Result := not(Left = Right);
 end;
 
 end.
