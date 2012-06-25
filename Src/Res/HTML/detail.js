b93697698456efe1fb37dcf68ef8076619cb720c
Peter Johnson <delphidabbler@users.noreply.github.com>
2012-06-25 21:00:53 +0000

    Removed showTestCompileHint() and compileSnippet() JavaScript functions made redundant now that there is no "Test Compile" link in snippet detail pane.

--- Src/Res/HTML/detail.js
+++ Src/Res/HTML/detail.js
@@ -23,7 +23,7 @@
  * The Initial Developer of the Original Code is Peter Johnson
  * (http://www.delphidabbler.com/).
  *
- * Portions created by the Initial Developer are Copyright (C) 2006-2011 Peter
+ * Portions created by the Initial Developer are Copyright (C) 2006-2012 Peter
  * Johnson. All Rights Reserved.
  *
  * Contributors:
@@ -60,15 +60,6 @@ function getInnerText(id) {
  */
 function getSnippetName() {
   return trim(getInnerText("snippetname"));
-}
-
-/*
- * Displays hint information that a test compilation can be performed. Hint
- * contains name of current snippet.
- *  @return void.
- */
-function showTestCompileHint() {
-  showHint("Compile \"" + getSnippetName() + "\"");
 }
 
 /*
