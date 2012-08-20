fd4978c8f08bb0c5d2d80e3877f4aa10ab4ce8c0
Peter Johnson <delphidabbler@users.noreply.github.com>
2012-08-20 20:33:30 +0000

    XMLDoc commented unit.

--- Src/UMenus.pas
+++ Src/UMenus.pas
@@ -45,63 +45,56 @@ uses
 
 
 type
-  ///  <summary>Interface supported by objects that can display a pop-up menu
-  ///  by means other than a menu associated with a PopupMenu
-  ///  property.</summary>
+  ///  <summary>Interface that defines operations to be supported by objects
+  ///  that can display a pop-up menu by means other than a menu associated with
+  ///  a component's PopupMenu property.</summary>
   IPopupMenu = interface(IInterface)
     ['{A01D903C-90B9-4D24-8B0C-D770852C6340}']
-    ///  <summary>Informs if the object has a pop-up menu available.</summary>
+    ///  <summary>Inform if a pop-up menu is available.</summary>
     function HasPopup: Boolean;
-    ///  <summary>Displays pop-up menu at given point on screen.</summary>
+    ///  <summary>Display pop-up menu at given point on screen.</summary>
     procedure Popup(const Pt: TPoint);
   end;
 
 type
-  {TMenuItemClass:
-    Class reference to TMenuItem.
-  }
+  ///  <summary>Class reference to a TMenuItem.</summary>
   TMenuItemClass = class of TMenuItem;
 
+///  <summary>Creates menu item that has an associated action.</summary>
+///  <param name="AOwner">TComponent [in] Menu item owner.</param>
+///  <param name="Cls">TMenuItemClass [in] Class of menu item to be created.
+///  </param>
+///  <param name="Action">TBasicAction [in] Action to be associated with menu
+///  item.</param>
+///  <returns>TMenuItem. New menu item object.</returns>
 function CreateMenuItem(AOwner: TComponent; const Cls: TMenuItemClass;
   const Action: TBasicAction): TMenuItem; overload;
-  {Creates a menu item of a specified subclass with an associated action.
-    @param AOwner [in] Menu item owner.
-    @param Cls [in] Type of menu item to create.
-    @param Action [in] Required action.
-    @return New menu item instance.
-  }
 
+///  <summary>Create a menu item that acts as a menu spacer.</summary>
+///  <param name="AOwner">TComponent [in] Menu item owner.</param>
+///  <param name="Cls">TMenuItemClass [in] Class of menu item to be created.
+///  </param>
+///  <returns>TMenuItem. New menu item spacer object.</returns>
 function CreateMenuSpacer(AOwner: TComponent;
   const Cls: TMenuItemClass): TMenuItem; overload;
-  {Creates a menu spacer of a specified subclass.
-    @param AOwner [in] Menu spacer owner.
-    @param Cls [in] Type of menu spacer to create.
-    @return New menu spacer instance.
-  }
 
 
 implementation
 
 
+///  <summary>Creates a menu item object.</summary>
+///  <param name="AOwner">TComponent [in] Menu item owner.</param>
+///  <param name="Cls">TMenuItemClass [in] Class of menu item to be created.
+///  </param>
+///  <returns>TMenuItem. New menu item object.</returns>
 function CreateMenuItem(AOwner: TComponent;
   const Cls: TMenuItemClass): TMenuItem; overload;
-  {Creates a menu item of a specified subclass.
-    @param AOwner [in] Menu item owner.
-    @param Cls [in] Type of menu item to create.
-    @return New menu item instance.
-  }
 begin
   Result := Cls.Create(AOwner);
 end;
 
 function CreateMenuItem(AOwner: TComponent; const Cls: TMenuItemClass;
   const Action: TBasicAction): TMenuItem; overload;
-  {Creates a menu item of a specified subclass with an associated action.
-    @param AOwner [in] Menu item owner.
-    @param Cls [in] Type of menu item to create.
-    @param Action [in] Required action.
-    @return New menu item instance.
-  }
 begin
   Result := CreateMenuItem(AOwner, Cls);
   Result.Action := Action;
@@ -109,14 +102,10 @@ end;
 
 function CreateMenuSpacer(AOwner: TComponent;
   const Cls: TMenuItemClass): TMenuItem; overload;
-  {Creates a menu spacer of a specified subclass.
-    @param AOwner [in] Menu spacer owner.
-    @param Cls [in] Type of menu spacer to create.
-    @return New menu spacer instance.
-  }
 begin
   Result := CreateMenuItem(AOwner, Cls);
   Result.Caption := '-';
 end;
 
 end.
+
