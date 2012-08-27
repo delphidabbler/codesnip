0ddbaaf59146a48f76f88f9d4f7549bdd19a705b
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2012-08-27 14:56:05 +0000

    Changed units in Web namespace from MPL v1.1 to MPL v2.0.

--- trunk/Src/Web.UHTTPEx.pas
+++ trunk/Src/Web.UHTTPEx.pas
@@ -1,38 +1,17 @@
 {
- * Web.UHTTPEx.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2010-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
  *
  * Class that makes HTTP requests over the internet. Correctly decodes text
  * according to the character set defined in the HTTP response and validates
  * responses that include a checksum. Uses Indy components to make the actual
  * HTTP request.
- *
- * $Rev$
- * $Date$
- *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is Web.UHTTPEx.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2010-2011 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s)
- *   NONE
- *
- * ***** END LICENSE BLOCK *****
 }
 
 
