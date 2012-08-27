e59eac95083d05d71e717a18e3f9474cfc63628a
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2012-08-27 13:18:21 +0000

    Changed units in Compilers namespace from MPL v1.1 to MPL v2.0.

--- trunk/Src/Compilers.UCompilers.pas
+++ trunk/Src/Compilers.UCompilers.pas
@@ -1,38 +1,17 @@
 {
- * Compilers.UCompilers.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2005-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
  *
  * Provides a class that maintains a list of all supported compilers and creates
  * a global singleton instance of the list. Also provides a class that can
  * detect some compilers that are registered on the local machine and another
  * that can persist the compilers list to the application's storage.
- *
- * $Rev$
- * $Date$
- *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is Compilers.UCompilers.pas, formerly UCompilers.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2005-2012 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s)
- *   NONE
- *
- * ***** END LICENSE BLOCK *****
 }
 
 
