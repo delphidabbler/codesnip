f14c5826a5ead3b6f0820bcb417976492eeab27a
Peter Johnson <delphidabbler@users.noreply.github.com>
2012-08-27 12:36:57 +0000

    Changed units in Browser namespace from MPL v1.1 to MPL v2.0.

--- Src/Browser.IntfDocHostUI.pas
+++ Src/Browser.IntfDocHostUI.pas
@@ -1,38 +1,17 @@
 {
- * Browser.IntfDocHostUI.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2005-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
  *
  * Interfaces, records and constants used when hosting the IE WebBrowser Control
  * or automating IE to replace the menus, toolbars, and context menus. The
  * content of the unit is based on Microsoft UI documentation from MSDN. Not all
  * the structures and constants are used in CodeSnip.
- *
- * $Rev$
- * $Date$
- *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is Browser.IntfDocHostUI.pas, formerly IntfUIHandlers.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2005-2010 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s)
- *   NONE
- *
- * ***** END LICENSE BLOCK *****
 }
 
 
