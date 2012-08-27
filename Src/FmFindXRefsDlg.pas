916939b0b91a4bb1bde44cdf44f5a58dce5eb62a
Peter Johnson <delphidabbler@users.noreply.github.com>
2012-08-27 14:10:33 +0000

    Changed form units, along with any subsidiary frames and classes in their namespace, from MPL v1.1 to MPL v2.0.

--- Src/FmFindXRefsDlg.pas
+++ Src/FmFindXRefsDlg.pas
@@ -1,37 +1,17 @@
 {
- * FmFindXRefsDlg.pas
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Dialog box that is used to select criteria for searches for cross referenced
- * snippets. Also defines a class that is used to persist the last chosen search
- * criteria.
+ * Copyright (C) 2006-2012, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
+ * Implements a dialogue box that is used to select criteria for searches for
+ * cross referenced snippets.
  *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is FmFindXRefsDlg.pas
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2006-2012 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributor(s)
- *   NONE
- *
- * ***** END LICENSE BLOCK *****
+ * Also defines a class that is used to persist the last chosen search criteria.
 }
 
 
