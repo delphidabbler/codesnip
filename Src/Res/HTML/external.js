1a78092470017ca22ca77cce1ded3e7b970b605b
Peter Johnson <delphidabbler@users.noreply.github.com>
2012-08-27 12:22:45 +0000

    Changed HTML, JavaScript and CSS files included in project resources from MPL v1.1 to MPL v2.0.

--- Src/Res/HTML/external.js
+++ Src/Res/HTML/external.js
@@ -1,37 +1,16 @@
 /*
- * external.js
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
  *
- * JavaScript code used call into host application via external object. All
- * HTML and JavaScript code should call these routines rather than making
- * direct calls to the external object.
+ * Copyright (C) 2006-2012, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * ***** BEGIN LICENSE BLOCK *****
- *
- * Version: MPL 1.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with the
- * License. You may obtain a copy of the License at http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
- * the specific language governing rights and limitations under the License.
- *
- * The Original Code is external.js
- *
- * The Initial Developer of the Original Code is Peter Johnson
- * (http://www.delphidabbler.com/).
- *
- * Portions created by the Initial Developer are Copyright (C) 2006-2012 Peter
- * Johnson. All Rights Reserved.
- *
- * Contributors:
- *    NONE
- *
- * ***** END LICENSE BLOCK *****
+ * JavaScript code used call into host application via external object. All
+ * HTML and JavaScript code should call these routines rather than making
+ * direct calls to the external object.
  */
 
 
