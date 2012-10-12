706eb3760a8a90edfad9281e78e879109c35d957
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2012-10-12 16:31:11 +0000

    New maintenance branch for CodeSnip release 4.0

---
+++ branches/4/4.0/Src/UViewItemAction.pas
@@ -0,0 +1,50 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2005-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Custom action used to request display of a view item.
+}
+
+
+unit UViewItemAction;
+
+
+interface
+
+
+uses
+  // Delphi
+  Classes,
+  // Project
+  UView;
+
+
+type
+  ///  <summary>
+  ///  Custom action used to request display of a view item.
+  ///  </summary>
+  TViewItemAction = class(TBasicAction)
+  strict private
+    ///  <summary>Value of ViewItem property.</summary>
+    fViewItem: IView;
+    ///  <summary>Value of NewTab property.</summary>
+    fNewTab: Boolean;
+  public
+    ///  <summary>View item to be displayed.</summary>
+    property ViewItem: IView read fViewItem write fViewItem;
+    ///  <summary>Flags whether view item is to be displayed in a new tab (True)
+    ///  or in existing tab (False).</summary>
+    property NewTab: Boolean read fNewTab write fNewTab;
+  end;
+
+
+implementation
+
+end.
+
