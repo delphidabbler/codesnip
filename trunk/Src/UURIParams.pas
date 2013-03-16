64c7de5c34e86d22ba7d045bcb73f7be1c8be7d2
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-03-16 11:50:25 +0000

    Major clean up of UURIParams unit. Removed all unused code in TURIParams and rationalised remaining code. Also emoved TURIParam record.
    Note: there was a bug somewhere in overloaded Add or Create operators that took array of TURIParam parameters. Weren't really needed so removed them instead of tracking down bug!

--- trunk/Src/UURIParams.pas
+++ trunk/Src/UURIParams.pas
@@ -3,13 +3,13 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2010-2012, Peter Johnson (www.delphidabbler.com).
+ * Copyright (C) 2010-2013, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * Encapsulates URI query string parameters and generates the query string from
- * the parameters.
+ * Encapsulates URI query string parameters and generates a query string from
+ * them.
 }
 
 
@@ -21,67 +21,32 @@ interface
 
 uses
   // Delphi
-  SysUtils, Classes, Generics.Collections;
+  Classes, Generics.Collections;
 
 
 type
-
-  {
-  TURIParam:
-    Records that encapsulates a query string parameter. Records the parameter's
-    name and value.
-  }
-  TURIParam = record
-    Name: string;   // Parameter name
-    Value: string;  // Parameter value
-    constructor Create(const AName, AValue: string);
-      {Initialises record with parameter name and value.
-        @param AName [in] Parameter name.
-        @param AValue [in] Parameter value.
-      }
-  end;
-
   {
   TURIParams:
-    Class that encapsulates URI query string parameters and generates the query
-    string from the parameters.
+    Class that encapsulates URI query string parameters and generates a query
+    string from them.
   }
   TURIParams = class(TObject)
   strict private
     fDict: TDictionary<string,string>;
       {Dictionary mapping param names to values}
-    function BuildQueryString(const Encode: TFunc<string,string>): string;
-      {Creates a custom encoded query string containing details of all recorded
-      parameters.
-        @param Encode [in] Anonymous method called to encode the parameters.
-        @return Required query string.
+    procedure Add(const Params: TStrings); overload;
+      {Adds zero or more new parameters to object.
+        @param Params [in] String list containing parameters to be added. Each
+          element is a string in name=value format.
       }
   public
     constructor Create; overload;
       {Constructs object that contains no parameters.
       }
-    constructor Create(const Name, Value: string); overload;
-      {Constructs object containing a single parameter.
-        @param Name [in] Name of parameter to be recorded.
-        @param Value [in] Value of parameter to be recorded.
-      }
-    constructor Create(const Param: TURIParam); overload;
-      {Constructs object containing a single parameter.
-        @param Param [in] Parameter to be recorded.
-      }
-    constructor Create(const Params: array of TURIParam); overload;
-      {Constructs object containing zero or more parameters.
-        @param Params [in] Array of parameters to be recorded.
-      }
     constructor Create(const Params: TStrings); overload;
       {Constructs object containing zero or more parameters.
         @param Params [in] String list where each element specifies a parameter
           in name=value format.
-      }
-    constructor Create(const Params: array of string); overload;
-      {Constructs object containing zero or more parameters.
-        @param Params [in] Array of strings where each element specifies a
-          parameter in name=value format.
       }
     destructor Destroy; override;
       {Tears down object.
@@ -90,49 +55,6 @@ type
       {Adds a single new parameter to object.
         @param Name [in] Name of parameter to be added.
         @param Value [in] Value of parameter to be added.
-      }
-    procedure Add(const Param: TURIParam); overload;
-      {Adds a single new parameter to object.
-        @param Param [in] Parameter to be added.
-      }
-    procedure Add(const Params: array of TURIParam); overload;
-      {Adds zero or more new parameters to object.
-        @param Params [in] Array of parameters to be added.
-      }
-    procedure Add(const Params: TStrings); overload;
-      {Adds zero or more new parameters to object.
-        @param Params [in] String list containing parameters to be added. Each
-          element is a string in name=value format.
-      }
-    procedure Add(const Params: array of string); overload;
-      {Adds zero or more new parameters to object.
-        @param Params [in] Array of strings containing parameters to be added.
-          Each element is a string in name=value format.
-      }
-    function Exists(const Name: string): Boolean; overload;
-      {Checks if a named parameter is recorded in object.
-        @param Name [in] Name of required parameter.
-        @return True if parameter is found, False if not.
-      }
-    function Exists(const Param: TURIParam): Boolean; overload;
-      {Checks if a specified parameter is recorded in object. Only the
-      parameter's name is checked for, the value is ignored.
-        @param Param [in] Required parameter.
-        @return True if parameter with same name is found, False if not.
-      }
-    procedure Update(const Name, Value: string); overload;
-      {Updates the value of a named parameter.
-        @param Name [in] Name of parameter to be updated.
-        @param Value [in] New parameter value.
-      }
-    procedure Update(const Param: TURIParam); overload;
-      {Updates the value of a specified parameter.
-        @param Param [in] Parameter to be updated with new value.
-      }
-    function QueryString: string;
-      {Creates an un-encoded query string containing details of all recorded
-      parameters.
-        @return Required query string.
       }
     function EncodedQueryString: string;
       {Creates a URI encoded query string containing details of all recorded
@@ -150,19 +72,13 @@ implementation
 
 
 uses
+  // Delphi
+  SysUtils,
   // Project
   UComparers, UURIEncode;
 
 
 { TURIParams }
-
-procedure TURIParams.Add(const Param: TURIParam);
-  {Adds a single new parameter to object.
-    @param Param [in] Parameter to be added.
-  }
-begin
-  Add(Param.Name, Param.Value);
-end;
 
 procedure TURIParams.Add(const Name, Value: string);
   {Adds a single new parameter to object.
@@ -171,17 +87,6 @@ procedure TURIParams.Add(const Name, Value: string);
   }
 begin
   fDict.Add(Name, Value);
-end;
-
-procedure TURIParams.Add(const Params: array of TURIParam);
-  {Adds zero or more new parameters to object.
-    @param Params [in] Array of parameters to be added.
-  }
-var
-  Param: TURIParam; // references each parameter in array
-begin
-  for Param in Params do
-    Add(Param);
 end;
 
 procedure TURIParams.Add(const Params: TStrings);
@@ -197,52 +102,6 @@ begin
     Add(Params.Names[Idx], Params.ValueFromIndex[Idx]);
 end;
 
-procedure TURIParams.Add(const Params: array of string);
-  {Adds zero or more new parameters to object.
-    @param Params [in] Array of strings containing parameters to be added. Each
-      element is a string in name=value format.
-  }
-var
-  Param: string;        // references each parameter string of Params
-  ParamList: TStrings;  // string list that receives elements of Params
-begin
-  ParamList := TStringList.Create;
-  try
-    for Param in Params do
-      ParamList.Add(Param);
-    Add(ParamList);
-  finally
-    ParamList.Free;
-  end;
-end;
-
-function TURIParams.BuildQueryString(
-  const Encode: TFunc<string, string>): string;
-  {Creates a custom encoded query string containing details of all recorded
-  parameters.
-    @param Encode [in] Anonymous method called to encode the parameters.
-    @return Required query string.
-  }
-var
-  AParam: TPair<string,string>; // references each recorded parameter
-  SB: TStringBuilder;           // used to build query string
-begin
-  SB := TStringBuilder.Create;
-  try
-    for AParam in fDict do
-    begin
-      if SB.Length > 0 then
-        SB.Append('&');
-      SB.Append(Encode(AParam.Key));
-      if AParam.Value <> '' then
-        SB.AppendFormat('=%s', [Encode(AParam.Value)]);
-    end;
-    Result := SB.ToString;
-  finally
-    SB.Free;
-  end;
-end;
-
 constructor TURIParams.Create;
   {Constructs object that contains no parameters.
   }
@@ -251,44 +110,6 @@ begin
   fDict := TDictionary<string,string>.Create(
     24, TTextEqualityComparer.Create
   );
-end;
-
-constructor TURIParams.Create(const Name, Value: string);
-  {Constructs object containing a single parameter.
-    @param Name [in] Name of parameter to be recorded.
-    @param Value [in] Value of parameter to be recorded.
-  }
-begin
-  Create;
-  Add(Name, Value);
-end;
-
-constructor TURIParams.Create(const Param: TURIParam);
-  {Constructs object containing a single parameter.
-    @param Param [in] Parameter to be recorded.
-  }
-begin
-  Create;
-  Add(Param);
-end;
-
-constructor TURIParams.Create(const Params: array of string);
-  {Constructs object containing zero or more parameters.
-    @param Params [in] Array of strings where each element specifies a parameter
-      in name=value format.
-  }
-begin
-  Create;
-  Add(Params);
-end;
-
-constructor TURIParams.Create(const Params: array of TURIParam);
-  {Constructs object containing zero or more parameters.
-    @param Params [in] Array of parameters to be recorded.
-  }
-begin
-  Create;
-  Add(Params);
 end;
 
 constructor TURIParams.Create(const Params: TStrings);
@@ -314,32 +135,24 @@ function TURIParams.EncodedQueryString: string;
   parameters.
     @return Required query string.
   }
+var
+  AParam: TPair<string,string>; // references each recorded parameter
+  SB: TStringBuilder;           // used to build query string
 begin
-  Result := BuildQueryString(
-    function(S: string): string
+  SB := TStringBuilder.Create;
+  try
+    for AParam in fDict do
     begin
-      Result := URIEncodeQueryString(S);
-    end
-  );
-end;
-
-function TURIParams.Exists(const Name: string): Boolean;
-  {Checks if a named parameter is recorded in object.
-    @param Name [in] Name of required parameter.
-    @return True if parameter is found, False if not.
-  }
-begin
-  Result := fDict.ContainsKey(Name);
-end;
-
-function TURIParams.Exists(const Param: TURIParam): Boolean;
-  {Checks if a specified parameter is recorded in object. Only the parameter's
-  name is checked for, the value is ignored.
-    @param Param [in] Required parameter.
-    @return True if parameter with same name is found, False if not.
-  }
-begin
-  Result := Exists(Param.Name);
+      if SB.Length > 0 then
+        SB.Append('&');
+      SB.Append(URIEncodeQueryString(AParam.Key));
+      if AParam.Value <> '' then
+        SB.AppendFormat('=%s', [URIEncodeQueryString(AParam.Value)]);
+    end;
+    Result := SB.ToString;
+  finally
+    SB.Free;
+  end;
 end;
 
 function TURIParams.IsEmpty: Boolean;
@@ -348,49 +161,6 @@ function TURIParams.IsEmpty: Boolean;
   }
 begin
   Result := fDict.Count = 0;
-end;
-
-function TURIParams.QueryString: string;
-  {Creates an un-encoded query string containing details of all recorded
-  parameters.
-    @return Required query string.
-  }
-begin
-  Result := BuildQueryString(
-    function(S: string): string
-    begin
-      Result := S;
-    end
-  );
-end;
-
-procedure TURIParams.Update(const Name, Value: string);
-  {Updates the value of a named parameter.
-    @param Name [in] Name of parameter to be updated.
-    @param Value [in] New parameter value.
-  }
-begin
-  fDict.Items[Name] := Value;
-end;
-
-procedure TURIParams.Update(const Param: TURIParam);
-  {Updates the value of a specified parameter.
-    @param Param [in] Parameter to be updated with new value.
-  }
-begin
-  Update(Param.Name, Param.Value);
-end;
-
-{ TURIParam }
-
-constructor TURIParam.Create(const AName, AValue: string);
-  {Initialises record with parameter name and value.
-    @param AName [in] Parameter name.
-    @param AValue [in] Parameter value.
-  }
-begin
-  Name := AName;
-  Value := AValue;
 end;
 
 end.
