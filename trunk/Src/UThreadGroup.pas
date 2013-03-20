991d89757dc6a1af42fead32a9a8ed05bc6d07eb
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-03-20 21:52:20 +0000

    XMLDoc commented unit.

--- trunk/Src/UThreadGroup.pas
+++ trunk/Src/UThreadGroup.pas
@@ -23,22 +23,58 @@ uses
 
 
 type
+  ///  <summary>Class that manages the lifetime of a group of thread objects.
+  ///  </summary>
+  ///  <remarks>
+  ///  <para>Threads should be created suspended (not enforced).</para>
+  ///  <para>Threads must not automatically free themselves on termination. Each
+  ///  thread's FreeOnTerminate property will be set False when it is added to
+  ///  the group.</para>
+  ///  </remarks>
   TThreadGroup = class(TObject)
   strict private
     var
+      ///  <summary>List if threads.</summary>
       fThreads: TList<TThread>;
+    ///  <summary>Read accessor for Threads[] property.</summary>
     function GetThread(Idx: Integer): TThread;
   public
+    ///  <summary>Constructs a new empty thread group object.</summary>
     constructor Create;
+    ///  <summary>Destroys object instance.</summary>
     destructor Destroy; override;
+    ///  <summary>Adds the given thread to the group.</summary>
     procedure Add(AThread: TThread); overload;
+    ///  <summary>Adds each thread in the given array to the group.</summary>
     procedure Add(const AThreads: array of TThread); overload;
+    ///  <summary>Notifies all threads to terminate.</summary>
+    ///  <remarks>Simply sets each thread's Terminate property True.</remarks>
     procedure Terminate;
+    ///  <summary>Starts all threads.</summary>
+    ///  <remarks>Threads are started in the order they were added.</remarks>
     procedure Start;
+    ///  <summary>Waits for each thread to terminate, subject to a timeout.
+    ///  </summary>
+    ///  <param name="Timeout">Cardinal [in] Timeout in ms.</param>
+    ///  <returns>Cardinal. Values are the same as for the Windows API
+    ///  WaitForMultipleObjects function. In summary, values $00..$79 represent
+    ///  success while values >= $80 are errors.</returns>
+    ///  <remarks>WaitFor must not be called when the group is empty.</remarks>
     function WaitFor(Timeout: Cardinal): Cardinal;
+    ///  <summary>Signals each thread to terminate and then waits for them to
+    ///  complete, subject to a timeout.</summary>
+    ///  <param name="Timeout">Cardinal [in] Timeout in ms.</param>
+    ///  <returns>Cardinal. Values are the same as for the Windows API
+    ///  WaitForMultipleObjects function. In summary, values $00..$79 represent
+    ///  success while values >= $80 are errors.</returns>
+    ///  <remarks>Kill must not be called when the group is empty.</remarks>
     function Kill(Timeout: Cardinal): Cardinal;
+    ///  <summary>Checks of the group is empty, i.e. contains no threads.
+    ///  </summary>
     function IsEmpty: Boolean;
+    ///  <summary>Returns the number of threads in the group.</summary>
     function Count: Integer;
+    ///  <summary>Indexed array of threads in the group.</summary>
     property Threads[Idx: Integer]: TThread read GetThread; default;
   end;
 
