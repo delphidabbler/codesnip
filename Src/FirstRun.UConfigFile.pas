26ee2ea6460ce91e86f951b6a40295fd2d28cd4a
Peter Johnson <delphidabbler@users.noreply.github.com>
2013-03-29 09:36:28 +0000

    Updated user config file version to 13.

--- Src/FirstRun.UConfigFile.pas
+++ Src/FirstRun.UConfigFile.pas
@@ -77,7 +77,7 @@ type
   strict private
     const
       ///  <summary>Current user config file version.</summary>
-      FileVersion = 12;
+      FileVersion = 13;
   strict protected
     ///  <summary>Returns current user config file version.</summary>
     class function GetFileVersion: Integer; override;
