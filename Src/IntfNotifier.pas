9d64ca74ea1b1a96458ee4b077afbf1f282e455e
Peter Johnson <delphidabbler@users.noreply.github.com>
2013-04-04 00:02:36 +0000

    Added methods to notifier object to support displaying a page of Preferences dialogue box specified by its frame's class name. This uses TShowPrefsPageAction action.

--- Src/IntfNotifier.pas
+++ Src/IntfNotifier.pas
@@ -75,6 +75,11 @@ type
     procedure CheckForUpdates;
     ///  <summary>Displays the program's About Box.</summary>
     procedure ShowAboutBox;
+    ///  <summary>Displays the Preferences dialogue box containing the specified
+    ///  page.</summary>
+    ///  <param name="ClsName">string [in] Class name of the frame that
+    ///  implements the required preferences page.</param>
+    procedure ShowPrefsPage(const ClsName: string);
   end;
 
 type
@@ -133,6 +138,9 @@ type
     ///  <summary>Sets action used to display the program's About Box.</summary>
     ///  <param name="Action">TBasicAction [in] Required action.</param>
     procedure SetAboutBoxAction(const Action: TBasicAction);
+    ///  <summary>Sets action used to display a given page of the Preferences
+    ///  dialogue box.</summary>
+    procedure SetShowPrefsPageAction(const Action: TBasicAction);
   end;
 
 type
