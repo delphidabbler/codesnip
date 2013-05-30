3756846e7a2d56630687fe94cde8abb55dd563e2
delphidabbler@gmail.com <delphidabbler@gmail.com@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-05-30 03:32:03 +0000

    Corrected header comments

--- trunk/Src/ActiveText.UHTMLRenderer.pas
+++ trunk/Src/ActiveText.UHTMLRenderer.pas
@@ -3,13 +3,12 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2009-2012, Peter Johnson (www.delphidabbler.com).
+ * Copyright (C) 2009-2013, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * Static class that provides assistance when rendering active text as HTML.
- * The class renders the active text as HTML and provides CSS to style it.
+ * Provides a class that renders active text as HTML.
 }
 
 
