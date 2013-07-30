54055d64971a4a2426d7d3d3e4291c13381099e6
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-07-30 02:41:23 +0000

    XMLDoc commented unit.

--- trunk/Src/SWAG.UCommon.pas
+++ trunk/Src/SWAG.UCommon.pas
@@ -19,20 +19,37 @@ interface
 
 
 type
+  ///  <summary>Record that encapsulates the data of a SWAG category.</summary>
   TSWAGCategory = record
+    ///  <summary>String that uniquely identifies a SWAG category.</summary>
     ID: string;
+    ///  <summary>SWAG category title.</summary>
     Title: string;
   end;
 
 type
+  ///  <summary>Record that encapsulates the data that defines a SWAG snippet
+  ///  </summary>
   TSWAGSnippet = record
+    ///  <summary>Number that uniquely identifies a SWAG snippet.</summary>
     ID: Cardinal;
+    ///  <summary>ID of SWAG category that snippet belongs to.</summary>
     Category: string;
+    ///  <summary>File name of snippet in original SWAG archive.</summary>
     FileName: string;
+    ///  <summary>Date and time snippet was added to or updated in the SWAG
+    ///  archive.</summary>
     DateStamp: TDateTime;
+    ///  <summary>SWAG snippet title.</summary>
     Title: string;
+    ///  <summary>Name of author(s) of SWAG snippet.</summary>
     Author: string;
+    ///  <summary>Source code of SWAG snippet.</summary>
+    ///  <remarks>Strictly speaking this is the text of the snippet since not
+    ///  all snippets are pure source code - some are text documents.</remarks>
     SourceCode: string;
+    ///  <summary>Flag that indicates if SWAG snippet is a text document (True)
+    ///  or is Pascal source code (False).</summary>
     IsDocument: Boolean;
   end;
 
