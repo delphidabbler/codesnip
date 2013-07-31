aa6bc21d4e542b7d817d5dcd4723e0bc317d13b3
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-07-31 18:17:59 +0000

    CodeSnip version 4.7.0

---
+++ tags/version-4.7.0/Src/UBox.pas
@@ -0,0 +1,49 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Implements a generic class that can wrap any type in an object. Designed for
+ * use in wrapping value types and strings in objects.
+}
+
+
+unit UBox;
+
+
+interface
+
+
+type
+  ///  <summary>Generic class that wraps a type in an object.</summary>
+  ///  <remarks>Although this type can be used to wrap any type it is aimed at
+  ///  wrapping value types and strings.</remarks>
+  TBox<T> = class(TObject)
+  strict private
+    var
+      ///  <summary>Value of Value property.</summary>
+      fValue: T;
+  public
+    ///  <summary>Constructs an object that wraps the specified value.</summary>
+    constructor Create(Value: T);
+    ///  <summary>Value wrapped by object.</summary>
+    property Value: T read fValue;
+  end;
+
+
+implementation
+
+
+{ TBox<T> }
+
+constructor TBox<T>.Create(Value: T);
+begin
+  fValue := Value;
+end;
+
+end.
---
+++ tags/version-4.7.0/Src/UColours.pas
@@ -0,0 +1,71 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2006-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Defines some special colour constants that record various program colours.
+}
+
+
+unit UColours;
+
+
+interface
+
+
+uses
+  // Delphi
+  Graphics;
+
+
+const
+  // Colour constants defining special program colours
+
+  // general
+  clBorder              = clGrayText;   // borders used in HTML
+  clWarningText         = clRed;        // warning message text
+
+  // backgrounds
+  clSourceBg            = $CCFFFF;      // background of displayed source code
+  clCompTblHeadBg       = cl3DLight;    // background of compiler table heading
+
+  // links
+  clDefaultLink         = clWindowText; // un-styled links
+  clExternalLink        = clBlue;       // external links
+  clHelpLink            = clGreen;      // help links
+  clDBLink              = clNavy;       // database links (cat. and snippet)
+  clCommandLink         = clMaroon;     // links that execute program commands
+
+  // news pane
+  clNewsHeading         = clHotLight;   // headings in news frames
+  clNewsHighlight       = clHotLight;   // hilited text in news frames
+  clNewsBodyBorder      = clBorder;     // border round news frame
+  clNewsDateText        = clGrayText;   // dates in news frame
+
+  // text search
+  clTextSearchHighlight = $77FFFF;      // background of info pane search text
+  clTextSearchText      = clNone;       // search text in info pane
+
+  // splash screen
+  clSplashPlainText     = $B52F20;      // splash screen plain text
+  clSplashShadowText    = clWhite;      // splash screen text shadow
+
+  // new tab
+  clNewTabText          = cl3DLight;    // color of text displayed in new tabs
+
+  // colours of snippet titles / heading
+  clMainSnippet         = clWindowText; // main database snippets
+  clUserSnippet         = clBlue;       // user database snippets
+
+  // markup tags
+  clVarText             = clPurple;     // <var> tag text
+
+implementation
+
+end.
+
