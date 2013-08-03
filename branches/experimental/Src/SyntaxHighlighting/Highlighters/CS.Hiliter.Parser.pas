100ff0358fc324400d6acafd52a4ae19b5e1b48d
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-08-03 14:19:10 +0000

    Modified TSyntaxHiliteParser to pass a TSyntaxHiliteElemInfo record to begin and end element events instead of just attribute ID. Some changes to code to support this.

--- branches/experimental/Src/SyntaxHighlighting/Highlighters/CS.Hiliter.Parser.pas
+++ branches/experimental/Src/SyntaxHighlighting/Highlighters/CS.Hiliter.Parser.pas
@@ -29,6 +29,20 @@ uses
 
 
 type
+  ///  <summary>Provides information about the brush and attribute that apply to
+  ///  a syntax highlight element.</summary>
+  ///  <remarks>Callers can use this information to determine the styling to
+  ///  apply to the element.</remarks>
+  TSyntaxHiliteElemInfo = record
+  strict private
+    var
+      fBrushID: string;
+      fAttrID: string;
+  public
+    constructor Create(const ABrushID, AAttrID: string);
+    property BrushID: string read fBrushID;
+    property AttrID: string read fAttrID;
+  end;
 
   ///  <summary>Parses source code into different highlighting elements in a
   ///  manner dependant on a given syntax highlighter brush.</summary>
@@ -49,7 +63,8 @@ type
       ///  <remarks>The caller must determine how to style each highlight
       ///  atrribute.</remarks>
       TParseElementEvent = procedure(Parser: TObject;
-        AttrID, ElemText: string) of object;
+        const ElemInfo: TSyntaxHiliteElemInfo; const ElemText: string)
+        of object;
   strict private
     var
       ///  <summary>Reference to OnElement event handler.</summary>
@@ -62,21 +77,25 @@ type
       ///  source code.</summary>
       ///  <param name="Source">TStrings [in] Lines of source code to be
       ///  processed.</param>
-      ///  <param name="Highlighter">TSynCustomHighlighter [in] Highlighter
-      ///  object that performs the highlighting.</param>
+      ///  <param name="Brush">TSyntaxHiliterBrush [in] Brush used to perform
+      ///  the highlighting. Must not be a null brush.</param>
       procedure DoHighlighting(const Source: TStrings;
-        const Highlighter: TSynCustomHighlighter);
+        const Brush: TSyntaxHiliterBrush);
       ///  <summary>Performs a null highlighting operation on the given source
       ///  code.</summary>
       ///  <param name="Source">TStrings [in] Lines of source code to be
       ///  processed.</param>
+      ///  <param name="Brush">TSyntaxHiliterBrush [in] Null highlighter brush
+      ///  that does the "highlighting". Must be a null brush.</param>
       ///  <remarks>The source is simply broken into lines which are emitted one
       ///  at a time.</remarks>
-      procedure DoNullHighlighting(const Source: TStrings);
+      procedure DoNullHighlighting(const Source: TStrings;
+        const Brush: TSyntaxHiliterBrush);
   strict protected
     ///  <summary>Triggers OnElement event for the given element text and
     ///  highlighting attribute.</summary>
-    procedure DoElement(const AttrID: string; const ElemText: string);
+    procedure DoElement(const ElemInfo: TSyntaxHiliteElemInfo;
+      const ElemText: string);
     ///  <summary>Triggers OnLineBegin event.</summary>
     procedure DoLineBegin; virtual;
     ///  <summary>Triggers OnLineEnd events.</summary>
@@ -112,40 +131,50 @@ uses
 
 { TSyntaxHiliteParser }
 
-procedure TSyntaxHiliteParser.DoElement(const AttrID: string;
+procedure TSyntaxHiliteParser.DoElement(const ElemInfo: TSyntaxHiliteElemInfo;
   const ElemText: string);
 begin
   if Assigned(fOnElement) then
-    fOnElement(Self, AttrID, ElemText);
+    fOnElement(Self, ElemInfo, ElemText);
 end;
 
 procedure TSyntaxHiliteParser.DoHighlighting(const Source: TStrings;
-  const Highlighter: TSynCustomHighlighter);
+  const Brush: TSyntaxHiliterBrush);
 var
   LineIdx: Integer;
   Line: string;
   Attr: TSynHighlighterAttributes;
+  Highlighter: TSynCustomHighlighter;
 begin
+  Assert(not Brush.IsNull, ClassName + '.DoHighlighting: Brush is null');
   // Do the parsing
-  Highlighter.ResetRange;
-  for LineIdx := 1 to Source.Count do  // this is a 1-based loop deliberately
-  begin
-    DoLineBegin;
-    Line := StrTrimRight(Source[LineIdx - 1]); // trim trailing white space
-    // NOTE: LineIdx parameter to .SetLine seems to be redundant except in
-    // SynHighlighterMulti, which is not used here
-    Highlighter.SetLine(Line, LineIdx);
-    while not Highlighter.GetEOL do
+  Highlighter := Brush.CreateHighlighter;
+  try
+    Highlighter.ResetRange;
+    for LineIdx := 1 to Source.Count do  // this is a 1-based loop deliberately
     begin
-      Attr := Highlighter.GetTokenAttribute;
-      // NOTE: We check there is an attribute since some highlighters can
-      // return a nil Attr: e.g. the Pascal highlighter does this if a
-      // trailing EOF or null terminates the code
-      if Assigned(Attr) then
-        DoElement(Attr.Name, Highlighter.GetToken);
-      Highlighter.Next;
+      DoLineBegin;
+      Line := StrTrimRight(Source[LineIdx - 1]); // trim trailing white space
+      // NOTE: LineIdx parameter to .SetLine seems to be redundant except in
+      // SynHighlighterMulti, which is not used here
+      Highlighter.SetLine(Line, LineIdx);
+      while not Highlighter.GetEOL do
+      begin
+        Attr := Highlighter.GetTokenAttribute;
+        // NOTE: We check there is an attribute since some highlighters can
+        // return a nil Attr: e.g. the Pascal highlighter does this if a
+        // trailing EOF or null terminates the code
+        if Assigned(Attr) then
+          DoElement(
+            TSyntaxHiliteElemInfo.Create(Brush.ID, Attr.Name),
+            Highlighter.GetToken
+          );
+        Highlighter.Next;
+      end;
+      DoLineEnd;
     end;
-    DoLineEnd;
+  finally
+    Highlighter.Free;
   end;
 end;
 
@@ -161,14 +190,15 @@ begin
     fOnLineEnd(Self);
 end;
 
-procedure TSyntaxHiliteParser.DoNullHighlighting(const Source: TStrings);
+procedure TSyntaxHiliteParser.DoNullHighlighting(const Source: TStrings;
+  const Brush: TSyntaxHiliterBrush);
 var
   Line: string;
 begin
   for Line in Source do
   begin
     DoLineBegin;
-    DoElement('', Line);
+    DoElement(TSyntaxHiliteElemInfo.Create(Brush.ID, ''), Line);
     DoLineEnd;
   end;
 end;
@@ -177,31 +207,33 @@ procedure TSyntaxHiliteParser.Parse(const Source: string;
   const Brush: TSyntaxHiliterBrush);
 var
   Lines: TStringList;
-  Highlighter: TSynCustomHighlighter;
 begin
   Assert(Assigned(Brush), ClassName + '.Parse: Brush is nil.');
-  Highlighter := Brush.CreateHighlighter;
+  // Split source code into lines, trimming leading and trailing blank lines
+  Lines := TStringList.Create;
   try
-    // Split source code into lines, trimming leading and trailing blank lines
-    Lines := TStringList.Create;
-    try
-      Lines.Text := Source;
-      { TODO: Create a StrIsBlank routine to replace numerous calls in form
-              if StrTrim(S) = '' }
-      while (Lines.Count > 0) and (StrTrim(Lines[Pred(Lines.Count)]) = '') do
-        Lines.Delete(Pred(Lines.Count));
-      while (Lines.Count > 0) and (StrTrim(Lines[0]) = '') do
-        Lines.Delete(0);
-      if Assigned(Highlighter) then
-        DoHighlighting(Lines, Highlighter)
-      else
-        DoNullHighlighting(Lines);
-    finally
-      Lines.Free;
-    end
+    Lines.Text := Source;
+    { TODO: Create a StrIsBlank routine to replace numerous calls in form
+            if StrTrim(S) = '' }
+    while (Lines.Count > 0) and (StrTrim(Lines[Pred(Lines.Count)]) = '') do
+      Lines.Delete(Pred(Lines.Count));
+    while (Lines.Count > 0) and (StrTrim(Lines[0]) = '') do
+      Lines.Delete(0);
+    if not Brush.IsNull then
+      DoHighlighting(Lines, Brush)
+    else
+      DoNullHighlighting(Lines, Brush);
   finally
-    Highlighter.Free;
-  end;
+    Lines.Free;
+  end
+end;
+
+{ TSyntaxHiliteElemInfo }
+
+constructor TSyntaxHiliteElemInfo.Create(const ABrushID, AAttrID: string);
+begin
+  fBrushID := ABrushID;
+  fAttrID := AAttrID;
 end;
 
 end.
