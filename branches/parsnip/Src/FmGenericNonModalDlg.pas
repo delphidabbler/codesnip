fe684290557575d31d14049d8d8af6e26737c42f
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-08-18 14:51:47 +0000

    Recreated "parsnip" (v5 development) branch by branching trunk just after updating it with changes in v4.7.1.

---
+++ branches/parsnip/Src/FmGenericNonModalDlg.pas
@@ -0,0 +1,87 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2013, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Implements a base class for all the program's non-modal dialogue boxes.
+}
+
+
+unit FmGenericNonModalDlg;
+
+
+interface
+
+
+uses
+  // Delphi
+  StdCtrls, Controls, ExtCtrls, Classes,
+  // Project
+  FmGenericDlg;
+
+
+type
+  ///  <summary>Base class for all non-modal dialogue boxes in the program.
+  ///  </summary>
+  ///  <remarks>By default non-modal dialogue boxes are fixed size tool windows.
+  ///  </remarks>
+  TGenericNonModalDlg = class(TGenericDlg)
+    btnClose: TButton;
+    ///  <summary>Traps key down events on form. Closes dialogue box if
+    ///  unshifted ESC key detected.</summary>
+    procedure FormKeyDown(Sender: TObject; var Key: Word; Shift: TShiftState);
+    ///  <summary>Handles clicks on Close button by closing form.</summary>
+    procedure btnCloseClick(Sender: TObject);
+  strict protected
+    ///  <summary>Positions controls and sizes dialogue box form.</summary>
+    ///  <remarks>This override positions Close button in addition to inherited
+    ///  behaviour.</remarks>
+    procedure ArrangeForm; override;
+  end;
+
+
+implementation
+
+
+uses
+  // Delphi
+  Windows,
+  // Project
+  UCtrlArranger;
+
+
+{$R *.dfm}
+
+
+{ TGenericNonModalDlg }
+
+procedure TGenericNonModalDlg.ArrangeForm;
+begin
+  inherited;
+  // Arrange Close to left of any Help button
+  TCtrlArranger.AlignTops([btnHelp, btnClose]);
+  if btnHelp.Visible then
+    TCtrlArranger.MoveToLeftOf(btnHelp, btnClose, 4)
+  else
+    btnClose.Left := btnHelp.Left;
+end;
+
+procedure TGenericNonModalDlg.btnCloseClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TGenericNonModalDlg.FormKeyDown(Sender: TObject; var Key: Word;
+  Shift: TShiftState);
+begin
+  inherited;
+  if (Key = VK_ESCAPE) and (Shift = []) then
+    Close;
+end;
+
+end.
---
+++ branches/parsnip/Src/UNulFormAligner.pas
@@ -0,0 +1,62 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2007-2013, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Defines a do nothing class that implements IAligner for use with forms that
+ * do not support alignment.
+}
+
+
+unit UNulFormAligner;
+
+
+interface
+
+
+uses
+  // Delphi
+  Forms,
+  // Project
+  IntfAligner;
+
+
+type
+
+  {
+  TNulAligner:
+    Do nothing IAligner implementation. Used for forms that do not support
+    alignment.
+  }
+  TNulAligner = class(TInterfacedObject,
+    IFormAligner
+  )
+  public
+    {Method of IFormAligner}
+    procedure AlignForm(const AForm: TCustomForm);
+      {Does nothing.
+        @param AForm [in] Not used.
+      }
+  end;
+
+
+implementation
+
+
+{ TNulAligner }
+
+procedure TNulAligner.AlignForm(const AForm: TCustomForm);
+  {Does nothing.
+    @param AForm [in] Not used.
+  }
+begin
+  // Do nothing
+end;
+
+end.
+
