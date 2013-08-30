4f966f33feaf67630d0ecfc4101ccfd42d4e71e9
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-08-30 16:06:02 +0000

    Revised usage information in header comments.

--- branches/parsnip/Src/Tests/DUnit/CodeSnipTests.dpr
+++ branches/parsnip/Src/Tests/DUnit/CodeSnipTests.dpr
@@ -3,42 +3,17 @@
  * --------------------------------------
  *
  * This project provides a DUnit test framework for some of the source code from
- * the CodeSnip project. It can be compiled as either a GUI or Console
- * application.
+ * the CodeSnip project.
  *
- * Console Application
- * ~~~~~~~~~~~~~~~~~~~
+ * The project can be compiled either a GUI application (the default) or as a
+ * console application. To compile as a console application the
+ * CONSOLE_TESTRUNNER symbol must be defined.
  *
- * The CONSOLE_TESTRUNNER symbol must be defined to create a console
- * application. From the IDE define the symbol in the conditional defines entry
- * in project options. To compile from the command line pass the -D switch to
- * the compiler to define the symbol. The command line compiler also need to be
- * told the location of the DUnit binaries. This is usually $(BDS)\lib. Use the
- * following command line:
+ * Some tests will not be available unless the TESTING symbol is defined, which
+ * it is by default when compiling from the Delphi IDE.
  *
- *   DCC32 -DCONSOLE_TESTRUNNER -B -R<path-to-dunit-binaries> CodeSnipTests
- *
- * For example (all on one line):
- *
- *   DCC32 -DCONSOLE_TESTRUNNER -B
- *     -R"C:\Program Files\Embarcadero\RAD Studio\7.0\lib" CodeSnipTests
- *
- * GUI Application
- * ~~~~~~~~~~~~~~~
- *
- * The CONSOLE_TESTRUNNER symbol must not be defined to compile a GUI
- * application. From the IDE make sure the symbol is removed from the
- * conditional defines entry in project options if necessary. To compile from
- * the command line do this:
- *
- *   DCC32 -B -R<path-to-dunit-binaries> CodeSnipTests
- *
- * Note that once again the path to the DUnit binaries is required.
- *
- * -----------------------------------------------------------------------------
  * $Rev$
  * $Date$
- * -----------------------------------------------------------------------------
 }
 
 
