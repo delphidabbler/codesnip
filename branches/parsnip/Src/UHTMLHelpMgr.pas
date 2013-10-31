a0e125ec7122aa0d059da1a3175e8b5f376ad7d7
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-10-31 01:43:18 +0000

    XMLDoc commented unit + some other commenting tweaks.

--- branches/parsnip/Src/UHTMLHelpMgr.pas
+++ branches/parsnip/Src/UHTMLHelpMgr.pas
@@ -3,14 +3,14 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2007-2012, Peter Johnson (www.delphidabbler.com).
+ * Copyright (C) 2007-2013, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
  *
- * Implements a class that implements the IHelpMgr interface and registers an
- * instantiation of it as the program's help manager. The object provides help
- * using the HTML help system.
+ * Provides a class that implements the IHelpMgr interface using the HTML Help
+ * system and registers an instantiation of the class as the program's
+ * help manager.
 }
 
 
@@ -31,60 +31,66 @@ uses
 
 
 type
-  {
-  THHAKLink:
-    Structure used to specify one or more ALink names or KLink keywords to be
-    searched for.
-  }
+  ///  <summary>Structure used to specify one or more ALink names or KLink
+  ///  keywords to be searched for.</summary>
+  ///  <remarks>The LPCTSTR fields behave strangely when using the Unicode API.
+  ///  In this case LPCTSTR is defined as PWideChar, but casting a Unicode
+  ///  string to a PChar (=PWideChar) causes HTML Help to see only the first
+  ///  character of the keyword, implying HTML Help is treating the string an
+  ///  ANSI string. To get this to work a Unicode Delphi string must first be
+  ///  cast to an ANSI string and then to a pointer before assiging to the
+  ///  LPCTSTR valued field.</remarks>
   THHAKLink = packed record
-    cbStruct: Integer;    // sizeof this structure
-    fReserved: BOOL;      // must be FALSE (really!)
-    pszKeywords: LPCTSTR; // semi-colon separated keywords
-    pszUrl: LPCTSTR;      // URL to jump to if no keywords found (may be nil)
-    pszMsgText: LPCTSTR;  // MessageBox text on failure (used if pszUrl nil)
-    pszMsgTitle: LPCTSTR; // Title of any failure MessageBox
-    pszWindow: LPCTSTR;   // Window to display pszURL in
-    fIndexOnFail: BOOL;   // Displays index if keyword lookup fails.
+    ///  <summary>Size of record in bytes.</summary>
+    cbStruct: Integer;
+    ///  <summary>Reserved. Must be False.</summary>
+    fReserved: BOOL;
+    ///  <summary>Semi-colon separated keywords.</summary>
+    pszKeywords: LPCTSTR;
+    ///  <summary>URL to jump to if none of the keywords are found (may be nil).
+    ///  </summary>
+    pszUrl: LPCTSTR;
+    ///  <summary>Text to be displayed in a message box on failure (may be nil).
+    ///  </summary>
+    pszMsgText: LPCTSTR;
+    ///  <summary>Title of any failure message box.</summary>
+    pszMsgTitle: LPCTSTR;
+    ///  <summary>Name of window where pszUrl is to be displayed.</summary>
+    pszWindow: LPCTSTR;
+    ///  <summary>Flag determining if help index is displayed if keyword lookup
+    ///  fails.</summary>
+    fIndexOnFail: BOOL;
   end;
 
-  {
-  THTMLHelpMgr:
-    Class that interacts with and controls HTML Help and implements IHelpMgr.
-  }
-  THTMLHelpMgr = class(TInterfacedObject,
-    IHelpMgr
-  )
+  ///  <summary>Class that implements the help manager interface for the HTML
+  ///  help system.</summary>
+  THTMLHelpMgr = class(TInterfacedObject, IHelpMgr)
   strict private
+    ///  <summary>Calls HtmlHelp API function with given command, help page and
+    ///  parameters.</summary>
+    ///  <param name="Command">UINT [in] Command to send to HTML Help.</param>
+    ///  <param name="HelpPage">string [in] Name of required HTML topic "file"
+    ///  within help file. May be empty string if no specific page is required.
+    ///  </param>
+    ///  <param name="Data">DWORD [in] Data to pass to HTML Help. Meaning
+    ///  depends on Command.</param>
     procedure DoAppHelp(const Command: UINT; const HelpPage: string;
       const Data: DWORD);
-      {Calls HtmlHelp API with specified command and parameters.
-        @param Command [in] Command to send to HTML Help.
-        @param HelpPage [in] Name of HTML file containing required help page
-          within help file. May be '' if no specific page required.
-        @param Data [in] Data to pass to HTML Help. Format depends on command.
-      }
   public
+    ///  <summary>Displays any help topics that match the given A-Link keyword.
+    ///  </summary>
     procedure ShowHelp(const AKeyword: string);
-      {Displays HTML help topic specified by an A-Link keyword.
-        @param AKeyword [in] Required A-Link keyword.
-      }
+    ///  <summary>Displays HTML help contents tab and default help page.
+    ///  </summary>
     procedure ShowContents;
-      {Displays HTML help contents tab and default help page.
-      }
   end;
 
 { THTMLHelpMgr }
 
 procedure THTMLHelpMgr.DoAppHelp(const Command: UINT; const HelpPage: string;
   const Data: DWORD);
-  {Calls HtmlHelp API with specified command and parameters.
-    @param Command [in] Command to send to HTML Help.
-    @param HelpPage [in] Name of HTML file containing required help page within
-      help file. May be '' if no specific page required.
-    @param Data [in] Data to pass to HTML Help. Format depends on command.
-  }
 var
-  HelpSpec: string; // Help file followed by any required page
+  HelpSpec: string; // Help file name followed by any required topic page
 begin
   HelpSpec := TAppInfo.HelpFileName;
   if HelpPage <> '' then
@@ -93,38 +99,25 @@ begin
 end;
 
 procedure THTMLHelpMgr.ShowContents;
-  {Displays HTML help contents tab and default help page.
-  }
 begin
   DoAppHelp(HH_DISPLAY_TOC, '', 0);
 end;
 
 procedure THTMLHelpMgr.ShowHelp(const AKeyword: string);
-  {Displays HTML help topic specified by an A-Link keyword.
-    @param AKeyword [in] Required A-Link keyword.
-  }
 var
   ALink: THHAKLink; // structure containing details of A-Link
 begin
   ZeroMemory(@ALink, SizeOf(THHAKLink));
-  // Fill in A link structure
   ALink.cbStruct := SizeOf(THHAKLink);
-  // *** NOTE:
-  // This one is weird: when using the Unicode API just casting the keyword to
-  // PChar causes HTML Help to see only the first character of the keyword. We
-  // have to cast to ASCII string and then to a pointer to get this to work,
-  // even though pszKeywords is declared as LPCTSTR which is defined as
-  // PWideChar.
+  // See remarks for THHAKLink for explanation of the following string casts.
   ALink.pszKeywords := Pointer(StringToASCIIString(AKeyword));
   ALink.fIndexOnFail := True;
-  // Display help
+  // We pass "pointer" to ALink as command's data
   DoAppHelp(HH_ALINK_LOOKUP, '', DWORD(@ALink));
 end;
 
 initialization
 
-// Create instance of help manager and register it for use as program's help
-// system
 RegisterHelpMgr(THTMLHelpMgr.Create);
 
 end.
