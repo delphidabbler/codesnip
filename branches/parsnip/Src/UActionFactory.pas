47707f7403190ff3d32550e6131c68507ecd67dd
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-12-03 02:58:08 +0000

    Added methods to TActionFactory to create instance of the new TDisplayTagAction and TRemoveTagAction actions.

--- branches/parsnip/Src/UActionFactory.pas
+++ branches/parsnip/Src/UActionFactory.pas
@@ -20,7 +20,8 @@ interface
 
 uses
   // Delphi
-  Classes, ActnList,
+  Classes,
+  ActnList,
   // Project
   UBaseObjects;
 
@@ -96,6 +97,20 @@ type
         @param OnExecHandler [in] Handler for action's OnExecute event.
         @return Reference to newly created action.
       }
+    class function CreateDisplayTagAction(const AOwner: TComponent;
+      const OnExecHandler: TNotifyEvent = nil): TBasicAction;
+      {Creates a Display Tag action and sets OnExecute handler if provided.
+        @param AOwner [in] Owner of action.
+        @param OnExecHandler [in] Handler for action's OnExecute event.
+        @return Reference to newly created action.
+      }
+    class function CreateRemoveTagAction(const AOwner: TComponent;
+      const OnExecHandler: TNotifyEvent = nil): TBasicAction;
+      {Creates a Remove Tag action and sets OnExecute handler if provided.
+        @param AOwner [in] Owner of action.
+        @param OnExecHandler [in] Handler for action's OnExecute event.
+        @return Reference to newly created action.
+      }
   end;
 
 
@@ -106,8 +121,15 @@ uses
   // Delphi
   StdActns,
   // Project
-  UCategoryAction, UDetailTabAction, UEditSnippetAction, ULinkAction,
-  UShowPrefsPageAction, USnippetAction, UViewItemAction;
+  CS.Actions.DisplayTag,
+  CS.Actions.RemoveTag,
+  UCategoryAction,
+  UDetailTabAction,
+  UEditSnippetAction,
+  ULinkAction,
+  UShowPrefsPageAction,
+  USnippetAction,
+  UViewItemAction;
 
 
 { TActionFactory }
@@ -149,6 +171,17 @@ begin
   Result := CreateAction(TDetailTabAction, AOwner, OnExecHandler);
 end;
 
+class function TActionFactory.CreateDisplayTagAction(const AOwner: TComponent;
+  const OnExecHandler: TNotifyEvent): TBasicAction;
+  {Creates a Display Tag action and sets OnExecute handler if provided.
+    @param AOwner [in] Owner of action.
+    @param OnExecHandler [in] Handler for action's OnExecute event.
+    @return Reference to newly created action.
+  }
+begin
+  Result := CreateAction(TDisplayTagAction, AOwner, OnExecHandler);
+end;
+
 class function TActionFactory.CreateEditSnippetAction(const AOwner: TComponent;
   const OnExecHandler: TNotifyEvent): TBasicAction;
   {Creates an Edit Snipper action and sets OnExecute handler if provided.
@@ -169,6 +202,17 @@ class function TActionFactory.CreateLinkAction(const AOwner: TComponent;
   }
 begin
   Result := CreateAction(TLinkAction, AOwner, OnExecHandler) as TCustomAction;
+end;
+
+class function TActionFactory.CreateRemoveTagAction(const AOwner: TComponent;
+  const OnExecHandler: TNotifyEvent): TBasicAction;
+  {Creates a Remove Tag action and sets OnExecute handler if provided.
+    @param AOwner [in] Owner of action.
+    @param OnExecHandler [in] Handler for action's OnExecute event.
+    @return Reference to newly created action.
+  }
+begin
+  Result := CreateAction(TRemoveTagAction, AOwner, OnExecHandler);
 end;
 
 class function TActionFactory.CreateShowPrefsPageAction(
