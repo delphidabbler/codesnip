77ae21a1762e20975e0411eadc1e09764b03c9a6
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-12-12 17:37:03 +0000

    Changed TDatabaseIOFactory.CreateWriter to return an IDatabaseWriter instance that writes database in native format. Re-ordered code in TDatabaseIOFactory.CreateLoader.

--- branches/parsnip/Src/DB.UDatabaseIO.pas
+++ branches/parsnip/Src/DB.UDatabaseIO.pas
@@ -82,7 +82,7 @@ type
   }
   TDatabaseIOFactory = class(TNoConstructObject)
   public
-    ///  <summary>Create and returns an instance of a database loader object
+    ///  <summary>Creates and returns an instance of a database loader object
     ///  that can be used to load the database.</summary>
     ///  <remarks>If a database in native format is present a loader for that
     ///  database is returned. If there is no native database then any legacy
@@ -94,11 +94,11 @@ type
       {Creates an object to use to load the user database.
         @return Required object instance.
       }
-    class function CreateWriter: _IDatabaseWriter;
-      {Create an object that can write user defined data from the Database
-      object to storage.
-        @return Required object instance.
-      }
+    ///  <summary>Creates and returns an instance of a an object that can be
+    ///  used to save the database.</summary>
+    ///  <remarks>This method always returns an writer object that saves in
+    ///  native format. No other output formats are supported.</remarks>
+    class function CreateWriter: IDatabaseWriter;
   end;
 
   {
@@ -279,10 +279,12 @@ type
 class function TDatabaseIOFactory.CreateLoader: IDatabaseLoader;
 begin
   Result := TDBNativeReader.Create(TAppInfo.UserDataDir);
+  if Result.DatabaseExists then
+    Exit;
+  Result := TDBLegacyUserDBReader.Create(TAppInfo.UserDataDir);
   if not Result.DatabaseExists then
-    Result := TDBLegacyUserDBReader.Create(TAppInfo.UserDataDir);
-  if not Result.DatabaseExists then
-    Result := TEmptyDBReader.Create;
+    Exit;
+  Result := TEmptyDBReader.Create;
 end;
 
 class function TDatabaseIOFactory.CreateUserDBLoader: _IDatabaseLoader;
@@ -293,13 +295,9 @@ begin
   Result := TUserDatabaseLoader.Create;
 end;
 
-class function TDatabaseIOFactory.CreateWriter: _IDatabaseWriter;
-  {Create an object that can write user defined data from the Database object to
-  storage.
-    @return Required object instance.
-  }
+class function TDatabaseIOFactory.CreateWriter: IDatabaseWriter;
 begin
-  Result := TDatabaseWriter.Create;
+  Result := TDBNativeWriter.Create(TAppInfo.UserDataDir);
 end;
 
 { TDatabaseLoader }
