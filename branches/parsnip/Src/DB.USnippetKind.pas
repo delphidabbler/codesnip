600eae9cad4b2948116164aaf3c5afd302ba2c9f
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-12-17 10:56:11 +0000

    Removed redundant TSnippetKindInfoList class.

--- branches/parsnip/Src/DB.USnippetKind.pas
+++ branches/parsnip/Src/DB.USnippetKind.pas
@@ -29,32 +29,6 @@ uses
 
 
 type
-  ///  <summary>
-  ///  Static class that provides a read-only, enumerable, list of
-  ///  TSnippetKindInfo records, one for each snippet kind.
-  ///  </summary>
-  TSnippetKindInfoList = class(TNoConstructObject)
-  strict private
-    type
-      ///  <summary>Array of snippet kind information records. Has element for
-      ///  each snippet kind.</summary>
-      TSnippetKindInfoArray = array[TSnippetKind] of TSnippetKindInfo;
-    class var
-      ///  <summary>Array of snippet kind info records.</summary>
-      fItems: TSnippetKindInfoArray;
-      ///  <summary>Flag indicating if fItems has been initialised.</summary>
-      fInitialised: Boolean;
-  strict private
-    ///  <summary>Initialises fItems array.</summary>
-    class procedure Init;
-    ///  <summary>Read accessor for Items property.</summary>
-    class function GetItems: TSnippetKindInfoArray; static;
-  public
-    ///  <summary>Enumerable array of snippet kind info records, one element for
-    ///  each snippet kind.</summary>
-    class property Items: TSnippetKindInfoArray read GetItems;
-  end;
-
   TSnippetKindList = class(TInterfacedObject, ISnippetKindList)
   strict private
     type
@@ -84,43 +58,6 @@ type
 
 implementation
 
-
-{ TSnippetKindInfoList }
-
-class function TSnippetKindInfoList.GetItems: TSnippetKindInfoArray;
-begin
-  if not fInitialised then
-  begin
-    // The obvious thing to do is to initialise the Items[] array in a class
-    // constructor. But the required resource strings are read as '' when
-    // initialisation is done in a class constructor, so we need to jump thru
-    // these hoops.
-    Init;
-    fInitialised := True;
-  end;
-  Result := fItems;
-end;
-
-class procedure TSnippetKindInfoList.Init;
-resourcestring
-  // Snippet kind descriptions
-  sFreeForm         = 'Freeform';
-  sRoutine          = 'Routine';
-  sConstant         = 'Constant';
-  sTypeDef          = 'Type Definition';
-  sUnit             = 'Unit';
-  sClass            = 'Class / Advanced Record';
-const
-  // Map of snippet kinds onto their descriptions
-  Descriptions: array[TSnippetKind] of string = (
-    sFreeform, sRoutine, sConstant, sTypeDef, sUnit, sClass
-  );
-var
-  Kind: TSnippetKind;
-begin
-  for Kind := Low(TSnippetKind) to High(TSnippetKind) do
-    fItems[Kind] := TSnippetKindInfo.Create(Kind, Descriptions[Kind]);
-end;
 
 { TSnippetKindList }
 
