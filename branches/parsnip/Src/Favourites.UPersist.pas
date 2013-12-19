d398e8542bc57b25216bcb50f2276dc8e45002ce
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2013-12-19 14:07:10 +0000

    Reverse merged revision(s) 4439-4438 from branches/parsnip:
    Reverted the changes made in r4438 to r4440:
    * Support removed for v2 of the Favourites file format
    * Legacy database reader now uses custom code to read the legacy favourites file.
    
    I've decided to get favourites for display in favourites dialogue box directly from database and to remove support for displaying and sorting by a last access date. This means that the TFavourites and TFavouritesPersist classes will become redundant.

--- branches/parsnip/Src/Favourites.UPersist.pas
+++ branches/parsnip/Src/Favourites.UPersist.pas
@@ -21,8 +21,7 @@ interface
 
 uses
   // Project
-  Favourites.UFavourites,
-  UExceptions;
+  Favourites.UFavourites, UExceptions;
 
 
 type
@@ -35,24 +34,18 @@ type
   TFavouritesPersist = record
   strict private
     const
-      ///  <summary>Watermark that is present one the first line of a valid v1
-      ///  of the favourites file.</summary>
-      WatermarkV1 = #$25BA + ' CodeSnip Favourites v1 ' + #$25C4;
-      ///  <summary>Watermark that is present one the first line of a valid v2
-      ///  of the favourites file.</summary>
-      WatermarkV2 = #$25BA + ' CodeSnip Favourites v2 ' + #$25C4;
+      ///  <summary>Watermark that is present one the first line of a valid
+      ///  favourites file.</summary>
+      Watermark = #$25BA + ' CodeSnip Favourites v1 ' + #$25C4;
   strict private
     ///  <summary>Returns fully specified name of the favourites file.</summary>
     class function FavouritesFileName: string; static;
   public
     ///  <summary>Saves all favourites from given favourites list to file.
     ///  </summary>
-    ///  <remarks>Always saves the file in version 2 format.</remarks>
     class procedure Save(Favourites: TFavourites); static;
     ///  <summary>Loads all favourites from file into given favourites list.
     ///  </summary>
-    ///  <remarks>Can read from files in either v1 or v2 format. The "user
-    ///  defined" field of v1 files is ignored.</remarks>
     class procedure Load(Favourites: TFavourites); static;
   end;
 
@@ -67,6 +60,7 @@ uses
   Classes,
   /// Project
   CS.Database.Types,
+  DB.UMain,
   UAppInfo,
   UConsts,
   UIOUtils,
@@ -82,33 +76,14 @@ begin
 end;
 
 class procedure TFavouritesPersist.Load(Favourites: TFavourites);
-resourcestring
-  sBadFormat = 'Invalid favourites file format';
-type
-  TVersionNumber = 1..2;
 var
   Lines: IStringList;
   Line: string;
+  Fields: IStringList;
   SnippetID: TSnippetID;
   LastAccess: TDateTime;
-  Version: TVersionNumber;
-
-  procedure ReadFieldsFromLine;
-  const
-    FieldCount: array[TVersionNumber] of Byte = (3, 2);
-    IDFileIdx = 0;
-    DateFieldIdx: array[TVersionNumber] of Byte = (2, 1);
-  var
-    Fields: IStringList;
-  begin
-    Fields := TIStringList.Create(Line, TAB, False, True);
-    if Fields.Count <> FieldCount[Version] then
-      raise EFavouritesPersist.Create(sBadFormat);
-    SnippetID := TSnippetID.Create(Fields[IDFileIdx]);
-    if not TryStrToDateTime(Fields[DateFieldIdx[Version]], LastAccess) then
-      raise EFavouritesPersist.Create(sBadFormat);
-  end;
-
+resourcestring
+  sBadFormat = 'Invalid favourites file format';
 begin
   if not TFile.Exists(FavouritesFileName, False) then
     Exit;
@@ -125,19 +100,22 @@ begin
       raise;
   end;
   Line := Lines[0];
-  if Line = WatermarkV2 then
-    Version := 2
-  else if Line = WatermarkV1 then
-    Version := 1
-  else
+  if Line <> Watermark then
     raise EFavouritesPersist.Create(sBadFormat);
   Lines.Delete(0);
   for Line in Lines do
   begin
     if StrIsBlank(Line) then
       Continue;
-    ReadFieldsFromLine;
-    Favourites.Add(SnippetID, LastAccess);
+    Fields := TIStringList.Create(Line, TAB, False, True);
+    if Fields.Count <> 3 then
+      raise EFavouritesPersist.Create(sBadFormat);
+    SnippetID := TSnippetID.Create(Fields[0]);
+    if not TryStrToDateTime(Fields[2], LastAccess) then
+      raise EFavouritesPersist.Create(sBadFormat);
+    // only add to favourites if snippet in database
+    if Database.SnippetExists(SnippetID) then
+      Favourites.Add(SnippetID, LastAccess);
   end;
 end;
 
@@ -148,10 +126,12 @@ var
 begin
   SB := TStringBuilder.Create;
   try
-    SB.AppendLine(WatermarkV2); // always save favourites as v2
+    SB.AppendLine(Watermark);
     for Fav in Favourites do
     begin
       SB.Append(Fav.SnippetID.ToString);
+      SB.Append(TAB);
+      SB.Append(BoolToStr(True, True)); // redundant field: always True in v5
       SB.Append(TAB);
       SB.Append(DateTimeToStr(Fav.LastAccessed));
       SB.AppendLine;
