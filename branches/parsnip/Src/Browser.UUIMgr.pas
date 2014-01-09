93b81910bc2fde52b8f0cca732e8d53a4d700467
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2014-01-09 14:50:23 +0000

    Deleted TaskAllocWideString routine and modified unit to use the version of the routine from the CS.Utils.COM unit.

--- branches/parsnip/Src/Browser.UUIMgr.pas
+++ branches/parsnip/Src/Browser.UUIMgr.pas
@@ -3,7 +3,7 @@
  * v. 2.0. If a copy of the MPL was not distributed with this file, You can
  * obtain one at http://mozilla.org/MPL/2.0/
  *
- * Copyright (C) 2005-2013, Peter Johnson (www.delphidabbler.com).
+ * Copyright (C) 2005-2014, Peter Johnson (www.delphidabbler.com).
  *
  * $Rev$
  * $Date$
@@ -22,9 +22,14 @@ interface
 
 uses
   // Delphi
-  Classes, Windows, ActiveX, SHDocVw, Menus,
+  Classes,
+  Windows,
+  ActiveX,
+  SHDocVw,
+  Menus,
   // Project
-  Browser.IntfDocHostUI, Browser.UNulUIHandler;
+  Browser.IntfDocHostUI,
+  Browser.UNulUIHandler;
 
 
 type
@@ -341,28 +346,12 @@ uses
   // Delphi
   SysUtils,
   // Project
-  Browser.UControlHelper, UHTMLDOMHelper, UThemesEx, UUrlMonEx;
+  CS.Utils.COM,
+  Browser.UControlHelper,
+  UHTMLDOMHelper,
+  UThemesEx,
+  UUrlMonEx;
 
-
-function TaskAllocWideString(const S: string): PWChar;
-  {Allocates memory for a wide string using the Shell's task allocator and
-  copies a given string into the memory as a wide string. Caller is responsible
-  for freeing the buffer and must use the shell's allocator to do this.
-    @param S [in] String to convert.
-    @return Pointer to buffer containing wide string.
-  }
-var
-  StrLen: Integer;  // length of string in bytes
-begin
-  // Store length of string, allowing for terminal #0
-  StrLen := Length(S) + 1;
-  // Allocate buffer for wide string using task allocator
-  Result := CoTaskMemAlloc(StrLen * SizeOf(WideChar));
-  if not Assigned(Result) then
-    raise EOutOfMemory.Create('TaskAllocWideString: can''t allocate buffer.');
-  // Convert string to wide string and store in buffer
-  StringToWideChar(S, Result, StrLen);
-end;
 
 { TWBUIMgr }
 
