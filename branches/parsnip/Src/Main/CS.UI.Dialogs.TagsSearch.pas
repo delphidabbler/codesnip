f73d6887018aa9e3f9285b6e762819c69f038fc7
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2014-03-05 21:54:08 +0000

    Modified to persist selected tags and search logic between executions.

--- branches/parsnip/Src/Main/CS.UI.Dialogs.TagsSearch.pas
+++ branches/parsnip/Src/Main/CS.UI.Dialogs.TagsSearch.pas
@@ -39,10 +39,35 @@ type
     procedure FormCreate(Sender: TObject);
     procedure FormDestroy(Sender: TObject);
   strict private
+    type
+      TSearchParams = class(TObject)
+      strict private
+        var
+          fTags: ITagSet;
+          fLogic: TSearchLogic;
+          fUpdated: Boolean;
+        procedure SetTags(ATags: ITagSet);
+        procedure SetLogic(ALogic: TSearchLogic);
+        procedure Load;
+        procedure Save;
+      public
+        ///  <summary>Constructs object and initialises properties from
+        ///  persistent storage.</summary>
+        constructor Create;
+        ///  <summary>Saves current property values to persistent storage then
+        ///  destroys instance.</summary>
+        destructor Destroy; override;
+        ///  <summary>Set of tags to include in search.</summary>
+        property Tags: ITagSet read fTags write SetTags;
+        ///  <summary>Search logic. Either AND or OR.</summary>
+        property Logic: TSearchLogic read fLogic write SetLogic;
+      end;
+  strict private
     var
       fSearch: ISearch;
       fRefinePreviousSearch: Boolean;
       fTagListMgr: TSortedCollectionCtrlKVMgr<TTag>;
+      fSearchParams: TSearchParams;
   strict protected
     procedure InitForm; override;
     procedure ArrangeForm; override;
@@ -57,6 +82,8 @@ uses
   CS.Database.Tags,
   DB.UMain,
   UCtrlArranger,
+  UIStringList,
+  USettings,
   UStrUtils {for inlining};
 
 {$R *.dfm}
@@ -101,15 +128,14 @@ procedure TTagsSearchDlg.btnOKClick(Sender: TObject);
 var
   Filter: ITagSearchFilter; // search filter
 begin
-  // Create search filter from entries made in dialog box
+  // Create filter and search objects from entries in dialogue box
   Filter := TSearchFilterFactory.CreateTagSearchFilter(GetTags, GetLogic);
-//  // Persist the search criteria
-//  fSearchParams.Logic := Filter.Logic;
-//  fSearchParams.Tags := Filter.Tags;
-  // Create search object from filter
   fSearch := TSearchFactory.CreateSearch(Filter);
   // Record search scope
   fRefinePreviousSearch := rgScope.ItemIndex = 0;
+  // Persist the search criteria
+  fSearchParams.Logic := Filter.Logic;
+  fSearchParams.Tags := Filter.Tags;
 end;
 
 class function TTagsSearchDlg.Execute(const AOwner: TComponent;
@@ -140,10 +166,12 @@ begin
     end,
     stIgnoreCase
   );
+  fSearchParams := TSearchParams.Create;
 end;
 
 procedure TTagsSearchDlg.FormDestroy(Sender: TObject);
 begin
+  fSearchParams.Free;
   fTagListMgr.Free;
   inherited;
 end;
@@ -151,10 +179,81 @@ end;
 procedure TTagsSearchDlg.InitForm;
 var
   Tag: TTag;
+  CheckedTag: TTag;
+  I: Integer;
 begin
   inherited;
+  // Populate tags check list box
   for Tag in Database.GetAllTags do
     fTagListMgr.Add(Tag, Tag.ToString);
+  // Select tags per persistent storage
+  for CheckedTag in fSearchParams.Tags do
+  begin
+    I := fTagListMgr.IndexOfKey(CheckedTag);
+    clbTags.Checked[I] := True;
+  end;
+  // Select appropriate search logic radio button
+  // radio button index is ordinal value of Logic
+  rgLogic.ItemIndex := Ord(fSearchParams.Logic);
+end;
+
+{ TTagsSearchDlg.TSearchParams }
+
+constructor TTagsSearchDlg.TSearchParams.Create;
+begin
+  inherited Create;
+  fTags := TTagSet.Create;
+  Load;
+end;
+
+destructor TTagsSearchDlg.TSearchParams.Destroy;
+begin
+  if fUpdated then
+    Save;
+  inherited;
+end;
+
+procedure TTagsSearchDlg.TSearchParams.Load;
+var
+  Storage: ISettingsSection;
+  TagNames: IStringList;
+  TagName: string;
+begin
+  Storage := Settings.ReadSection(ssFindTags);
+  fTags.Clear;
+  TagNames := Storage.GetStrings('TagCount', 'Tag%d');
+  for TagName in TagNames do
+    fTags.Add(TTag.Create(TagName));
+  fLogic := TSearchLogic(Storage.GetInteger('Logic', Ord(slOr)));
+  fUpdated := False;
+end;
+
+procedure TTagsSearchDlg.TSearchParams.Save;
+var
+  Storage: ISettingsSection;  // object used to access persistent storage
+  TagNames: IStringList;
+  Tag: TTag;
+begin
+  Storage := Settings.EmptySection(ssFindTags);
+  TagNames := TIStringList.Create;
+  for Tag in fTags do
+    TagNames.Add(Tag.ToString);
+  Storage.SetStrings('TagCount', 'Tag%d', TagNames);
+  Storage.SetInteger('Logic', Ord(fLogic));
+  Storage.Save;
+end;
+
+procedure TTagsSearchDlg.TSearchParams.SetLogic(ALogic: TSearchLogic);
+begin
+  fLogic := ALogic;
+  fUpdated := True;
+end;
+
+procedure TTagsSearchDlg.TSearchParams.SetTags(ATags: ITagSet);
+begin
+  fTags := ATags;
+  fUpdated := True;
 end;
 
 end.
+
