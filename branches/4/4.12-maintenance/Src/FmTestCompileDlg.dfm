eeaf9c0acd9df15e99632c546eecbf07ae0fc842
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2015-05-06 02:09:23 +0000

    New maintenance branch for CodeSnip v4.12 created as a copy of release v4.12.0

---
+++ branches/4/4.12-maintenance/Src/FmTestCompileDlg.dfm
@@ -0,0 +1,79 @@
+inherited TestCompileDlg: TTestCompileDlg
+  Caption = 'Test Compile Results'
+  PixelsPerInch = 96
+  TextHeight = 13
+  inherited pnlBody: TPanel
+    Top = 9
+    Width = 209
+    ExplicitTop = 9
+    ExplicitWidth = 209
+    object lblSnippetName: TLabel
+      Left = 96
+      Top = 8
+      Width = 89
+      Height = 13
+      Caption = 'lblSnippetName'
+      Font.Charset = DEFAULT_CHARSET
+      Font.Color = clWindowText
+      Font.Height = -11
+      Font.Name = 'Tahoma'
+      Font.Style = [fsBold]
+      ParentFont = False
+    end
+    object lblSnippetNameDesc: TLabel
+      Left = 0
+      Top = 8
+      Width = 53
+      Height = 13
+      Caption = 'Results for '
+    end
+    object sbCompilers: TScrollBox
+      Left = 0
+      Top = 27
+      Width = 201
+      Height = 249
+      BevelInner = bvNone
+      BevelOuter = bvRaised
+      BevelKind = bkFlat
+      BorderStyle = bsNone
+      TabOrder = 0
+    end
+  end
+  object btnViewErrors: TButton
+    Left = 8
+    Top = 304
+    Width = 153
+    Height = 25
+    Action = actViewErrors
+    TabOrder = 3
+  end
+  object alMain: TActionList
+    Left = 256
+    Top = 16
+    object actScrollPageUp: TAction
+      Caption = 'actScrollPageUp'
+      ShortCut = 33
+      OnExecute = actScrollPageUpExecute
+    end
+    object actScrollPageDown: TAction
+      Caption = 'actScrollPageDown'
+      ShortCut = 34
+      OnExecute = actScrollPageDownExecute
+    end
+    object actScrollLineUp: TAction
+      Caption = 'actScrollLineUp'
+      ShortCut = 38
+      OnExecute = actScrollLineUpExecute
+    end
+    object actScrollLineDown: TAction
+      Caption = 'actScrollLineDown'
+      ShortCut = 40
+      OnExecute = actScrollLineDownExecute
+    end
+    object actViewErrors: TAction
+      Caption = '&View Errors && Warnings...'
+      OnExecute = actViewErrorsExecute
+      OnUpdate = actViewErrorsUpdate
+    end
+  end
+end
---
+++ branches/4/4.12-maintenance/Src/Res/Scripts/overflowXFix.js
@@ -0,0 +1,92 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2013, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * JavaScript code used fix the overflow "auto" display bug in Internet
+ * explorer where specifying "auto" as the value of the overflow-x CSS property
+ * causes an unexpected vertical scroll bar to be displayed whenever the
+ * horizontal scroll bar is displayed.
+ */
+
+(function () {
+    var elemsToFix;
+
+    function initElems() {
+        var sourceCodeParent;
+        var compileResultsElem;
+        var elemProps;
+        var i;
+        sourceCodeParent = document.getElementById('sourcecode');
+        if (sourceCodeParent)
+            elemProps = getElementsByClass('pas-source', sourceCodeParent);
+        else
+            elemProps = [];
+        compileResultsElem = document.getElementById('compile-results');
+        if (compileResultsElem)
+            elemProps.push(compileResultsElem);
+        elemsToFix = [];
+        for (i = 0; i < elemProps.length; i += 1) {
+            elemsToFix.push({
+                props: elemProps[i],
+                oldPaddingBottom: elemProps[i].style['paddingBottom']
+            });
+        }
+    }
+
+    // Modification of function by Dustin Diaz:
+    //   http://www.dustindiaz.com/getelementsbyclass
+    function getElementsByClass(searchClass,node,tag) {
+        var classElements = [];
+        if (node == null) {
+            node = document;
+        }
+        if (tag == null) {
+            tag = '*';
+        }
+        var els = node.getElementsByTagName(tag);
+        var elsLen = els.length;
+        var pattern = new RegExp("(^|\\s)" + searchClass + "(\\s|$)");
+        var i = 0, j = 0;
+        while (i < elsLen) {
+            if ( pattern.test(els[i].className) ) {
+                classElements[j] = els[i];
+                j += 1;
+            }
+            i += 1;
+        }
+        return classElements;
+    }
+
+    // Derived from Remy Sharp's code:
+    //   http://remysharp.com/2008/01/21/fixing-ie-overflow-problem/
+    function fixOverflow(elems) {
+        var i;
+        for (i = 0; i < elems.length; i += 1) {
+            // if the scrollWidth (the real width) is greater than the visible
+            // width, then apply style changes
+            if (elems[i].props.scrollWidth > elems[i].props.offsetWidth) {
+                elems[i].props.style['paddingBottom'] = '18px';
+                elems[i].props.style['overflowY'] = 'hidden';
+            } else {
+                elems[i].props.style['paddingBottom'] = 
+                    elems[i].oldPaddingBottom;
+            }
+        }
+    }
+
+    window.onload = function () {
+        initElems();
+        fixOverflow(elemsToFix);
+    };
+
+    window.onresize = function () {
+        fixOverflow(elemsToFix);
+    };
+
+}());
