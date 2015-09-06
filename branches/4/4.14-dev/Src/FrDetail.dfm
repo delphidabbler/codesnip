e52a69f104e9bc94c40a6596c0da82c0cb891994
delphidabbler <delphidabbler@b4df9409-4820-46a1-b8f1-46e6eca88fe3>
2015-09-06 01:47:45 +0000

    New development branch for CodeSnip v4.14 created as a copy of release v4.13.0

---
+++ branches/4/4.14-dev/Src/FrDetail.dfm
@@ -0,0 +1,57 @@
+inherited DetailFrame: TDetailFrame
+  inherited pnlTitle: TPanel
+    inherited lblTitle: TLabel
+      Width = 39
+      Caption = 'Details'
+      ExplicitWidth = 39
+    end
+  end
+  inline frmDetailView: TDetailViewFrame
+    Left = 0
+    Top = 50
+    Width = 318
+    Height = 188
+    Align = alClient
+    TabOrder = 2
+    TabStop = True
+    ExplicitTop = 50
+    ExplicitWidth = 318
+    ExplicitHeight = 188
+    inherited pnlBrowser: TPanel
+      Width = 318
+      Height = 188
+      ExplicitWidth = 318
+      ExplicitHeight = 188
+      inherited wbBrowser: TWebBrowser
+        Width = 318
+        Height = 188
+        ExplicitWidth = 318
+        ExplicitHeight = 188
+        ControlData = {
+          4C000000DE2000006E1300000000000000000000000000000000000000000000
+          000000004C000000000000000000000001000000E0D057007335CF11AE690800
+          2B2E126208000000000000004C0000000114020000000000C000000000000046
+          8000000000000000000000000000000000000000000000000000000000000000
+          00000000000000000100000000000000000000000000000000000000}
+      end
+    end
+  end
+  object tcViews: TTabControl
+    Left = 0
+    Top = 27
+    Width = 318
+    Height = 23
+    Align = alTop
+    PopupMenu = mnuTabs
+    TabOrder = 1
+    OnChange = tcViewsChange
+    OnDragDrop = tcViewsDragDrop
+    OnDragOver = tcViewsDragOver
+    OnMouseDown = tcViewsMouseDown
+  end
+  object mnuTabs: TPopupMenu
+    AutoPopup = False
+    Left = 144
+    Top = 104
+  end
+end
---
+++ branches/4/4.14-dev/Src/UHTMLHelpMgr.pas
@@ -0,0 +1,114 @@
+{
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/
+ *
+ * Copyright (C) 2007-2012, Peter Johnson (www.delphidabbler.com).
+ *
+ * $Rev$
+ * $Date$
+ *
+ * Implements a class that implements the IHelpMgr interface and registers an
+ * instantiation of it as the program's help manager. The object provides help
+ * using the HTML help system.
+}
+
+
+unit UHTMLHelpMgr;
+
+
+interface
+
+
+implementation
+
+
+uses
+  // Delphi
+  Windows,
+  // Project
+  UAppInfo, UEncodings, UHTMLHelp, UHelpMgr;
+
+
+type
+  {
+  THTMLHelpMgr:
+    Class that interacts with and controls HTML Help and implements IHelpMgr.
+  }
+  THTMLHelpMgr = class(TInterfacedObject,
+    IHelpMgr
+  )
+  strict private
+    procedure DoAppHelp(const Command: LongWord; const HelpPage: string;
+      const Data: LongWord);
+      {Calls HtmlHelp API with specified command and parameters.
+        @param Command [in] Command to send to HTML Help.
+        @param HelpPage [in] Name of HTML file containing required help page
+          within help file. May be '' if no specific page required.
+        @param Data [in] Data to pass to HTML Help. Format depends on command.
+      }
+  public
+    procedure ShowHelp(const AKeyword: string);
+      {Displays HTML help topic specified by an A-Link keyword.
+        @param AKeyword [in] Required A-Link keyword.
+      }
+    procedure ShowContents;
+      {Displays HTML help contents tab and default help page.
+      }
+  end;
+
+{ THTMLHelpMgr }
+
+procedure THTMLHelpMgr.DoAppHelp(const Command: LongWord;
+  const HelpPage: string; const Data: LongWord);
+  {Calls HtmlHelp API with specified command and parameters.
+    @param Command [in] Command to send to HTML Help.
+    @param HelpPage [in] Name of HTML file containing required help page within
+      help file. May be '' if no specific page required.
+    @param Data [in] Data to pass to HTML Help. Format depends on command.
+  }
+var
+  HelpSpec: string; // Help file followed by any required page
+begin
+  HelpSpec := TAppInfo.HelpFileName;
+  if HelpPage <> '' then
+    HelpSpec := HelpSpec + '::/' + HelpPage;
+  HtmlHelp(GetDesktopWindow(), PChar(HelpSpec), Command, Data);
+end;
+
+procedure THTMLHelpMgr.ShowContents;
+  {Displays HTML help contents tab and default help page.
+  }
+begin
+  DoAppHelp(HH_DISPLAY_TOC, '', 0);
+end;
+
+procedure THTMLHelpMgr.ShowHelp(const AKeyword: string);
+  {Displays HTML help topic specified by an A-Link keyword.
+    @param AKeyword [in] Required A-Link keyword.
+  }
+var
+  ALink: THHAKLink; // structure containing details of A-Link
+begin
+  ZeroMemory(@ALink, SizeOf(THHAKLink));
+  // Fill in A link structure
+  ALink.cbStruct := SizeOf(THHAKLink);
+  // *** NOTE:
+  // This one is weird: when using the unicode API just casting the keyword to
+  // PChar causes HTML Help to see only the first character of the keyword. We
+  // have to cast to ASCII string and then to a pointer to get this to work,
+  // even though pszKeywords is declared as PWideChar
+  ALink.pszKeywords := Pointer(StringToASCIIString(AKeyword));
+  ALink.fIndexOnFail := True;
+  // Display help
+  DoAppHelp(HH_ALINK_LOOKUP, '', LongWord(@ALink));
+end;
+
+initialization
+
+// Create instance of help manager and register it for use as program's help
+// system
+RegisterHelpMgr(THTMLHelpMgr.Create);
+
+end.
+
