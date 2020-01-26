251af6b1c7e7f4b50158a6cb2813800ef075c15e
delphidabbler <git@delphidabbler.com>
2020-01-26 02:38:54 +0000

    Added TODO

--- Src/USnippetValidator.pas
+++ Src/USnippetValidator.pas
@@ -11,6 +11,7 @@
  * Implements a static class that checks a snippet for validity.
 }
 
+// TODO: Validate module names - surley they must be valid (optionally dotted) Pascal identifiers.
 
 unit USnippetValidator;
 
