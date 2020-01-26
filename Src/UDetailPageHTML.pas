delphidabbler/codesnip

commit 56630a9fadbda6ea9886eed86334df1977c65dda

Reorganised outstanding TODOs

Src/UDetailPageHTML.pas
@@ -469,7 +469,7 @@ var
   end;
 
 begin
-  inherited;
+  // TODO -cUI: remove unused placeholder resolutions  inherited;
   Tplt.ResolvePlaceholderHTML(
     'externalScript', TJavaScript.LoadScript('external.js', etWindows1252)
   );
@@ -509,11 +509,6 @@ begin
     'ProgramAutoCheckFrequency',
     UpdateFrequencyText(Preferences.AutoCheckProgramFrequency)
   );
-  // TODO: Restore line similar to following when synch spaces can auto-update
-//  Tplt.ResolvePlaceholderText(
-//    'DatabaseAutoCheckFrequency',
-//    UpdateFrequencyText(Preferences.AutoCheckDatabaseFrequency)
-//  );
 end;
 
 { TDBUpdatedPageHTML }

Src/CodeSnip.todo
@@ -1,4 +1,4 @@
-{TODO -odatabase.html :
+{TODO -cQuery -odatabase.html :
 Consider adding License property to snippets.
 See database.html for full details}
 {TODO -cDatabase :

Src/DB.UMain.pas
@@ -43,7 +43,7 @@ type
     evSnippetDeleted,       // a snippet has been deleted
     evBeforeSnippetChange,  // a snippet is about to be changed
     evSnippetChanged        // a snippet's properties / references have changed
-    // TODO: add suitable event kinds for changes to tags as required
+    // TODO -cDatabase: add suitable event kinds for changes to tags as required
   );
 
   {
@@ -217,16 +217,16 @@ begin
       raise ECodeSnip.CreateFmt(sNameExists, [ASnippet.ID.ToString]);
   if not fAllTags.ContainsSubSet(ASnippet.Tags) then
   begin
-    { TODO: Add TriggerNullDataEvent(evChangeBegin); }
+    { TODO -cDatabase: Add TriggerNullDataEvent(evChangeBegin); }
     fAllTags.Include(ASnippet.Tags);
-    { TODO: Add change event for all tags added. Something like:
+    { TODO -cDatabase: Add change event for all tags added. Something like:
        for Tag in ASnippet.Tags do
         TriggerTagChangeEvent(evTagAdded, Tag);  }
-    { TODO: Add TriggerNullDataEvent(evChangeEnd); }
+    { TODO -cDatabase: Add TriggerNullDataEvent(evChangeEnd); }
   end;
   TriggerNullDataEvent(evChangeBegin);
   try
-    // TODO: Do we need to check snippet references and xrefs here?
+    // TODO -cQuery: Do we need to check snippet references and xrefs here?
     DBSnippet := TDBSnippet.CreateFrom(ASnippet);
     FlagUpdate; // sets fLastModifed
     DBSnippet.SetCreated(fLastModified);
@@ -256,7 +256,7 @@ begin
   try
     Dependents := Database.GetDependentsOf(ASnippetID);
     Referrers := Database.GetReferrersTo(ASnippetID);
-    // TODO: scan all snippets and remove references that match snippet ID
+    // TODO -cDatabase: scan all snippets and remove references that match snippet ID
     // Delete snippet for XRef or Depends list of referencing snippets
     for Referrer in Referrers do
       fSnippetsTable.Get(Referrer).GetXRefs.Remove(ASnippetID);
@@ -375,7 +375,7 @@ procedure TDatabase.Load;
 var
   Loader: IDatabaseLoader;
 begin
-  fSnippetsTable.Clear; // TODO: decide if loaders should clear this table
+  fSnippetsTable.Clear; // TODO -cDatabase: decide if loaders should clear this table
   Loader := TDatabaseIOFactory.CreateLoader;
   Loader.Load(fSnippetsTable, fAllTags, fLastModified);
   FixUpSnippetRefs;
@@ -507,21 +507,21 @@ procedure TDatabase.UpdateSnippet(ASnippet: IEditableSnippet);
 var
   DBSnippet: TDBSnippet;
 begin
-  // TODO: refactor out this common in common with AddSnippet
+  // TODO -cRefactor: refactor out this common code in common with AddSnippet
   if not fAllTags.ContainsSubSet(ASnippet.Tags) then
   begin
-    { TODO: Add TriggerNullDataEvent(evChangeBegin); }
+    { TODO -cDatabase: Add TriggerNullDataEvent(evChangeBegin); }
     fAllTags.Include(ASnippet.Tags);
-    { TODO: Add change event for all tags added. Something like:
+    { TODO -cDatabase: Add change event for all tags added. Something like:
        for Tag in ASnippet.Tags do
         TriggerTagChangeEvent(evTagAdded, Tag);  }
-    { TODO: Add TriggerNullDataEvent(evChangeEnd); }
+    { TODO -cDatabase: Add TriggerNullDataEvent(evChangeEnd); }
   end;
   TriggerNullDataEvent(evChangeBegin);
   TriggerSnippetChangeEvent(evBeforeSnippetChange, ASnippet.ID);
   DBSnippet := TDBSnippet.CreateFrom(ASnippet);
   try
-    // TODO: Do we need to check snippet references and xrefs here?
+    // TODO -cQuery: Do we need to check snippet references and xrefs here?
     FlagUpdate;
     DBSnippet.SetModified(fLastModified);
     fSnippetsTable.Update(DBSnippet);

Src/FmSWAGImportDlg.pas
@@ -12,7 +12,7 @@
  * snippets from the DelphiDabbler implementation of the SWAG Pascal archive.
 }
 
-// TODO -cwebsvc: change to import SWAG from local file - user must download and extract
+// TODO -cSWAG: change to import SWAG from local file - user must download and extract
 
 unit FmSWAGImportDlg;
 
@@ -400,7 +400,7 @@ begin
       // means indices of new items added are not sequential, and we must have
       // displayed title at same index in clbSelectSnippets as its snippet is in
       // fCurrentCatSnippets.
-      { TODO: When conversion to Delphi Collections complete, try to assign
+      { TODO -cRefactor: When conversion to Delphi Collections complete, try to assign
               Snippets to fCurrentCatSnippets in one operation. }
       for Snippet in Snippets do
         fCurrentCatSnippets.Add(Snippet);
@@ -489,7 +489,7 @@ begin
       // indices of new items added are not sequential, and we must have
       // displayed title at same index in lbCategories as its category is in
       // fSortedCategories.
-      { TODO: When conversion to Delphi Collections complete, try to assign
+      { TODO -cRefactor: When conversion to Delphi Collections complete, try to assign
               Cats to fSortedCategories in one operation. }
       for Cat in Cats do
         fSortedCategories.Add(Cat);
@@ -553,7 +553,7 @@ begin
     )
   );
 
-  // TODO: change to some structure that doesn't permit duplicates.
+  // TODO -cSWAG: change to some structure that doesn't permit duplicates.
   fSelectedSnippets := TSortedList<TSWAGSnippet>.Create(
     TRulesFactory<TSWAGSnippet>.Construct(
       function (const Left, Right: TSWAGSnippet): Integer
@@ -658,7 +658,7 @@ begin
       WaitWrapper(Self, CallProc, sWaitMsg);
     end
   );
-  // TODO: Display snippet as HTML, highlighted if necessary
+  // TODO -cLater: Display snippet as HTML, highlighted if necessary
   Content := Format(
     sContentTplt,
     [

Src/FmSnippetsEditorDlg.pas
@@ -883,7 +883,7 @@ begin
   begin
     // We are adding a new snippet: clear all controls or set default values
     frmSourceEditor.Clear;
-    { TODO: Permit default language to be customisable by user: modify
+    { TODO -cSettings: Permit default language to be customisable by user: modify
             Preferences dialogue box and Preferences object ? }
     Language := TConfig.Instance.SourceCodeLanguages[
       TSourceCodeLanguageID.CreatePascal

Src/FmUserDataPathDlg.FrProgress.pas
@@ -17,7 +17,7 @@
 unit FmUserDataPathDlg.FrProgress;
 
 
-// TODO: Rename this unit to relate to frame class name
+// TODO -cRename: Rename this unit to relate to frame class name
 
 
 interface

Src/FmUserDataPathDlg.pas
@@ -16,7 +16,7 @@
 unit FmUserDataPathDlg;
 
 
-// TODO: Rename this unit to relate to form class name
+// TODO -cRename: Rename this unit to relate to form class name
 
 
 interface

Src/FrOverview.pas
@@ -112,7 +112,7 @@ type
       fCommandBars: TCommandBarMgr; // Configures popup menu and toolbar
       fPreviousGrouping: Integer;
       fGroupingCBMgr: TUnsortedCollectionCtrlKVMgr<Integer>;
-        // TODO: make grouping IDs into enum type instead of integer consts
+        // TODO -cRefactor: make grouping IDs into enum type instead of integer consts
   strict private
     procedure SelectNode(const Node: TTreeNode; const MakeVisible: Boolean);
       {Selects a specified node and optionally make it visible in the tree view.

commit ad2268f13afe49cd019ae58fc98b01ae872b19b7

Fixed bug introduced by commit 56630a9

Src/UDetailPageHTML.pas
@@ -469,7 +469,8 @@ var
   end;
 
 begin
-  // TODO -cUI: remove unused placeholder resolutions  inherited;
+  inherited;
+  // TODO -cUI: remove unused placeholder resolutions
   Tplt.ResolvePlaceholderHTML(
     'externalScript', TJavaScript.LoadScript('external.js', etWindows1252)
   );