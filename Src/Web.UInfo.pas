8e9d69f8bea334133e0e3081259d2a4b2240a700
delphidabbler <git@delphidabbler.com>
2020-02-06 16:34:58 +0000

    Deleted unused URL from TWebInfo

--- Src/Web.UInfo.pas
+++ Src/Web.UInfo.pas
@@ -89,8 +89,6 @@ type
     const
       ///  <summary>URL of CodeSnip's GitHub page.</summary>
       GitHubURL = 'https://github.com/delphidabbler/codesnip';
-      ///  <summary>URL of home page of the CodeSnip project.</summary>
-      ProgramHomeURL = WebsiteURL + '/url/codesnip-home';
       ///  <summary>URL of the online Code Snippets database.</summary>
       DatabaseURL = WebsiteURL + '/url/csdb';
       ///  <summary>URL used to make donations towards the CodeSnip project.
