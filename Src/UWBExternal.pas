a909e28bc34682add4bf97e620e590a2e0a0e31f
delphidabbler <git@delphidabbler.com>
2020-03-24 23:51:15 +0000

    Remove CheckForUpdates method from external object

--- Src/UWBExternal.pas
+++ Src/UWBExternal.pas
@@ -26,7 +26,7 @@ uses
 
 
 type
-  ///  <summary>COM object that implements the methods of the IWBExternal12
+  ///  <summary>COM object that implements the methods of the IWBExternal14
   ///  interface that extend the browser control's 'external' object.</summary>
   ///  <remarks>
   ///  <para>This class enables application code to be called from JavaScript
@@ -34,7 +34,7 @@ type
   ///  <para>The methods a declared in the type library that is defined in
   ///  External.idl.</para>
   ///  </remarks>
-  TWBExternal = class(TAutoIntfObject, IWBExternal13, ISetNotifier)
+  TWBExternal = class(TAutoIntfObject, IWBExternal14, ISetNotifier)
   strict private
     var
       ///  <summary>Object used to call application code in response to
@@ -54,7 +54,7 @@ type
     constructor Create;
 
     ///  <summary>Updates database from internet.</summary>
-    ///  <remarks>Method of IWBExternal12.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure UpdateDbase; safecall;
 
     ///  <summary>Displays a named snippet.</summary>
@@ -64,12 +64,12 @@ type
     ///  defined.</param>
     ///  <param name="NewTab">WordBool [in] Whether to display snippet in a new
     ///  tab.</param>
-    ///  <remarks>Method of IWBExternal12.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure DisplaySnippet(const SnippetName: WideString;
       UserDefined: WordBool; NewTab: WordBool); safecall;
 
     ///  <summary>Displays the Configure Compilers dialogue box.</summary>
-    ///  <remarks>Method of IWBExternal12.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure ConfigCompilers; safecall;
 
     ///  <summary>Edits a named snippet.</summary>
@@ -77,7 +77,7 @@ type
     ///  </param>
     ///  <remarks>
     ///  <para>The named snippet must be user defined.</para>
-    ///  <para>Method of IWBExternal13.</para>
+    ///  <para>Method of IWBExternal14.</para>
     ///  </remarks>
     procedure EditSnippet(const SnippetName: WideString); safecall;
 
@@ -86,30 +86,26 @@ type
     ///  </param>
     ///  <param name="NewTab">WordBool [in] Whether to display category in a new
     ///  tab.</param>
-    ///  <remarks>Method of IWBExternal13.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure DisplayCategory(const CatID: WideString; NewTab: WordBool);
       safecall;
 
     ///  <summary>Opens Snippet Editor ready to create a new snippet.</summary>
-    ///  <remarks>Method of IWBExternal13.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure NewSnippet; safecall;
 
     ///  <summary>Shows latest news items from CodeSnip news feed.</summary>
-    ///  <remarks>Method of IWBExternal13.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure ShowNews; safecall;
 
-    ///  <summary>Checks for program updates.</summary>
-    ///  <remarks>Method of IWBExternal13.</remarks>
-    procedure CheckForUpdates; safecall;
-
     ///  <summary>Displays the program's About Box.</summary>
-    ///  <remarks>Method of IWBExternal13.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure ShowAboutBox; safecall;
 
     ///  <summary>Displays specified page of the Preferences dialogue.</summary>
     ///  <param name="ClsName">WideString [in] Class name of the frame that
     ///  implements the required preferences page.</param>
-    ///  <remarks>Method of IWBExternal13.</remarks>
+    ///  <remarks>Method of IWBExternal14.</remarks>
     procedure ShowPrefsPage(const ClsName: WideString); safecall;
 
     ///  <summary>Records the notifier object that is used to call application
@@ -133,16 +129,6 @@ uses
 
 { TWBExternal }
 
-procedure TWBExternal.CheckForUpdates;
-begin
-  try
-    if Assigned(fNotifier) then
-      fNotifier.CheckForUpdates;
-  except
-    HandleException;
-  end;
-end;
-
 procedure TWBExternal.ConfigCompilers;
 begin
   try
@@ -162,7 +148,7 @@ begin
   ExeName := TAppInfo.AppExeFilePath;
   OleCheck(LoadTypeLib(PWideChar(ExeName), TypeLib));
   // Create the object using type library
-  inherited Create(TypeLib, IWBExternal13);
+  inherited Create(TypeLib, IWBExternal14);
 end;
 
 procedure TWBExternal.DisplayCategory(const CatID: WideString;
