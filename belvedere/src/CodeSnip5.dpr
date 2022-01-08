2b1bb83e59220502269662b3f5a2282a0e970651
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-01-08 16:25:16 +0000

    Add default "Windows 10 Dark" theme

--- belvedere/src/CodeSnip5.dpr
+++ belvedere/src/CodeSnip5.dpr
@@ -1,12 +1,15 @@
 program CodeSnip5;
 
 uses
-  Vcl.Forms;
+  Vcl.Forms,
+  Vcl.Themes,
+  Vcl.Styles;
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.MainFormOnTaskbar := True;
+  TStyleManager.TrySetStyle('Windows10 Dark');
   Application.Run;
 end.
