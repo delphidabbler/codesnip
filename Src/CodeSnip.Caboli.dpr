c1dcf3a240af942590b14ca1971d2fadc67bd35d
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2022-12-03 22:46:02 +0000

    Add UThemesEx unit to project
    Also added units required by UThemesEx, recursively:
    
    * UHiddenWindow
    * UMessageWindow
    * UMultiCastEvents
    * UStructs
    *

--- Src/CodeSnip.Caboli.dpr
+++ Src/CodeSnip.Caboli.dpr
@@ -1,7 +1,12 @@
 program CodeSnip.Caboli;
 
 uses
-  Vcl.Forms;
+  Vcl.Forms,
+  UThemesEx in 'UThemesEx.pas',
+  UMultiCastEvents in 'UMultiCastEvents.pas',
+  UMessageWindow in 'UMessageWindow.pas',
+  UHiddenWindow in 'UHiddenWindow.pas',
+  UStructs in 'UStructs.pas';
 
 {$R *.res}
 
