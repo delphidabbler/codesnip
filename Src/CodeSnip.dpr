a23192b76d33d863c1c979c99ecc162ba91e366b
delphidabbler <5164283+delphidabbler@users.noreply.github.com>
2025-03-01 15:32:07 +0000

    Add new DB.MetaData unit to project
    This unit encapsulates meta data. It provides an enumerated type,
    TMetaDataCap that enumerate the various meta data capabilities along with
    an associated TMetaDataCaps set type. There are 3 records: TLicenseInfo,
    TCopyrightInfo and TMetaData. TMetaData contains fields of type
    TLicenseInfo and TCopyrightInfo.
    
    TLicenseInfo and TCopyrightInfo are similar to TDBLicenseInfo and
    TDBCopyrightInfo in DB.UMetaData while TMetaDataCap / TMetaDataCaps are
    similar to TMetaDataCapability and TMetaDataCapabilities in
    DB.UMetaData.
    
    Ultimately DB.MetaData will replace DB.UMetaData and related units.

--- Src/CodeSnip.dpr
+++ Src/CodeSnip.dpr
@@ -381,7 +381,8 @@ uses
   DBIO.MetaData.DCSC in 'DBIO.MetaData.DCSC.pas',
   FmCollectionBackup in 'FmCollectionBackup.pas' {CollectionBackupDlg},
   DB.DataFormats in 'DB.DataFormats.pas',
-  DB.IO.DataFormat.Native in 'DB.IO.DataFormat.Native.pas';
+  DB.IO.DataFormat.Native in 'DB.IO.DataFormat.Native.pas',
+  DB.MetaData in 'DB.MetaData.pas';
 
 // Include resources
 {$Resource ExternalObj.tlb}       // Type library file
